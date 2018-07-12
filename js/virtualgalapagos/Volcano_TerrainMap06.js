93f8d9fd8df02dadeb0440818ee877d3f73eef45
Caio Brighenti <caio.brig@gmail.com>
2018-07-12 14:06:42 -0400

    Added missing buttons, added image map to plume placement, color coded buttons
    Former-commit-id: bf322a6a4ed392500ed061d8edd0ce9c39ba3360 [formerly 36d5b52e3b96896cd4b5a4403dee0b9ebde161dc]
    Former-commit-id: 36fd3a36da9d0c66172d46b506f0e52176f5cb9c

--- js/virtualgalapagos/Volcano_TerrainMap06.js
+++ js/virtualgalapagos/Volcano_TerrainMap06.js
@@ -2,9 +2,9 @@ var tog = false;
 
 function ToggleDates() {
   if  (tog) {
-    document.getElementById("gemap").src="/images/ge_map.png";
+    document.getElementById("gemap").src="../images/ge_map.png";
   } else {
-    document.getElementById("gemap").src="/images/ge_map_volcdates.png";
+    document.getElementById("gemap").src="../images/ge_map_volcdates.png";
   }
   tog = !tog;
 }
