8bd643a2fd7763a9417bcaff14858c3e3aaf2b2b
Caio Brighenti <caio.brig@gmail.com>
2018-07-25 14:59:56 -0400

    Added sample audio file to main menu
    Former-commit-id: 7fc22741e2bf651c21539ab9e4c9171e208a1e97 [formerly bff650f83959c5f766c16d667436eed181ccef5c] [formerly 2c5e540e09d43d733ecf2e7e866cc4f08ecdc651 [formerly 75942558e1bf65e710cce673c08e0873905b8332]]
    Former-commit-id: 04292aac1374e58a5bbe633b26849ae770d872e4 [formerly 4335c8fe0fafd989f6a96a32fc8f58a1966ce738]
    Former-commit-id: 7c2231009f8022dd5f1b1870d070f86e41a6ae3f

---
+++ js/virtualgalapagos/audiocontroller.js
@@ -0,0 +1,17 @@
+var paused = false;
+var bg_track;
+
+
+// toggle play/pause
+function playPause(){
+  if (paused){
+    play();
+  } else {
+    pause();
+  }
+  paused = !paused;
+}
+
+function rewind(time){
+  
+}
