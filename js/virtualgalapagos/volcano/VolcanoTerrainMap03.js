1dbbf0d55b0c19b16b122b8168ab00800c42a283
Caio Brighenti <caio.brig@gmail.com>
2018-08-02 10:15:06 -0400

    Fully baked implementation of TerrainMap03

--- js/virtualgalapagos/volcano/VolcanoTerrainMap03.js
+++ js/virtualgalapagos/volcano/VolcanoTerrainMap03.js
@@ -9,18 +9,29 @@ init();
 
 function init(){
   addFlag("island_clicks");
+  
+  document.getElementById("player").onended = function() {
+    trackDone();
+    enableOptional();
+  };
+  
+  if (localStorage.getItem("TerrainMap03") == "true") {
+    document.getElementById("opt_audio").disabled = false;
+  }
 }
 
 function on(name) {
   if (name=="santacruz"){
     if (opt_on){
       setTrack("Volcano_SantaCruz01_01");
+      play();
     }
     sc_click = true;
   } else if (name=="fernandina"){
     ferd_click = true;
     if (opt_on){
       setTrack("Volcano_Fernandina01_opt");
+      play();
     }
   }  
   document.getElementById("overlay").style.display = "block";
@@ -43,10 +54,12 @@ function bgAudioListener(){
 function enableOptional(){
   if (done_flags["island_clicks"] && done_flags["audio"]){
     document.getElementById("opt_audio").disabled = false;
+    localStorage.setItem("TerrainMap03", "true");
   }
 }
 
 function narrationOn(){
   opt_on = true;
   setTrack("Volcano_TerrainMap03opt");
+  play();
 }
