a8f770a29b809e2907851d37fb3cbdb23131c1e7
dgathogo <edgetech26@gmail.com>
2019-05-23 15:34:16 -0400

    refactor code into classes
    Former-commit-id: ef493843f86ea0ee2fe20d41ee1437ecc5ab0888 [formerly 91ad3bd34d4afa9f7dfb6d982e0316d55b3a43c6] [formerly be7b125fef87b5bc376d0bb14cfd6c6f813ccc40 [formerly 25c10c5062cbff81fffd5c3c7382f87ccefc7856]]
    Former-commit-id: 32263ec1b5a75264a0c1738c36465f12cebb95ba [formerly a85c9a29ebb7bd10d4ad7e80d1ab3be2b04c4cc6]
    Former-commit-id: 6d21d2ba5c0ef50fc0439dddb14db8e28aee1237

---
+++ js/virtualgalapagos/volcano/Volcano_TerrainMapAges01.js
@@ -0,0 +1,13 @@
+var ddDict = {
+  "7000001": ["Fernandina", "Isabela"],
+  "7000002": ["Fernandina", "Isabela"],
+  "1700000": ["Santa Fe"],
+  "2300000": ["Santa Cruz"],
+  "3300000": ["Espanola"],
+  "4000000": ["San Cristobal"],
+};
+
+
+dragdrop = new DragDrop(ddDict);
+dragdrop.initDrops();
+dragdrop.initDrags();
