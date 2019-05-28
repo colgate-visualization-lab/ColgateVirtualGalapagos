fec60c1e3320195aaf7f2466e2f6062e73756ac4
dgathogo <edgetech26@gmail.com>
2019-05-28 17:03:31 -0400

    More refactoring

--- js/virtualgalapagos/mastercontroller.js
+++ js/virtualgalapagos/mastercontroller.js
@@ -1,143 +1,273 @@
 "use strict"
 
-var pageList; 
-var next_btn;
-var pageName;
-var dev_mode = true;
-var done_flags = [];
-var idx;
-
-init();
-
-function init(){
-  // grab page name
-  // NEEDS REFACTOR
-  pageName = grabPageName();
-  pageName = pageName.substr(0, pageName.length-5);
-  
-  // initialize page list
-  pageList = ["Volcano_TerrainMap01", "panotour/Volcano_Exploration01", "panotour/Volcano_Exploration02", "panotour/Volcano_Exploration03", "panotour/Volcano_Exploration04", "panotour/Volcano_Exploration05", "Volcano_TerrainMap02", "Volcano_TerrainMap03", "Volcano_TerrainMap04", "Volcano_TerrainMapAges01", "Volcano_TerrainMapAges02", "Volcano_OtherVolcanoes01", "Volcano_TectonicPlateWhiteboard01", "Volcano_TectonicPlates01", "Volcano_PlatesGame01", "Volcano_TectonicPlates02", "Volcano_MantlePlumesWhiteboard01", "Volcano_SouthAmerica01", "Volcano_TerrainMap06", "Volcano_PlumePlacement01", "Volcano_WhereNext01", "Volcano_MainMenu01"];
-  
-  // setup progress storage
-  initProgress();
-  
-  // setup control bar
-  if (pageList.includes(pageName)){
-    setupControlBar();
-  } else {
-    next_btn =  document.getElementById("next_btn");
-    updateNext();
-  }
-  
-  //  dev mode
-  if (dev_mode){
+class MasterController {
+  dev_mode = true;
+  constructor(pageList, pageName) {
+    if (!new.target) {
+      return new MasterController();
+    }
+    this.pageList = pageList;
+    this.pageName = pageName;
+    this.next_btn;
+    this.done_flags = [];
+    this.idx;
+    if (this.pageList.includes(pageName)) {
+      setupControlBar();
+    }
+    else {
+      this.next_btn = document.getElementById("next_btn");
+      updateNext();
+    }
+    //  dev mode
+    if (dev_mode) {
       pageDone();
+    }
   }
-}
-
-
-// create back and next buttons
-function setupControlBar(){
-  idx = pageList.indexOf(pageName);
-  
-  // first page doesn't need back button
-  if (idx>0){
-    var back_btn = document.createElement('BUTTON');
-    back_btn.className = 'btn btn-dark';
-    back_btn.onclick = function () { 
+  init() {
+      if (localStorage.getItem("initProgress") != "true") {
+        alert("DEV MESSAGE PLEASE IGNORE - Setting up progress storage.");
+        for (var i = 0; i < this.pageList.length; i++) {
+          localStorage.setItem(this.pageList[i], "false");
+        }
+        // special cases
+        localStorage.setItem("TerrainMap03", "false");
+        localStorage.setItem("PlumePlacement01", "false");
+        localStorage.setItem("initProgress", "true");
+      }
+    }
+    // create back and next buttons
+  setupControlBar() {
+    this.idx = this.pageList.indexOf(pageName);
+    // first page doesn't need back button
+    if (this.idx > 0) {
+      var back_btn = document.createElement('BUTTON');
+      back_btn.className = 'btn btn-dark';
+      back_btn.onclick = function () {
         back();
-    };
-    var icon = document.createElement("i");
-    icon.className = "material-icons";
-    icon.innerHTML = "arrow_back";
-    back_btn.appendChild(icon);
-    document.getElementById("nav_control").appendChild(back_btn);
-  }  
-  
-  // last page doesn't need next button
-  if (idx<pageList.length-1){  
-    next_btn = document.createElement('BUTTON');
-    next_btn.className = 'btn btn-dark';
-    next_btn.style = "margin-left: 5px;";
-    // Button disabled until user done with page
-    if (localStorage.getItem(pageName) == "false" && !dev_mode){
-      next_btn.disabled = true; 
+      };
+      var icon = document.createElement("i");
+      icon.className = "material-icons";
+      icon.innerHTML = "arrow_back";
+      back_btn.appendChild(icon);
+      document.getElementById("nav_control").appendChild(back_btn);
     }
-    next_btn.onclick = function () { 
+    // last page doesn't need next button
+    if (this.idx < this.pageList.length - 1) {
+      var btn = document.createElement('BUTTON');
+      btn.className = 'btn btn-dark';
+      btn.style = "margin-left: 5px;";
+      // Button disabled until user done with page
+      if (localStorage.getItem(pageName) == "false" && !dev_mode) {
+        btn.disabled = true;
+      }
+      btn.onclick = function () {
         next();
-    };
-    var icon = document.createElement("i");
-    icon.className = "material-icons";
-    icon.innerHTML = "arrow_forward";
-    next_btn.appendChild(icon);
-    document.getElementById("nav_control").appendChild(next_btn);
+      };
+      var icon = document.createElement("i");
+      icon.className = "material-icons";
+      icon.innerHTML = "arrow_forward";
+      btn.appendChild(icon);
+      this.next_btn = btn;
+      document.getElementById("nav_control").appendChild(this.next_btn);
+    }
   }
+     updateNext() {
+      if (localStorage.getItem(pageName) != "true" && !this.dev_mode) {
+        this.next_btn.disabled = true;
+      }
+    }
+    // go to next page
+    next() {
+      movePage(1);
+    }
+    // go to previous page
+    back() {
+      movePage(-1);
+    }
+    movePage(shift) {
+      window.location.href = "/" + document.location.pathname.split("/").splice(1)[0] + "/" + this.pageList[this.idx + shift] + ".html";
+    }
+    //
+    addFlag(name) {
+      this.done_flags[name] = false;
+      console.log("Adding flag of type: " + name);
+    }
+
+    flagDone(name) {
+      console.log("Flag of type: " + name + " done");
+      this.done_flags[name] = true;
+      var keys = Object.keys(this.done_flags);
+      for (var i = 0; i < keys.length; i++) {
+        if (this.done_flags[keys[i]] == false) {
+          return;
+        }
+      }
+      pageDone();
+    }
+
+    pageDone() {
+      if (this.idx < this.pageList.length - 1) {
+        this.next_btn.disabled = false;
+        localStorage.setItem(this.pageName, "true");
+      }
+    }
+}
+export{MasterController};
+
+
+
+
+
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+
+
+
+
+// var pageList; 
+// var next_btn;
+// var pageName;
+// var dev_mode = true;
+// var done_flags = [];
+// var idx;
+
+// init();
+
+// function init(){
+//   // grab page name
+//   // NEEDS REFACTOR
+//   pageName = grabPageName();
+//   pageName = pageName.substr(0, pageName.length-5);
   
-}
+//   // initialize page list
+//   pageList = ["Volcano_TerrainMap01", "panotour/Volcano_Exploration01", "panotour/Volcano_Exploration02", "panotour/Volcano_Exploration03", "panotour/Volcano_Exploration04", "panotour/Volcano_Exploration05", "Volcano_TerrainMap02", "Volcano_TerrainMap03", "Volcano_TerrainMap04", "Volcano_TerrainMapAges01", "Volcano_TerrainMapAges02", "Volcano_OtherVolcanoes01", "Volcano_TectonicPlateWhiteboard01", "Volcano_TectonicPlates01", "Volcano_PlatesGame01", "Volcano_TectonicPlates02", "Volcano_MantlePlumesWhiteboard01", "Volcano_SouthAmerica01", "Volcano_TerrainMap06", "Volcano_PlumePlacement01", "Volcano_WhereNext01", "Volcano_MainMenu01"];
+  
+//   // setup progress storage
+//   initProgress();
+  
+//   // setup control bar
+//   if (pageList.includes(pageName)){
+//     setupControlBar();
+//   } else {
+//     next_btn =  document.getElementById("next_btn");
+//     updateNext();
+//   }
+  
+//   //  dev mode
+//   if (dev_mode){
+//       pageDone();
+//   }
+// }
 
-function grabPageName(){
-  var path = window.location.pathname;
-  path = path.split("/").splice(-2);
-  if (path[0] != "volcano"){
-    return path.join('/');
-  } else {
-    return path[1];
-  }
-}
 
-// go to next page
-function next(){
-  movePage(1);
-}
+// // create back and next buttons
+// function setupControlBar(){
+//   idx = pageList.indexOf(pageName);
+  
+//   // first page doesn't need back button
+//   if (idx>0){
+//     var back_btn = document.createElement('BUTTON');
+//     back_btn.className = 'btn btn-dark';
+//     back_btn.onclick = function () { 
+//         back();
+//     };
+//     var icon = document.createElement("i");
+//     icon.className = "material-icons";
+//     icon.innerHTML = "arrow_back";
+//     back_btn.appendChild(icon);
+//     document.getElementById("nav_control").appendChild(back_btn);
+//   }  
+  
+//   // last page doesn't need next button
+//   if (idx<pageList.length-1){  
+//     next_btn = document.createElement('BUTTON');
+//     next_btn.className = 'btn btn-dark';
+//     next_btn.style = "margin-left: 5px;";
+//     // Button disabled until user done with page
+//     if (localStorage.getItem(pageName) == "false" && !dev_mode){
+//       next_btn.disabled = true; 
+//     }
+//     next_btn.onclick = function () { 
+//         next();
+//     };
+//     var icon = document.createElement("i");
+//     icon.className = "material-icons";
+//     icon.innerHTML = "arrow_forward";
+//     next_btn.appendChild(icon);
+//     document.getElementById("nav_control").appendChild(next_btn);
+//   }
+  
+// }
 
-// go to previous page
-function back(){
-  movePage(-1);
-}
+// function grabPageName(){
+//   var path = window.location.pathname;
+//   path = path.split("/").splice(-2);
+//   if (path[0] != "volcano"){
+//     return path.join('/');
+//   } else {
+//     return path[1];
+//   }
+// }
 
-function movePage(shift){
-  window.location.href = "/" + document.location.pathname.split("/").splice(1)[0] + "/" + pageList[idx+shift] + ".html";
-}
+// // go to next page
+// function next(){
+//   movePage(1);
+// }
 
-function pageDone(type){
-  if(idx<pageList.length-1){
-    next_btn.disabled = false;
-    localStorage.setItem(pageName, "true"); 
-  }
-}
+// // go to previous page
+// function back(){
+//   movePage(-1);
+// }
 
-function initProgress(){
-  if (localStorage.getItem("initProgress") != "true"){
-    alert("DEV MESSAGE PLEASE IGNORE - Setting up progress storage.");
-    for (var i = 0; i < pageList.length; i++){
-      localStorage.setItem(pageList[i], "false");
-    }
-    // special cases
-    localStorage.setItem("TerrainMap03", "false");
-    localStorage.setItem("PlumePlacement01", "false");
-    localStorage.setItem("initProgress", "true");
-  }
-}
+// function movePage(shift){
+//   window.location.href = "/" + document.location.pathname.split("/").splice(1)[0] + "/" + pageList[idx+shift] + ".html";
+// }
 
-function updateNext(){
-  if (localStorage.getItem(pageName) != "true" && !dev_mode){
-    next_btn.disabled = true; 
-  }
-}
+// function pageDone(type){
+//   if(idx<pageList.length-1){
+//     next_btn.disabled = false;
+//     localStorage.setItem(pageName, "true"); 
+//   }
+// }
 
-function addFlag(name){
-  done_flags[name] = false;
-  console.log("Adding flag of type: " + name);
-}
+// function initProgress(){
+//   if (localStorage.getItem("initProgress") != "true"){
+//     alert("DEV MESSAGE PLEASE IGNORE - Setting up progress storage.");
+//     for (var i = 0; i < pageList.length; i++){
+//       localStorage.setItem(pageList[i], "false");
+//     }
+//     // special cases
+//     localStorage.setItem("TerrainMap03", "false");
+//     localStorage.setItem("PlumePlacement01", "false");
+//     localStorage.setItem("initProgress", "true");
+//   }
+// }
 
-function flagDone(name){
-  console.log("Flag of type: " + name + " done");
-  done_flags[name] = true;
-  var keys = Object.keys(done_flags);
-  for (var i = 0; i < keys.length ; i++) { 
-    if (done_flags[keys[i]] == false){
-      return;
-    }
-  }
-  pageDone();
-}
+// function updateNext(){
+//   if (localStorage.getItem(pageName) != "true" && !dev_mode){
+//     next_btn.disabled = true; 
+//   }
+// }
+
+// function addFlag(name){
+//   done_flags[name] = false;
+//   console.log("Adding flag of type: " + name);
+// }
+
+// function flagDone(name){
+//   console.log("Flag of type: " + name + " done");
+//   done_flags[name] = true;
+//   var keys = Object.keys(done_flags);
+//   for (var i = 0; i < keys.length ; i++) { 
+//     if (done_flags[keys[i]] == false){
+//       return;
+//     }
+//   }
+//   pageDone();
+// }
