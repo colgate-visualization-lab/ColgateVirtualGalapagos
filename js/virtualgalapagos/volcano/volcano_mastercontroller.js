953c2cdcad801f12c8825242c27bec9b6bfb47b3
dgathogo <edgetech26@gmail.com>
2019-05-29 15:17:38 -0400

    Fix mastercontroller
    Former-commit-id: 46aa0c1a0e1041bb45ec2f1ba06859f2e209101b [formerly 1f20b0b005ad1d8a14f4b2565e64e49094bff39b] [formerly bba0772516d3178ccefaae7f4795c608d6a9b7f4 [formerly d1fe5fd3fcbdb9a6a34261cf9096569a39959fb6]]
    Former-commit-id: cea4ffb92a8c2cc4e69e9b6585e0fb8a76693b5f [formerly 62c5165e82cd64e78cbe631c4100d850dcf1a53c]
    Former-commit-id: bef7c7d98ff9c9180f6af105540b203a804ba49d

--- js/virtualgalapagos/volcano/volcano_mastercontroller.js
+++ js/virtualgalapagos/volcano/volcano_mastercontroller.js
@@ -1,31 +1,32 @@
 "use strict"
 
-import {MasterController} from '../mastercontroller';
-import {VolcanoAudioPlayer} from './volcano_audiocontroller';
+import {MasterController} from '../mastercontroller.js';
+import {VolcanoAudioPlayer} from './volcano_audiocontroller.js';
 
 class VolcanoMasterController extends MasterController {
-    constructor(){
-        super();
-        this.pageName = grabPageName();
-        this.pageList = ["Volcano_TerrainMap01", "panotour/Volcano_Exploration01", "panotour/Volcano_Exploration02", "panotour/Volcano_Exploration03", "panotour/Volcano_Exploration04", "panotour/Volcano_Exploration05", "Volcano_TerrainMap02", "Volcano_TerrainMap03", "Volcano_TerrainMap04", "Volcano_TerrainMapAges01", "Volcano_TerrainMapAges02", "Volcano_OtherVolcanoes01", "Volcano_TectonicPlateWhiteboard01", "Volcano_TectonicPlates01", "Volcano_PlatesGame01", "Volcano_TectonicPlates02", "Volcano_MantlePlumesWhiteboard01", "Volcano_SouthAmerica01", "Volcano_TerrainMap06", "Volcano_PlumePlacement01", "Volcano_WhereNext01", "Volcano_MainMenu01"];
-
+    constructor(pageList, pageName){
+      super(pageList,pageName);
     }
+
     static grabPageName(){
         var path = window.location.pathname;
         path = path.split("/").splice(-2);
         if (path[0] != "volcano"){
-          return path.join('/');
+          throw "This is not in volcano module";
         } else {
-          return path[1];
+          path = path[1].split(".");
+          return path[0];
         }
       }
 }
 
 // initialize page list
+var pageName = VolcanoMasterController.grabPageName();
+var pageList = ["Volcano_TerrainMap01", "panotour/Volcano_Exploration01", "panotour/Volcano_Exploration02", "panotour/Volcano_Exploration03", "panotour/Volcano_Exploration04", "panotour/Volcano_Exploration05", "Volcano_TerrainMap02", "Volcano_TerrainMap03", "Volcano_TerrainMap04", "Volcano_TerrainMapAges01", "Volcano_TerrainMapAges02", "Volcano_OtherVolcanoes01", "Volcano_TectonicPlateWhiteboard01", "Volcano_TectonicPlates01", "Volcano_PlatesGame01", "Volcano_TectonicPlates02", "Volcano_MantlePlumesWhiteboard01", "Volcano_SouthAmerica01", "Volcano_TerrainMap06", "Volcano_PlumePlacement01", "Volcano_WhereNext01", "Volcano_MainMenu01"];
 
 
 var controller = new VolcanoMasterController(pageList, pageName);
 controller.init();
-controller.start();
+// controller.start();
 var audiocontroller = new VolcanoAudioPlayer(controller); 
 audiocontroller.start();
