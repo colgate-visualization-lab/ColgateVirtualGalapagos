984f191ab4613b494dbdb8f5662199f39a9c47cc
dgathogo <edgetech26@gmail.com>
2019-05-30 15:15:09 -0400

    Centralize entry point of all js in pages

---
+++ js/virtualgalapagos/volcano/volcano_dragdrop.js
@@ -0,0 +1,11 @@
+'use strict'
+
+
+import {DragDrop} from '../drag_drop.js'
+
+class VolcanoDragDrop extends DragDrop{
+    constructor(ddDict, mastercontroller){
+        super(ddDict,mastercontroller);
+    }
+}
+export {VolcanoDragDrop}
