b33a4d0a3c5dfc74a3f5942ba084aa52d82f8b69
dgathogo <edgetech26@gmail.com>
2019-06-11 15:04:03 -0400

    enforce style guides

--- js/virtualgalapagos/mastercontroller.js
+++ js/virtualgalapagos/mastercontroller.js
@@ -1,126 +1,125 @@
-"use strict"
+'use strict'
 
 class MasterController {
-  constructor(pageList, pageName) {
+  constructor (pageList, pageName) {
     if (!new.target) {
-      return new MasterController();
+      return new MasterController()
     }
-    this.dev_mode = true;
-    this.pageList = pageList;
-    this.pageName = pageName;
-    this.next_btn;
-    this.done_flags = [];
-    this.idx;
+    this.dev_mode = true
+    this.pageList = pageList
+    this.pageName = pageName
+    this.nextBtn = null
+    this.done_flags = []
+    this.idx = -1
     if (this.pageList.includes(pageName)) {
-      this.setupControlBar();
-    }
-    else {
-      this.next_btn = document.getElementById("next_btn");
-      this.updateNext();
+      this.setupControlBar()
+    } else {
+      this.nextBtn = document.getElementById('nextBtn')
+      this.updateNext()
     }
     //  dev mode
     if (this.dev_mode) {
-      this.pageDone();
+      this.pageDone()
     }
   }
 
-  init() {
-      if (MasterController.getItem("initProgress") != "true") {
-        alert("DEV MESSAGE PLEASE IGNORE - Setting up progress storage.");
-        for (var i = 0; i < this.pageList.length; i++) {
-          MasterController.storeItem(this.pageList[i], "false");
-        }
-        // special cases
-        MasterController.storeItem("TerrainMap03", "false");
-        MasterController.storeItem("PlumePlacement01", "false");
-        MasterController.storeItem("initProgress", "true");
+  init () {
+    if (MasterController.getItem('initProgress') != 'true') {
+      alert('DEV MESSAGE PLEASE IGNORE - Setting up progress storage.')
+      for (var i = 0; i < this.pageList.length; i++) {
+        MasterController.storeItem(this.pageList[i], 'false')
       }
+      // special cases
+      MasterController.storeItem('TerrainMap03', 'false')
+      MasterController.storeItem('PlumePlacement01', 'false')
+      MasterController.storeItem('initProgress', 'true')
     }
+  }
 
-    // create back and next buttons
-  setupControlBar() {
-    var controllerObj = this;
-    this.idx = this.pageList.indexOf(this.pageName);
+  // create back and next buttons
+  setupControlBar () {
+    var controllerObj = this
+    this.idx = this.pageList.indexOf(this.pageName)
     // first page doesn't need back button
     if (this.idx > 0) {
-      var back_btn = document.createElement('BUTTON');
-      back_btn.className = 'btn btn-dark';
-      back_btn.onclick = function () {
-        MasterController.back(controllerObj);
-      };
-      var icon = document.createElement("i");
-      icon.className = "material-icons";
-      icon.innerHTML = "arrow_back";
-      back_btn.appendChild(icon);
-      document.getElementById("nav_control").appendChild(back_btn);
+      var backBtn = document.createElement('BUTTON')
+      backBtn.className = 'btn btn-dark'
+      backBtn.onclick = function () {
+        MasterController.back(controllerObj)
+      }
+      var icon = document.createElement('i')
+      icon.className = 'material-icons'
+      icon.innerHTML = 'arrow_back'
+      backBtn.appendChild(icon)
+      document.getElementById('nav_control').appendChild(backBtn)
     }
     // last page doesn't need next button
     if (this.idx < this.pageList.length - 1) {
-      var btn = document.createElement('BUTTON');
-      btn.className = 'btn btn-dark';
-      btn.style = "margin-left: 5px;";
+      var btn = document.createElement('BUTTON')
+      btn.className = 'btn btn-dark'
+      btn.style = 'margin-left: 5px;'
       // Button disabled until user done with page
-      if (MasterController.getItem(this.pageName) == "false" && !this.dev_mode) {
-        btn.disabled = true;
+      if (MasterController.getItem(this.pageName) === 'false' && !this.dev_mode) {
+        btn.disabled = true
       }
       btn.onclick = function () {
-        MasterController.next(controllerObj);
-      };
-      var icon = document.createElement("i");
-      icon.className = "material-icons";
-      icon.innerHTML = "arrow_forward";
-      btn.appendChild(icon);
-      this.next_btn = btn;
-      document.getElementById("nav_control").appendChild(this.next_btn);
+        MasterController.next(controllerObj)
+      }
+      icon = document.createElement('i')
+      icon.className = 'material-icons'
+      icon.innerHTML = 'arrow_forward'
+      btn.appendChild(icon)
+      this.nextBtn = btn
+      document.getElementById('nav_control').appendChild(this.nextBtn)
     }
   }
-  updateNext() {
-    if (MasterController.getItem(this.pageName) != "true" && !this.dev_mode) {
-      this.next_btn.disabled = true;
+  updateNext () {
+    if (MasterController.getItem(this.pageName) !== 'true' && !this.dev_mode) {
+      this.nextBtn.disabled = true
     }
   }
   // go to next page
-  static next(controllerObj) {
-    MasterController.movePage(1,controllerObj);
+  static next (controllerObj) {
+    MasterController.movePage(1, controllerObj)
   }
   // go to previous page
-  static back(controllerObj) {
-    MasterController.movePage(-1,controllerObj);
+  static back (controllerObj) {
+    MasterController.movePage(-1, controllerObj)
   }
-  static movePage(shift, controllerObj) {
-    window.location.href = "/" + document.location.pathname.split("/").splice(1)[0] + "/" + controllerObj.pageList[controllerObj.idx + shift] + ".html";
+  static movePage (shift, controllerObj) {
+    window.location.href = '/' + document.location.pathname.split('/').splice(1)[0] + '/' + controllerObj.pageList[controllerObj.idx + shift] + '.html'
   }
   //
-  addFlag(name) {
-    this.done_flags[name] = false;
-    console.log("Adding flag of type: " + name);
+  addFlag (name) {
+    this.done_flags[name] = false
+    console.log('Adding flag of type: ' + name)
   }
 
-  flagDone(name) {
-    console.log("Flag of type: " + name + " done");
-    this.done_flags[name] = true;
-    var keys = Object.keys(this.done_flags);
+  flagDone (name) {
+    console.log('Flag of type: ' + name + ' done')
+    this.done_flags[name] = true
+    var keys = Object.keys(this.done_flags)
     for (var i = 0; i < keys.length; i++) {
-      if (this.done_flags[keys[i]] == false) {
-        return;
+      if (this.done_flags[keys[i]] === false) {
+        return
       }
     }
-    this.pageDone();
+    this.pageDone()
   }
 
-  pageDone() {
+  pageDone () {
     if (this.idx < this.pageList.length - 1) {
-      this.next_btn.disabled = false;
-      MasterController.storeItem(this.pageName, "true");
+      this.nextBtn.disabled = false
+      MasterController.storeItem(this.pageName, 'true')
     }
   }
-  
+
   // api to persist items in storage, can be expanded to use a database etc
-  static storeItem(key, value){
-    localStorage.setItem(key, value);
+  static storeItem (key, value) {
+    localStorage.setItem(key, value)
   }
-  static getItem(key){
-    return localStorage.getItem(key);
+  static getItem (key) {
+    return localStorage.getItem(key)
   }
 }
-export{MasterController};
+export { MasterController }
