72f12c032edbc99636c4739ae59c12106017b060
dgathogo <edgetech26@gmail.com>
2019-06-11 15:04:03 -0400

    enforce style guides
    Former-commit-id: b0f6a81fbedb490c1e13ebf574c7742c94322a9c [formerly 5732901e6ac0a6444ba4427020d486603a9c65da] [formerly 5d4bcd7577604237ffd6aee0f0ca4233958bd9fc [formerly 4718b5275d72233f3500a62abe7db304c6f3cd78]]
    Former-commit-id: 67d30b3d83e27c5120c712f105f34eb6e4d73b9d [formerly 7d800ed2698d86dd13e909c60a9dbdd27357f6ac]
    Former-commit-id: fcd68563d52a8593c45ed90dfb06961618017ed5

--- js/virtualgalapagos/volcano/volcano_avcontroller.js
+++ js/virtualgalapagos/volcano/volcano_avcontroller.js
@@ -1,49 +1,48 @@
-"use strict"
+'use strict'
 
-import {AVController} from '../avcontroller.js';
+import { AVController } from '../avcontroller.js'
 
-class VolcanoAVController extends AVController{
-    constructor(player, av_type, master_controller){
-        super(player, av_type, master_controller);
-        this.src = document.getElementById("player_src");
+class VolcanoAVController extends AVController {
+  constructor (player, avType, masterController) {
+    super(player, avType, masterController)
+    this.src = document.getElementById('player_src')
+  }
+  // if no track assigned, looks for track based on filename
+  setBGTrack () {
+    var bgTrack = this.src.src
+    if (bgTrack === '') {
+      var filename = window.location.pathname
+      filename = filename.split('/').pop()
+      filename = filename.substr(0, filename.length - 5)
+      this.setTrack(filename)
     }
-    // if no track assigned, looks for track based on filename
-    setBGTrack(){
-        var bg_track = this.src.src
-        if (bg_track == ''){
-            var filename = window.location.pathname;
-            filename = filename.split("/").pop();
-            filename = filename.substr(0, filename.length-5);
-            this.setTrack(filename);
-        }
+  }
+
+  setTrack (filename) {
+    console.log('setting track to: ' + filename)
+    var prefix
+    if (VolcanoAVController.grabPageName().includes('panotour')) {
+      prefix = '../../' + this.avType + '/volcano/'
+    } else {
+      prefix = '../' + this.avType + '/volcano/'
     }
-    
-    setTrack(filename){
-        console.log("setting track to: " + filename);
-        var prefix;
-        if (VolcanoAVController.grabPageName().includes("panotour")){
-            prefix = "../../" + this.av_type + "/volcano/";
-        } else {
-            prefix = "../" + this.av_type + "/volcano/";
-        }
-        if (this.av_type === "video"){
-            this.player.src = prefix + filename + ".mp4";
-        } else if (this.av_type === "audio"){
-            this.player.src = prefix + filename + ".mp3"
-        } else {
-            throw "Source type unsupported";
-        }
+    if (this.avType === 'video') {
+      this.player.src = prefix + filename + '.mp4'
+    } else if (this.avType === 'audio') {
+      this.player.src = prefix + filename + '.mp3'
+    } else {
+      throw new Error('Source type unsupported')
     }
-    
-    static grabPageName(){
-        var path = window.location.pathname;
-        path = path.split("/").splice(-2);
-        if (path[0] != "volcano"){
-          return path.join('/');
-        } else {
-          return path[1];
-        }
+  }
+
+  static grabPageName () {
+    var path = window.location.pathname
+    path = path.split('/').splice(-2)
+    if (path[0] !== 'volcano') {
+      return path.join('/')
+    } else {
+      return path[1]
     }
+  }
 }
-export {VolcanoAVController}
-
+export { VolcanoAVController }
