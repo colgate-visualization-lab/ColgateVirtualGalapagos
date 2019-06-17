83e6de5e1b7546b30ec5aab456e3040c296a7ca1
dgathogo <edgetech26@gmail.com>
2019-06-17 11:02:44 -0400

    fix panotour
    Former-commit-id: cb269cbddf6d0b9b5b879fc78b09b03b2e0d12b7 [formerly 6f512a97081ece8b5015cdf04c704bfcbfdcae51] [formerly b4e3001f3f6dbe5299a662fbc302864e09f7ef89 [formerly 7bdcfbf227d469b6ffd0ce32d11c65dec0dd1fa4]]
    Former-commit-id: e71a75b86c18404cbe65de767fc34ccc7f78c090 [formerly b0962302862ada63aa12341f5ba8d247e8c36762]
    Former-commit-id: 68046578a9f29d130181c3c2f0323c35d1887f7b

--- js/virtualgalapagos/volcano/panotour/utils.js
+++ js/virtualgalapagos/volcano/panotour/utils.js
@@ -1,15 +1,5 @@
 'use strict'
 
-import { removepano, embedpano } from '../../../libs/krpano.js'
-var currentPage = getPageName()
-
-function getPageName () {
-  var path = window.location.pathname
-  path = path.split('/').splice(-2)
-  path = path[1].split('.')
-  return path[0]
-}
-
 function readDeviceOrientation () {
   // window.innerHeight is not supported by IE
   var winH = window.innerHeight ? window.innerHeight : jQuery(window).height()
@@ -39,79 +29,3 @@ jQuery(document).ready(function () {
     setTimeout(function () { readDeviceOrientation() }, 10)
   }
 })
-
-function accessWebVr (curScene, curTime) {
-  unloadPlayer()
-
-  setTimeout(function () { loadPlayer(true, curScene, curTime) }, 100)
-}
-
-function accessStdVr (curScene, curTime) {
-  unloadPlayer()
-
-  setTimeout(function () { loadPlayer(false, curScene, curTime) }, 100)
-}
-
-function loadPlayer (isWebVr, curScene, curTime) {
-  if (isWebVr) {
-    embedpano({
-      id: 'krpanoSWFObject',
-      xml: currentPage + 'data/' + currentPage + '_vr.xml',
-      target: 'panoDIV',
-      passQueryParameters: true,
-      bgcolor: '#000000',
-      html5: 'only+webgl',
-      focus: false,
-      vars: { skipintro: true, norotation: true, startscene: curScene, starttime: curTime }
-    })
-  } else {
-    var isBot = /bot|googlebot|crawler|spider|robot|crawling/i.test(navigator.userAgent)
-    embedpano({
-      id: 'krpanoSWFObject',
-      swf: currentPage + 'data/' + currentPage + '.swf',
-      target: 'panoDIV',
-      passQueryParameters: true,
-      bgcolor: '#000000',
-      focus: false,
-      html5: isBot ? 'always' : 'prefer',
-      vars: { startscene: curScene, starttime: curTime },
-      localfallback: 'flash'
-    })
-  }
-  // apply focus on the visit if not embedded into an iframe
-  if (top.location === self.location) {
-    kpanotour.Focus.applyFocus()
-  }
-}
-
-function unloadPlayer () {
-  if (jQuery('#krpanoSWFObject')) {
-    removepano('krpanoSWFObject')
-  }
-}
-
-// var currentPanotourPlayer = null
-//
-// function getCurrentTourPlayer () {
-//   if (currentPanotourPlayer == null) {
-//     currentPanotourPlayer = document.getElementById('krpanoSWFObject')
-//   }
-//   return currentPanotourPlayer
-// }
-
-function isVRModeRequested () {
-  var querystr = window.location.search.substring(1)
-  var params = querystr.split('&')
-  for (var i = 0; i < params.length; i++) {
-    if (params[i].toLowerCase() === 'vr') {
-      return true
-    }
-  }
-  return false
-}
-
-if (isVRModeRequested()) {
-  accessWebVr()
-} else {
-  accessStdVr()
-}
