df4a19848dd9dcd132e7fa2bcf9d6167d4a29598
dgathogo <edgetech26@gmail.com>
2019-06-18 16:12:21 -0400

    unused files

--- js/libs/vrview_test.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener('load', onVrViewLoad)
-
-function onVrViewLoad () {
-  // Selector '#vrview' finds element with id 'vrview'.
-  var vrView = new VRView.Player('#vrview', {
-    video: '360/coral.jpg',
-    is_stereo: false
-  })
-}
