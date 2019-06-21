4112fe29bdb279b4c8fd7796221400c6418cf5dd
dgathogo <edgetech26@gmail.com>
2019-06-21 11:55:08 -0400

    image maps

--- js/virtualgalapagos/volcano/volcano_mastercontroller.js
+++ js/virtualgalapagos/volcano/volcano_mastercontroller.js
@@ -74,7 +74,7 @@ var ddList = [
     ddDict: {
       '7000001': ['Fernandina', 'Isabela'],
       '7000002': ['Fernandina', 'Isabela'],
-      '1700000': ['Santa Fe'],
+      '1700000': ['Santiago'],
       '2300000': ['Santa Cruz'],
       '3300000': ['Espanola'],
       '4000000': ['San Cristobal']
