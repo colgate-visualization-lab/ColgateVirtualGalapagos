89d9771968edaf90b86cda3c759fe6c94764abda
dgathogo <edgetech26@gmail.com>
2019-06-27 15:44:35 -0400

    Fieldbook
    Former-commit-id: 0ae4bac94790d80943670c404676fb785e7c81ed [formerly faff488dc173108d47afb8e52d40519032c28cee] [formerly 29396089a4df0257c6c4b1e60cb7827ac9c6533a [formerly a5bf8817b8eefd36a76b52d31de68fa8bdafd073]]
    Former-commit-id: 5c222c5c6a696b4d62c51600d1d17c949dcf4bb8 [formerly e25f5e2dfdc9cc1fe0b25ff6764fbe3a8f1145fc]
    Former-commit-id: 4501302dfb6735863ce297ef2017a88ece8141f6

--- js/virtualgalapagos/iguana/iguana_fieldbook.js
+++ js/virtualgalapagos/iguana/iguana_fieldbook.js
@@ -6,149 +6,95 @@ class IguanaFieldbook extends Fieldbook {
   // layout changes happen here, and in the future needs a system for handling multiple different fb layouts
   populateDivs () {
     var currentDiv = this.divArray[0]
-    var innerHeader = document.createTextNode('Table of Contents')
-    // innerHeader.id = 'inner_div_header' + i;
-    currentDiv.appendChild(innerHeader)
+    var innerHeader = document.createTextNode('Volcano')
     var textArea = document.createElement('textarea')
     textArea.className = 'fieldbook_ta'
     textArea.rows = 1
     textArea.cols = 1
-    Fieldbook.addSubtitle(currentDiv, 'General')
-    Fieldbook.addSubtitle(currentDiv, 'Volcano')
-    Fieldbook.addSubtitle(currentDiv, 'Iguana')
-    Fieldbook.addSubtitle(currentDiv, 'Island Life Cycle')
-    Fieldbook.addSubtitle(currentDiv, 'Eruption')
-    Fieldbook.addSubtitle(currentDiv, 'Wildlife')
-    Fieldbook.addSubtitle(currentDiv, 'Darwin and Finches')
-    Fieldbook.addSubtitle(currentDiv, 'Ocean Currents')
-    Fieldbook.addSubtitle(currentDiv, 'CDF and CDRS')
-    Fieldbook.addSubtitle(currentDiv, 'Dynamic Island')
-    Fieldbook.addSubtitle(currentDiv, 'Pink Iguana?')
+
+    // case for page 1
+    currentDiv.appendChild(innerHeader)
+    Fieldbook.addLink(currentDiv, 'General Notes', 1, this)
+    Fieldbook.addLink(currentDiv, 'How they were carried', 2, this)
+    Fieldbook.addLink(currentDiv, 'Pumice', 3, this)
+    Fieldbook.addLink(currentDiv, 'Marine vs. Land Iguana', 4, this)
+    Fieldbook.addLink(currentDiv, 'Phylogenetic Tree', 5, this)
+    Fieldbook.addLink(currentDiv, 'Green vs. Marine Iguana', 6, this)
+    Fieldbook.addLink(currentDiv, 'Hypothesis', 7, this)
 
     // case for page 2
     currentDiv = this.divArray[1]
-    innerHeader = document.createTextNode('Volcano')
-    // innerHeader.id = 'inner_div_header' + i;
+    innerHeader = document.createTextNode('General Notes')
     currentDiv.appendChild(innerHeader)
-    Fieldbook.addLink(currentDiv, 'Sierra Negra Caldera', 2, this)
-    Fieldbook.addLink(currentDiv, 'Coastal Isabela', 3, this)
-    Fieldbook.addLink(currentDiv, 'Santa Cruz', 4, this)
-    Fieldbook.addLink(currentDiv, 'Plazas', 5, this)
-    Fieldbook.addLink(currentDiv, 'Galapagos Map', 6, this)
-    Fieldbook.addLink(currentDiv, 'Santa Cruz vs. Fernandina', 7, this)
-    Fieldbook.addLink(currentDiv, 'Hypothesis', 8, this)
-    Fieldbook.addLink(currentDiv, 'Volcano Comparisons', 9, this)
-    Fieldbook.addLink(currentDiv, 'Tectonic Plates', 10, this)
+    Fieldbook.addSubtitle(currentDiv, 'OBSERVATIONS')
+    currentDiv.appendChild(textArea.cloneNode(true))
 
     // case for page 3
     currentDiv = this.divArray[2]
-    innerHeader = document.createTextNode('Sierra Negra Caldera')
-    // innerHeader.id = 'inner_div_header' + i;
+    innerHeader = document.createTextNode('How they were carried')
     currentDiv.appendChild(innerHeader)
     Fieldbook.addSubtitle(currentDiv, 'OBSERVATIONS')
     currentDiv.appendChild(textArea.cloneNode(true))
 
     // case for page 4
     currentDiv = this.divArray[3]
-    innerHeader = document.createTextNode('Coastal Isabela')
-    // innerHeader.id = 'inner_div_header' + i;
+    innerHeader = document.createTextNode('Pumice')
     currentDiv.appendChild(innerHeader)
     Fieldbook.addSubtitle(currentDiv, 'OBSERVATIONS')
     currentDiv.appendChild(textArea.cloneNode(true))
 
     // case for page 5
     currentDiv = this.divArray[4]
-    innerHeader = document.createTextNode('Santa Cruz')
-    // innerHeader.id = 'inner_div_header' + i;
-    currentDiv.appendChild(innerHeader)
-    Fieldbook.addSubtitle(currentDiv, 'OBSERVATIONS')
-    currentDiv.appendChild(textArea.cloneNode(true))
-
-    // case for page 6
-    currentDiv = this.divArray[5]
-    innerHeader = document.createTextNode('Plazas')
-    // innerHeader.id = 'inner_div_header' + i;
-    currentDiv.appendChild(innerHeader)
-    Fieldbook.addSubtitle(currentDiv, 'OBSERVATIONS')
-    currentDiv.appendChild(textArea.cloneNode(true))
-
-    // case for page 7
-    currentDiv = this.divArray[6]
-    innerHeader = document.createTextNode('Galapagos Map')
-    // innerHeader.id = 'inner_div_header' + i;
-    currentDiv.appendChild(innerHeader)
-    Fieldbook.addSubtitle(currentDiv, 'OBSERVATIONS')
-    currentDiv.appendChild(textArea.cloneNode(true))
-
-    // case for page 8
-    currentDiv = this.divArray[7]
-    innerHeader = document.createTextNode('Santa Cruz vs. Fernandina')
-    // innerHeader.id = 'inner_div_header' + i;
+    innerHeader = document.createTextNode('Marine vs. Land Iguana')
     currentDiv.appendChild(innerHeader)
     Fieldbook.addSubtitle(currentDiv, 'OBSERVATIONS')
     var rowDiv = document.createElement('DIV')
     rowDiv.className = 'row'
     var columnDiv = document.createElement('DIV')
     columnDiv.className = 'col'
-    Fieldbook.addSubtitle(columnDiv, 'Fernandina')
+    Fieldbook.addSubtitle(columnDiv, 'Marine Iguana')
     columnDiv.appendChild(textArea.cloneNode(true))
     rowDiv.appendChild(columnDiv)
     columnDiv = document.createElement('DIV')
     columnDiv.className = 'col'
-    Fieldbook.addSubtitle(columnDiv, 'Santa Cruz')
+    Fieldbook.addSubtitle(columnDiv, 'Land Iguana')
     columnDiv.appendChild(textArea.cloneNode(true))
     rowDiv.appendChild(columnDiv)
     currentDiv.appendChild(rowDiv)
 
-    // case for page 9
-    currentDiv = this.divArray[8]
-    innerHeader = document.createTextNode('Hypothesis')
-    // innerHeader.id = 'inner_div_header' + i;
+    // case for page 6
+    currentDiv = this.divArray[5]
+    innerHeader = document.createTextNode('Phylogenetic Tree')
     currentDiv.appendChild(innerHeader)
-    Fieldbook.addSubtitle(currentDiv, 'First Draft of Hypothesis')
-    currentDiv.appendChild(textArea.cloneNode(true))
-    Fieldbook.addSubtitle(currentDiv, 'Second Draft of Hypothesis')
+    Fieldbook.addSubtitle(currentDiv, 'OBSERVATIONS')
     currentDiv.appendChild(textArea.cloneNode(true))
 
-    // case for page 10
-    currentDiv = this.divArray[9]
-    innerHeader = document.createTextNode('Volcano Comparisons')
-    // innerHeader.id = 'inner_div_header' + i;
+    // case for page 7
+    currentDiv = this.divArray[6]
+    innerHeader = document.createTextNode('Green vs. Marine Iguana')
     currentDiv.appendChild(innerHeader)
     Fieldbook.addSubtitle(currentDiv, 'OBSERVATIONS')
     rowDiv = document.createElement('DIV')
     rowDiv.className = 'row'
     columnDiv = document.createElement('DIV')
     columnDiv.className = 'col'
-    Fieldbook.addSubtitle(columnDiv, 'Galapagos')
+    Fieldbook.addSubtitle(columnDiv, 'Green Iguana')
     columnDiv.appendChild(textArea.cloneNode(true))
     rowDiv.appendChild(columnDiv)
     columnDiv = document.createElement('DIV')
     columnDiv.className = 'col'
-    Fieldbook.addSubtitle(columnDiv, 'Hawaii')
-    columnDiv.appendChild(textArea.cloneNode(true))
-    rowDiv.appendChild(columnDiv)
-    currentDiv.appendChild(rowDiv)
-    rowDiv = document.createElement('DIV')
-    rowDiv.className = 'row'
-    columnDiv = document.createElement('DIV')
-    columnDiv.className = 'col'
-    Fieldbook.addSubtitle(columnDiv, 'Japan')
-    columnDiv.appendChild(textArea.cloneNode(true))
-    rowDiv.appendChild(columnDiv)
-    columnDiv = document.createElement('DIV')
-    columnDiv.className = 'col'
-    Fieldbook.addSubtitle(columnDiv, 'Central America')
+    Fieldbook.addSubtitle(columnDiv, 'Marine Iguana')
     columnDiv.appendChild(textArea.cloneNode(true))
     rowDiv.appendChild(columnDiv)
     currentDiv.appendChild(rowDiv)
 
-    // case for page 11
-    currentDiv = this.divArray[10]
-    innerHeader = document.createTextNode('Tectonic Plates')
-    // innerHeader.id = 'inner_div_header' + i;
+    // case for page 8
+    currentDiv = this.divArray[7]
+    innerHeader = document.createTextNode('Hypothesis')
     currentDiv.appendChild(innerHeader)
-    Fieldbook.addSubtitle(currentDiv, 'Observations')
+    Fieldbook.addSubtitle(currentDiv, 'First Draft of Hypothesis')
+    currentDiv.appendChild(textArea.cloneNode(true))
+    Fieldbook.addSubtitle(currentDiv, 'Second Draft of Hypothesis')
     currentDiv.appendChild(textArea.cloneNode(true))
   }
 }
