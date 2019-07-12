f2ad8cea1d4bc598ad2900cf8931bcf689eb4388
dgathogo <edgetech26@gmail.com>
2019-07-12 14:45:55 -0400

    add videos

--- js/virtualgalapagos/iguana/iguana_mastercontroller.js
+++ js/virtualgalapagos/iguana/iguana_mastercontroller.js
@@ -10,7 +10,6 @@
 import { MasterController } from '../mastercontroller.js'
 import { overlay } from '../overlay.js'
 import { IguanaAVController } from './iguana_avcontroller.js'
-import { IguanaDragDrop } from './iguana_dragdrop.js'
 import { IguanaFieldbook } from './iguana_fieldbook.js'
 import { IguanaToolbar } from './iguana_toolbar.js'
 import { evolution } from './Iguana_Evolution03.js'
@@ -44,7 +43,7 @@ class IguanaMasterController extends MasterController {
 var pageName = IguanaMasterController.grabPageName()
 
 // List of pages in module in order
-var pageList = ['panotour/Iguana_Endemic01', 'Iguana_Carried01', 'Iguana_Carried02', 'panotour/Iguana_Pumice01', 'Iguana_Pumice02', 'panotour/Iguana_Comparison01', 'panotour/Iguana_Comparison02', 'Iguana_Comparison03', 'panotour/Iguana_PhyloTree01', 'Iguana_PhyloTree02', 'Iguana_PhyloTree05', 'panotour/Iguana_Evolution01', 'Iguana_Evolution02', 'Iguana_Evolution03', 'panotour/Iguana_Evolution04', 'Iguana_Evolution05', 'panotour/Iguana_Scales01', 'panotour/Iguana_Scales02', 'panotour/Iguana_Scales03', 'Iguana_PhyloTree06', 'panotour/Iguana_PinkIggy01']
+var pageList = ['panotour/Iguana_Endemic01', 'Iguana_Carried01', 'Iguana_Carried02', 'panotour/Iguana_Pumice01', 'Iguana_Pumice02', 'panotour/Iguana_Comparison01', 'panotour/Iguana_Comparison02', 'Iguana_Comparison03', 'panotour/Iguana_PhyloTree01', 'Iguana_PhyloTree02', 'Iguana_PhyloTree05', 'panotour/Iguana_Evolution01', 'Iguana_Evolution02', 'Iguana_Evolution03', 'panotour/Iguana_Evolution04', 'Iguana_Evolution05', 'panotour/Iguana_Scales01', 'panotour/Iguana_Scales02', 'panotour/Iguana_Scales03', 'panotour/Iguana_PhyloTree06', 'panotour/Iguana_PinkIggy01']
 // Pages and associated fieldbook pages
 var fbPages = {
   'Iguana_Carried02': 2,
@@ -60,30 +59,6 @@ var fbPages = {
   'panotour/Iguana_Scales03': 7
 }
 
-// Drag and drop pages and associated dictionaries
-var ddList = [
-//   {
-//     name: 'Volcano_TerrainMapAges01',
-//     ddDict: {
-//       '7000001': ['Fernandina', 'Isabela'],
-//       '7000002': ['Fernandina', 'Isabela'],
-//       '1700000': ['Santiago'],
-//       '2300000': ['Santa Cruz'],
-//       '3300000': ['Espanola'],
-//       '4000000': ['San Cristobal']
-//     }
-//   },
-//   {
-//     name: 'Volcano_PlatesGame01',
-//     ddDict: {
-//       'convergent1': ['img_convergent1', 'img_convergent2'],
-//       'convergent2': ['img_convergent1', 'img_convergent2'],
-//       'divergent1': ['img_divergent1'],
-//       'transform1': ['img_transform1']
-//     }
-//   }
-]
-
 // sidebar navigation
 var toolbarLinks = []
 for (var i = 0; i < pageList.length; i++) {
@@ -93,7 +68,7 @@ for (var i = 0; i < pageList.length; i++) {
 }
 
 // whiteboard videos
-var videolist = ['Iguana_Pumice02', 'Iguana_PhyloTree02', 'Iguana_PhyloTree05', 'Iguana_Evolution02', 'Iguana_PhyloTree06']
+var videolist = ['Iguana_Pumice02', 'Iguana_PhyloTree02', 'Iguana_Evolution02']
 
 // init controllers
 var masterController = new IguanaMasterController(pageList, pageName)
@@ -131,14 +106,6 @@ var fbController = new IguanaFieldbook(fbPages, 8, masterController)
 fbController.init()
 fbController.populateDivs()
 fbController.start(currentPage)
-
-// Dragdrop controller
-var ddObj = ddList.filter(ddList => (ddList.name === pageName))
-if (ddObj.length !== 0) {
-  var dragdrop = new IguanaDragDrop(ddObj[0].ddDict, masterController)
-  dragdrop.initDrops()
-  dragdrop.initDrags()
-}
 
 if (pageName === 'Iguana_Evolution03') {
   evolution(masterController, avController)
