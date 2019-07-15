a8dabdb029935ee239f03df49613fb86a2f53247
dgathogo <edgetech26@gmail.com>
2019-07-15 16:02:21 -0400

    homepage
    Former-commit-id: a0fe84cf8581002b7bcfcb38cad72b8e178cf5b7 [formerly 65c78aea806b0322726e9b5bf31a23a9a01d6a21] [formerly 6a0ea6b8dc95170647282cbb52687e2579229d70 [formerly 71db58eeac6dcab4bd402fbc8fd7b0220f839802]]
    Former-commit-id: a1148cbb8aba0a39a40997819878df0da6da4691 [formerly 350613479af7e3f554bbf5f68ad034d97a30a664]
    Former-commit-id: 58aafcbe343a58b601b764ddde4c8306d2a1223f

--- js/virtualgalapagos/index.js
+++ js/virtualgalapagos/index.js
@@ -1,25 +1,28 @@
 'use strict'
 
-import { Toolbar } from './toolbar.js'
-import { MasterController } from './mastercontroller.js'
-
 function init () {
-  var masterController = new MasterController([], '', '')
-  var toolbarLinks = []
-  var modules
+  function locationHashChanged () {
+    $(location.hash).tab('show')
+  }
+
+  window.onhashchange = locationHashChanged
+  // location.reload(true)
+
   var moduleMappings = {
     'volcano': 'volcano/Volcano_TerrainMap01.html',
     'iguana': 'iguana/panotour/Iguana_Endemic01.html'
   }
-  var toolbar = new Toolbar(toolbarLinks, masterController)
-  toolbar.initToolbar()
-  function getToolbarLinks (modules) {
+
+  function getLinks (modules) {
+    var links = []
     modules.forEach(element => {
-      toolbarLinks.push({ text: element, url: moduleMappings[element] })
+      links.push({ text: element, url: moduleMappings[element] })
     })
+    return links
   }
-  
+
   function getModules (mystery) {
+    var modules
     switch (mystery) {
       case 'mystery1':
         modules = ['volcano', 'iguana']
@@ -30,15 +33,47 @@ function init () {
       default:
         modules = []
     }
-    getToolbarLinks(modules)
+    return modules
+  }
+
+  function createLinks (mystery) {
+    var modules = getModules(mystery)
+    var links = getLinks(modules)
+    var sidenav = document.getElementById('sidenav')
+    var length = links.length
+    for (var i = 0; i < length; i++) {
+      addLink(links[i], sidenav)
+    }
+
+    document.body.appendChild(sidenav)
+  }
+
+  function addLink (obj, parent) {
+    var link = document.createElement('a')
+    link.appendChild(document.createTextNode(obj.text))
+    link.setAttribute('href', obj.url)
+    link.style = 'color: white;'
+    parent.appendChild(link)
+  }
+
+  /* Set the width of the side navigation to 250px */
+  function openNav () {
+    document.getElementById('sidenav').style.width = '250px'
+  }
+
+  /* Set the width of the side navigation to 0 */
+  function closeNav () {
+    document.getElementById('sidenav').style.width = '0'
+  }
+
+  document.getElementById('closebtn').onclick = function () {
+    closeNav()
   }
 
   document.getElementById('mystery1').onclick = function () {
-    getModules('mystery1')
+    createLinks('mystery1')
+    // debugger
+    openNav()
   }
-
-  // document.getElementById('mystery2').onclick = function () {
-  //   getModules('mystery2')
-  // }
 }
 $(init)
