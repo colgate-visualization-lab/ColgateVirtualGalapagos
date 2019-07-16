9b68d2cc56212018dc1a1ac3ff90d04404ac0b92
dgathogo <edgetech26@gmail.com>
2019-07-16 16:17:27 -0400

    add mystery1

--- js/virtualgalapagos/index.js
+++ js/virtualgalapagos/index.js
@@ -1,4 +1,14 @@
 'use strict'
+var link = "https://www.colgate.edu"
+var element = document.createElement('a')
+element.setAttribute('href', link)
+element.innerHTML = 'Colgate University'
 
 var date = new Date
-document.getElementById('footer').innerText = 'Copyright \u00A9 ' + date.getFullYear() + ', Colgate University'
+var footer = document.getElementById('footer')
+footer.innerText = 'Copyright \u00A9 ' + date.getFullYear() + ', '
+footer.appendChild(element)
+
+$(document).ready(function(){
+  $('[data-toggle="tooltip"]').tooltip(); 
+});
