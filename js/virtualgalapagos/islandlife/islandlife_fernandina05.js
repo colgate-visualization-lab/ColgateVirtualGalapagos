12e379e3dd22552b72fa97b64acc7eced53f453b
tmnguyen1307 <thaydoi71@gmail.com>
2019-07-17 14:09:07 -0400

    Fix link for fernandina05

--- js/virtualgalapagos/islandlife/islandlife_fernandina05.js
+++ js/virtualgalapagos/islandlife/islandlife_fernandina05.js
@@ -1,7 +1,20 @@
-var img = document.getElementById()
+var boobie_diving = document.getElementById('boobie-diving')
+var caldera = document.getElementById('caldera-shot1')
+var iguana_path = document.getElementById('iguana-path')
+var still_water = document.getElementById('still-water')
 
-img.onclick = function(){
-  modal.style.display = "block";
-  modalImg.src = this.src;
-  captionText.innerHTML = this.alt;
+boobie_diving.onclick = function(){
+    location.href = "../../islandlife/panotour/IslandLifeCycle_BoobieDiving.html"
+}
+
+caldera.onclick = function(){
+    location.href = "../../islandlife/panotour/IslandLifeCycle_Caldera.html"
+}
+
+iguana_path.onclick = function(){
+    location.href = "../../islandlife/panotour/IslandLifeCycle_IguanaPath.html"
+}
+
+still_water.onclick = function(){
+    location.href = "../../islandlife/panotour/IslandLifeCycle_StillWater.html"
 }
