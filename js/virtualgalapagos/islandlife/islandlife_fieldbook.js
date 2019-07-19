2b2d6c4b381a7973d375ea6e7c3447f9f1726df3
tmnguyen1307 <thaydoi71@gmail.com>
2019-07-19 09:25:57 -0400

    Add new pages

--- js/virtualgalapagos/islandlife/islandlife_fieldbook.js
+++ js/virtualgalapagos/islandlife/islandlife_fieldbook.js
@@ -4,5 +4,8 @@ import { Fieldbook } from '../fieldbook.js'
 
 class IslandLifeFieldbook extends Fieldbook {
   // layout changes happen here, and in the future needs a system for handling multiple different fb layouts
+  populateDivs() {
+    
+  }
 }
 export { IslandLifeFieldbook }
