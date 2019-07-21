df430a0a3f97408d2d377541b1c8364e45ca73dd
tmnguyen1307 <thaydoi71@gmail.com>
2019-07-21 09:33:18 -0400

    Add new pages
    Former-commit-id: 1b240c078c2db305de66a6550bf736a153c35022 [formerly bf49d370b1be1b0ef5bbd169ac29e992cd45ff4b] [formerly 5b44b50e3d48462f7c0a77c71e1ee2abdede973c [formerly a12872b51ea0d3082d37ea7bd68e3cf03ade7fa2]]
    Former-commit-id: 4b606bd307302c9992430a5f37279eafb1648d13 [formerly a1f5141029ebbe95d46cb163509207d498495672]
    Former-commit-id: 19b7cbaa6367a5e9ec07dd7ee41acc281c710026

--- js/virtualgalapagos/islandlife/islandlife_fieldbook.js
+++ js/virtualgalapagos/islandlife/islandlife_fieldbook.js
@@ -4,8 +4,43 @@ import { Fieldbook } from '../fieldbook.js'
 
 class IslandLifeFieldbook extends Fieldbook {
   // layout changes happen here, and in the future needs a system for handling multiple different fb layouts
-  populateDivs() {
+  populateDivs () {
+    var currentDiv = this.divArray[0]
+    var innerHeader = document.createTextNode('Island Life Cycle Fieldbook')
+    var textArea = document.createElement('textarea')
+    textArea.className = 'fieldbook_ta'
+    textArea.rows = 1
+    textArea.cols = 1
+
+    var titles = ['Fernandina', 'Santa Cruz', 'Elevation Data', 'Espanola Photos', 'Island Life Cycle']
+    var subtitles = ['Notes on Fernandina elevation', 'Notes on environment - plants and animals', 'Notes on correlations', 'Notes on photos compared to Santa Cruz']
     
+    // case for page 1
+    currentDiv.appendChild(innerHeader)
+    for (var i=0; i < 5; i++){
+      Fieldbook.addLink(currentDiv, titles[i], i+1, this)
+    }
+
+    for (var i=0; i < 4; i++){
+      currentDiv = this.divArray[i+1]
+      innerHeader = document.createTextNode(titles[i])
+      currentDiv.appendChild(innerHeader)
+      Fieldbook.addSubtitle(currentDiv, subtitles[i])
+      currentDiv.appendChild(textArea.cloneNode(true))
+    }
+
+    // case for page 6
+    currentDiv = this.divArray[5]
+    innerHeader = document.createTextNode(titles[4])
+    currentDiv.appendChild(innerHeader)
+    Fieldbook.addSubtitle(currentDiv, 'Drawing of Island Life Cycle')
+    var mainDiv = document.createElement('DIV')
+    var canvas = document.createElement('CANVAS')
+    mainDiv.classList = 'container'
+    mainDiv.setAttribute('id', 'draw-container')
+    canvas.setAttribute('id', 'draw')
+    mainDiv.appendChild(canvas)
+    currentDiv.appendChild(mainDiv)
   }
 }
 export { IslandLifeFieldbook }
