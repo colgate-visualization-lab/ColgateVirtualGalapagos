b134928949d2bf80cd5f0739a94ccf6a7ac86528
dgathogo <edgetech26@gmail.com>
2019-07-22 15:27:18 -0400

    improve canvas
    Former-commit-id: 5076caaa02f5c933ce15ac019930bddb7e84ee73 [formerly 78917e377c63290669c341c3ad08614d58e674f0] [formerly e3d670bb168988e31e5098c93755fdf599769ef8 [formerly 3de5b5a1c4c4f09931a1cfd61c87f9b80ead544f]]
    Former-commit-id: d561b1e1821fcf1daa722d2b62c8f46b98e67c2f [formerly e1cb913326da567135ed89d13ff41969067ca437]
    Former-commit-id: 3c6eb5d63ee9293b7d491c15fadcd0d017008051

--- js/libs/paint.js
+++ js/libs/paint.js
@@ -2,7 +2,6 @@
 var strokes
 
 function saveToLocalStorage () {
-  console.log('heere')
   localStorage.setItem('canvas_strokes', JSON.stringify(strokes))
 }
 
@@ -52,10 +51,51 @@ function paintCanvas () {
       ctx.stroke()
     }
   }
+  function initButtons () {
+    var rootElement = document.getElementById('draw')
+    var canvas = document.createElement('CANVAS')
+    canvas.setAttribute('id', 'drawCanvas')
+    var container = document.createElement('DIV')
+
+    var undoBtn = document.createElement('BUTTON')
+    undoBtn.setAttribute('id', 'undo-btn')
+    undoBtn.className = 'btn btn-light btn-sm'
+    var icon = document.createElement('i')
+    icon.className = 'material-icons'
+    icon.innerHTML = 'undo'
+    undoBtn.appendChild(icon)
+
+    var exportBtn = document.createElement('BUTTON')
+    exportBtn.setAttribute('id', 'export-btn')
+    exportBtn.className = 'btn btn-light btn-sm'
+    icon = document.createElement('i')
+    icon.className = 'material-icons'
+    icon.innerHTML = 'save_alt'
+    exportBtn.appendChild(icon)
+
+    var colorPalette = document.createElement('INPUT')
+    colorPalette.setAttribute('id', 'color-picker')
+    colorPalette.setAttribute('type', 'color')
+
+    var brushSize = document.createElement('INPUT')
+    brushSize.setAttribute('id', 'brush-size')
+    brushSize.setAttribute('type', 'range')
+    brushSize.setAttribute('min', '1')
+    brushSize.setAttribute('max', '25')
+    brushSize.className = 'slider'
+
+    container.appendChild(undoBtn)
+    container.appendChild(exportBtn)
+    container.appendChild(colorPalette)
+    container.appendChild(brushSize)
+    rootElement.appendChild(container)
+    rootElement.appendChild(canvas)
+  }
 
   function init () {
-    canvas = $('#draw')
-    canvasElement = document.getElementById('draw')
+    initButtons()
+    canvas = $('#drawCanvas')
+    canvasElement = document.getElementById('drawCanvas')
     if (canvasElement === null) {
       return
     }
@@ -97,7 +137,7 @@ function paintCanvas () {
 
     // check if localstorage has an array of strokes saved
     var savedStrokes = localStorage.getItem('canvas_strokes')
-    if (savedStrokes !== null) {
+    if (savedStrokes !== null && savedStrokes !== 'undefined') {
       strokes = JSON.parse(savedStrokes)
       redraw()
     } else {
@@ -105,21 +145,21 @@ function paintCanvas () {
     }
 
     // buttons
-    $('#save-to-local-storage').click(function () {
-      saveToLocalStorage()
-    })
-    $('#save-btn').click(function () {
-      window.open(canvasElement.toDataURL())
+    $('#export-btn').click(function () {
+      var link = document.createElement('a')
+      link.href = canvasElement.toDataURL()
+      link.download = 'canvas.png'
+      document.body.appendChild(link)
+      link.click()
     })
     $('#undo-btn').click(function () {
       strokes.pop()
       redraw()
     })
-    $('#clear-btn').click(function () {
+    $('#clear-btn').click(function () { // not implemented
       strokes = []
       redraw()
     })
-
     $('#color-picker').on('input', function () {
       brush.color = this.value
     })
@@ -129,9 +169,6 @@ function paintCanvas () {
 
     // bind touch actions
     canvasElement.addEventListener('touchstart', function (e) {
-    // mousePos = getTouchPos(canvas, e)
-    // brush.x = mousePos.x
-    // brush.x = 3
       var touch = e.touches[0]
       var mouseEvent = new MouseEvent('mousedown', {
         clientX: touch.clientX,
