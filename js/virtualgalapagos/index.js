65805eb20ecd54b805d86d33c059d7cf9f4b4924
dgathogo <edgetech26@gmail.com>
2019-07-23 11:39:24 -0400

    init html fiels

--- js/virtualgalapagos/index.js
+++ js/virtualgalapagos/index.js
@@ -1,14 +1,14 @@
 'use strict'
-var link = "https://www.colgate.edu"
+var link = 'https://www.colgate.edu'
 var element = document.createElement('a')
 element.setAttribute('href', link)
 element.innerHTML = 'Colgate University'
 
-var date = new Date
+var date = new Date()
 var footer = document.getElementById('footer')
 footer.innerText = 'Copyright \u00A9 ' + date.getFullYear() + ', '
 footer.appendChild(element)
 
-$(document).ready(function(){
-  $('[data-toggle="tooltip"]').tooltip(); 
-});
+$(document).ready(function () {
+  $('[data-toggle="tooltip"]').tooltip()
+})
