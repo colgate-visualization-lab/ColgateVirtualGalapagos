331d6079907c4eb22b1f09d167c7e871941dcc2f
tmnguyen1307 <thaydoi71@gmail.com>
2019-07-23 11:04:02 -0400

    Fix bugs
    Former-commit-id: aec2ccd210e03b06d00911fc92b67406046dde3b [formerly d25430d892ce69bb3babc6499493f70368d0a7ba] [formerly 6c054c6c77d0ca864aa3be9693a9341beaa73bd3 [formerly 4eeedeb01f6eae7b819a7005bb0162f5ed1e7a45]]
    Former-commit-id: 3d14c80d1f8c210cf0d6560d33bcbdbd35f57563 [formerly 94dedad0eafe05df68907475a037983f96baae52]
    Former-commit-id: 690fcdfe7a7c3e9c74598863dd0174f35b8a3c7b

--- js/virtualgalapagos/islandlife/islandlife_colonization.js
+++ js/virtualgalapagos/islandlife/islandlife_colonization.js
@@ -10,4 +10,11 @@ function drop(ev) {
     ev.preventDefault();
     var data = ev.dataTransfer.getData("text");
     ev.target.appendChild(document.getElementById(data));
-  }
+}
+
+var c = document.getElementById("myCanvas");
+var ctx = c.getContext("2d");
+ctx.beginPath();
+ctx.moveTo(20, 20);
+ctx.bezierCurveTo(20, 100, 200, 100, 200, 20);
+ctx.stroke();
