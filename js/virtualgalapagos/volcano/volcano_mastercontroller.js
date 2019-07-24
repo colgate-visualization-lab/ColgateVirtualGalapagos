69a15c2f4bc1ff4837bc5b8bd1abaa1fd363819e
dgathogo <edgetech26@gmail.com>
2019-07-24 15:00:17 -0400

    add volcano audio, change some pages
    Former-commit-id: 769b36713a902a4ca6fff00e9166567155c3403c [formerly 85236ea9ece8c46eb75725689779c326677cbdf6] [formerly cc84fde38dd71de867d06559d66f100d13839a67 [formerly a3510f0f0d4dea48219ae99fe3d520d43c55a950]]
    Former-commit-id: e30b70887b4daf54573691717f9b3f21ac2c6478 [formerly d803e910aca2ae9a579a8a80900f3b010bf8b84b]
    Former-commit-id: 27ea52bae298fb803957301861d54ac11075fe00

--- js/virtualgalapagos/volcano/volcano_mastercontroller.js
+++ js/virtualgalapagos/volcano/volcano_mastercontroller.js
@@ -15,7 +15,6 @@ import { VolcanoFieldbook } from './volcano_fieldbook.js'
 import { VolcanoToolbar } from './volcano_toolbar.js'
 import { plumeplacement } from './Volcano_PlumePlacement01.js'
 import { terrainmap } from './Volcano_TerrainMap03.js'
-import { terrainMapToggle } from './Volcano_TerrainMap06.js'
 
 class VolcanoMasterController extends MasterController {
   constructor (pageList, pageName) {
@@ -46,7 +45,7 @@ class VolcanoMasterController extends MasterController {
 var pageName = VolcanoMasterController.grabPageName()
 
 // List of pages in module in order
-var pageList = ['Volcano_TerrainMap01', 'panotour/Volcano_Exploration01', 'panotour/Volcano_Exploration02', 'panotour/Volcano_Exploration03', 'panotour/Volcano_Exploration04', 'panotour/Volcano_Exploration05', 'Volcano_TerrainMap02', 'Volcano_TerrainMap03', 'Volcano_TerrainMap04', 'Volcano_TerrainMapAges01', 'Volcano_TerrainMapAges02', 'Volcano_OtherVolcanoes01', 'Volcano_TectonicPlateWhiteboard01', 'Volcano_TectonicPlates01', 'Volcano_PlatesGame01', 'Volcano_TectonicPlates02', 'Volcano_MantlePlumesWhiteboard01', 'Volcano_SouthAmerica01', 'Volcano_TerrainMap06', 'Volcano_PlumePlacement01', 'Volcano_WhereNext01', 'Volcano_MainMenu01']
+var pageList = ['Volcano_TerrainMap01', 'panotour/Volcano_Exploration01', 'panotour/Volcano_Exploration02', 'panotour/Volcano_Exploration03', 'panotour/Volcano_Exploration04', 'panotour/Volcano_Exploration05', 'Volcano_TerrainMap02', 'Volcano_TerrainMap03', 'Volcano_TerrainMap04', 'Volcano_TerrainMapAges01', 'Volcano_TerrainMapAges02', 'Volcano_OtherVolcanoes01', 'Volcano_TectonicPlateWhiteboard01', 'Volcano_TectonicPlates01', 'Volcano_PlatesGame01', 'Volcano_TectonicPlates02', 'Volcano_MantlePlumesWhiteboard01', 'Volcano_SouthAmerica01', 'Volcano_TerrainMap06', 'Volcano_TerrainMapEruptionDates01', 'Volcano_PlumePlacement01', 'Volcano_WhereNext01', 'Volcano_MainMenu01']
 
 // Pages and associated fieldbook pages
 var fbPages = {
@@ -155,9 +154,4 @@ if (pageName === 'Volcano_PlumePlacement01') {
 // TerrainMap03
 if (pageName === 'Volcano_TerrainMap03') {
   terrainmap(masterController, avController)
-}
-
-// TerrainMap06
-if (pageName === 'Volcano_TerrainMap06') {
-  terrainMapToggle()
 }
