71cf3f874d21f29a0e87c83334b04f0df3edfcaa
dgathogo <edgetech26@gmail.com>
2019-07-30 12:38:45 -0400

    bug fix

--- js/virtualgalapagos/index.js
+++ js/virtualgalapagos/index.js
@@ -13,17 +13,23 @@ $(document).ready(function () {
   $('[data-toggle="tooltip"]').tooltip()
 })
 
+navbarScroll()
 /* When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar */
-var prevScrollpos = window.pageYOffset
-var navbar = document.getElementById('navbar')
-var height = navbar.offsetHeight
-console.log(height)
-window.onscroll = function () {
-  var currentScrollPos = window.pageYOffset
-  if (prevScrollpos > currentScrollPos) {
-    navbar.style.top = '0'
-  } else {
-    navbar.style.top = '-' + height + 'px'
+function navbarScroll () {
+  var prevScrollpos = window.pageYOffset
+  var navbar = document.getElementById('navbar')
+  if (navbar === null) {
+    return
   }
-  prevScrollpos = currentScrollPos
+  var height = navbar.offsetHeight
+  console.log(height)
+  window.onscroll = function () {
+    var currentScrollPos = window.pageYOffset
+    if (prevScrollpos > currentScrollPos) {
+      navbar.style.top = '0'
+    } else {
+      navbar.style.top = '-' + height + 'px'
+    }
+    prevScrollpos = currentScrollPos
+  }
 }
