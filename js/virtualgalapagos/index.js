9369e00b0d61a1bb00ebf7d3263f3b72af44a026
Tam Nguyen '22 <tmnguyen@colgate.edu>
2019-08-02 16:07:20 -0400

    Create sidebar
    Former-commit-id: 4821734ab4b87c5f60261fbf17b843857ccea277 [formerly 9c601e88657e8ef1a16064ce83e1e0e1e3ac83aa] [formerly 0492ab2979352d48a03393ed5fc3bd9a3ee9f11f [formerly 6ad9b9ed03a3e1592141b76d9c40502a254e7b5a]]
    Former-commit-id: 1b019163f4b21d2db70e2f9113a27e0d024f6c2b [formerly b96914d0e7e29438345726c1e6a55b4281bda82e]
    Former-commit-id: 280954fbcea77e2b43f376f4853e318ca81494ff

--- js/virtualgalapagos/index.js
+++ js/virtualgalapagos/index.js
@@ -24,22 +24,23 @@ function createSideBar(){
 
     var fieldbook = document.getElementById("component-list").firstElementChild
     fieldbook.className = "active"
-    var a_tag = fieldbook.firstChild
-    a_tag.setAttribute("data-toggle", "collapse")
-    a_tag.setAttribute("aria-expanded", "false")
-    a_tag.className = "dropdown-toggle"
     var fieldbook_content_link_map = {"Iguana Mystery": "#mystery1", "Mystery 2": "#", "Mystery 3": "#"}
-    var fieldbook_content = createDropDown("fieldbook", fieldbook_content_link_map, "collapse list-unstyled")
-    fieldbook.appendChild(fieldbook_content)
+    createSubList("fieldbook", fieldbook, fieldbook_content_link_map)
     
     var iguana_mystery = document.getElementById("fieldbook").firstElementChild
-    a_tag = iguana_mystery.firstChild
-    a_tag.setAttribute("data-toggle", "collapse")
-    a_tag.setAttribute("aria-expanded", "false")
-    a_tag.className = "dropdown-toggle"
     var iguana_content_link_map = {"Volcano": "#", "Iguana": "#", "Island Life Cycle": "#", "Currents": "#"}
-    var iguana_content = createDropDown("mystery1", iguana_content_link_map, "list-unstyled collapse show")
-    iguana_mystery.appendChild(iguana_content)
+    createSubList("mystery1", iguana_mystery, iguana_content_link_map)
+
+    var buttons = document.createElement("ul")
+    buttons.className = "list-unstyled CTAs"
+    var setting_btn = document.createElement("li")
+    var setting_link = document.createElement("a")
+    setting_link.href = "#"
+    setting_link.className = "download"
+    setting_link.innerHTML = "Settings"
+    setting_btn.appendChild(setting_link)
+    buttons.appendChild(setting_btn)
+    side_bar.appendChild(buttons)
 }
 
 function createDropDown(id, map, class_name) {
@@ -58,3 +59,25 @@ function createDropDown(id, map, class_name) {
     }
     return component_list
 }
+
+function createSubList(id, parent_node, map){
+    var a_tag = parent_node.firstChild
+    a_tag.setAttribute("data-toggle", "collapse")
+    a_tag.setAttribute("aria-expanded", "false")
+    a_tag.className = "dropdown-toggle"
+    var child = createDropDown(id, map, "collapse list-unstyled")
+    parent_node.appendChild(child)
+}
+
+$(document).ready(function () {
+    createSideBar()
+    $("#sidebar").mCustomScrollbar({
+        theme: "minimal"
+    });
+
+    $('#sidebarCollapse').on('click', function () {
+        $('#sidebar, #content').toggleClass('active');
+        $('.collapse.in').toggleClass('in');
+        $('a[aria-expanded=true]').attr('aria-expanded', 'false');
+    });
+});
