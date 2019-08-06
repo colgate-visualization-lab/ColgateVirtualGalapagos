6d2bb49c8d16086056e2bcb160ec4bac1f0b43ac
Tam Nguyen '22 <tmnguyen@colgate.edu>
2019-08-06 15:35:56 -0400

    Fix navbar code to OOP

--- js/virtualgalapagos/index.js
+++ js/virtualgalapagos/index.js
@@ -1,75 +1,94 @@
 'use strict'
 
 class NavigationBar {
-    constructor(){}
-}
-
-function createSideBar(){
-    //Sidebar 
-    var side_bar = document.getElementById("sidebar")
-
-    //Side bar Header
-    var sidebar_header = document.createElement("div")
-    sidebar_header.classList.add("sidebar-header")
-    var header_content = document.createElement("h3")
-    header_content.innerHTML = "Backpack"
-    sidebar_header.appendChild(header_content)
-    side_bar.appendChild(sidebar_header)
-
-    //Back pack content
-    var backpack_content_link_map = {"Field Book": "#fieldbook", "Map": "#", "Database": "#", "Interviews": "#"}
-    var component_list = createDropDown("component-list", backpack_content_link_map, "list-unstyled components")
-    side_bar.appendChild(component_list)
-
-    var fieldbook = document.getElementById("component-list").firstElementChild
-    fieldbook.className = "active"
-    var fieldbook_content_link_map = {"Iguana Mystery": "#mystery1", "Mystery 2": "#", "Mystery 3": "#"}
-    createSubList("fieldbook", fieldbook, fieldbook_content_link_map)
-    
-    var iguana_mystery = document.getElementById("fieldbook").firstElementChild
-    var iguana_content_link_map = {"Volcano": "#", "Iguana": "#", "Island Life Cycle": "#", "Currents": "#"}
-    createSubList("mystery1", iguana_mystery, iguana_content_link_map)
-
-    var buttons = document.createElement("ul")
-    buttons.className = "list-unstyled CTAs"
-    var setting_btn = document.createElement("li")
-    var setting_link = document.createElement("a")
-    setting_link.href = "#"
-    setting_link.className = "download"
-    setting_link.innerHTML = "Settings"
-    setting_btn.appendChild(setting_link)
-    buttons.appendChild(setting_btn)
-    side_bar.appendChild(buttons)
-}
-
-function createDropDown(id, map, class_name) {
-    var component_list = document.createElement("ul")
-    component_list.className = class_name
-    component_list.id = id
-
-    var content = Object.keys(map)
-    for (var i = 0; i < content.length; i++) {
-        var element = document.createElement("li")
-        var link = document.createElement("a")
-        link.href = map[content[i]]
-        link.innerHTML = content[i]
-        element.appendChild(link)
-        component_list.appendChild(element)
+    constructor(){
     }
-    return component_list
+
+    init(){
+        createSideBar()
+    }
+
+    createSidebar(){
+        var side_bar = document.getElementById("sidebar")
+        var sidebar_header = this.setupSideBarHeader()
+        var setting_button = this.setupSettingButton()
+        side_bar.appendChild(sidebar_header)
+        side_bar.appendChild(setting_button)
+    }
+
+    setupSideBarHeader(){
+        var sidebar_header = document.createElement("div")
+        sidebar_header.classList.add("sidebar-header")
+        var header_content = document.createElement("h3")
+        header_content.innerHTML = "Backpack"
+        sidebar_header.appendChild(header_content)
+        return sidebar_header
+    }
+
+    setupBackPackContent(map){
+        return createDropDown("component-list", map, "list-unstyled components")
+    }
+    
+    //Placeholder Method - Need to change to incorporate the real fieldbooks later
+    setupFieldbook(){
+        var fieldbook = document.getElementById("component-list").firstElementChild
+        fieldbook.className = "active"
+        var fieldbook_content_link_map = {"Iguana Mystery": "#mystery1", "Mystery 2": "#", "Mystery 3": "#"}
+        createSubList("fieldbook", fieldbook, fieldbook_content_link_map)
+
+        var iguana_mystery = document.getElementById("fieldbook").firstElementChild
+        var iguana_content_link_map = {"Volcano": "#", "Iguana": "#", "Island Life Cycle": "#", "Currents": "#"}
+        createSubList("mystery1", iguana_mystery, iguana_content_link_map)
+    }
+
+    setupSettingButton(){ 
+        var buttons = document.createElement("ul")
+        buttons.className = "list-unstyled CTAs"
+        var setting_btn = document.createElement("li")
+        var setting_link = document.createElement("a")
+        setting_link.href = "#"
+        setting_link.className = "setting"
+        setting_link.innerHTML = "Settings"
+        setting_btn.appendChild(setting_link)
+        buttons.appendChild(setting_btn)
+        return buttons
+    }
+
+    createTopNav(){
+        var topnav = document.getElementById('topnav-container')
+    }
+
+    static createDropDown(id, map, class_name) {
+        var component_list = document.createElement("ul")
+        component_list.className = class_name
+        component_list.id = id
+    
+        var content = Object.keys(map)
+        for (var i = 0; i < content.length; i++) {
+            var element = document.createElement("li")
+            var link = document.createElement("a")
+            link.href = map[content[i]]
+            link.innerHTML = content[i]
+            element.appendChild(link)
+            component_list.appendChild(element)
+        }
+        return component_list
+    }
+
+    static createSubList(id, parent_node, map){
+        var a_tag = parent_node.firstChild
+        a_tag.setAttribute("data-toggle", "collapse")
+        a_tag.setAttribute("aria-expanded", "false")
+        a_tag.className = "dropdown-toggle"
+        var child = createDropDown(id, map, "collapse list-unstyled")
+        parent_node.appendChild(child)
+    }
+    
 }
 
-function createSubList(id, parent_node, map){
-    var a_tag = parent_node.firstChild
-    a_tag.setAttribute("data-toggle", "collapse")
-    a_tag.setAttribute("aria-expanded", "false")
-    a_tag.className = "dropdown-toggle"
-    var child = createDropDown(id, map, "collapse list-unstyled")
-    parent_node.appendChild(child)
-}
+
 
 function createTopNav(){
-    var topnav = document.getElementById('topnav-container')
 
     //Create button to toggle side navigation 
     var sidebar_button = document.createElement('button')
