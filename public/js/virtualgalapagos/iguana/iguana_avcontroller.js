516cc0441d38d496f5dcae7a706dbf6222bb6bf2
tmnguyen1307 <tmnguyen@colgate.edu>
2020-02-19 22:22:04 -0500

    Adding backend
    Former-commit-id: c6172bc91e06f4d7df4bea27c2baba72d86818f6 [formerly aaa3b500161a713e43eacc4d3362959698c5e9f7] [formerly fb31f41dd4cc6c5b703acee0b5185bc5ae83b9c1 [formerly edafb9c17327c7235a6b5100eb90e54271d58378]]
    Former-commit-id: 023f3bae1092bd60a4f83ec8c03f3484ae0a6478 [formerly a85bb545db82fbbc06129f1756ba9f017f4ce770]
    Former-commit-id: 084397bc853d19af2df8b666408798382d15e44c

---
+++ public/js/virtualgalapagos/iguana/iguana_avcontroller.js
@@ -0,0 +1,29 @@
+'use strict'
+
+import { AVController } from '../avcontroller.js'
+
+class IguanaAVController extends AVController {
+  constructor (player, avType, masterController) {
+    super(player, avType, masterController)
+    this.src = document.getElementById('player_src')
+  }
+
+  // overrides method in avcontroller, needed because of file structure of panotour submodule
+  setTrack (filename) {
+    console.log('setting track to: ' + filename)
+    var prefix
+    if (this.masterController.pageName.includes('panotour')) {
+      prefix = '../../' + this.avType + '/' + this.masterController.moduleName + '/'
+    } else {
+      prefix = '../' + this.avType + '/' + this.masterController.moduleName + '/'
+    }
+    if (this.avType === 'video') {
+      this.player.src = prefix + filename + '.mp4'
+    } else if (this.avType === 'audio') {
+      this.player.src = prefix + filename + '.mp3'
+    } else {
+      throw new Error('Source type unsupported')
+    }
+  }
+}
+export { IguanaAVController }
