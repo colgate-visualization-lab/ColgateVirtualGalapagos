d75024a8067416abee08aeef4d48a5ac8746b5ea
Tam Nguyen <tmnguyen@colgate.edu>
2020-03-24 20:13:57 -0400

    New Website

---
+++ oldpage/js/virtualgalapagos/currents/currents_dragdrop.js
@@ -0,0 +1,7 @@
+'use strict'
+
+import { DragDrop } from '../dragdrop.js'
+
+class CurrentsDragDrop extends DragDrop {
+}
+export { CurrentsDragDrop }
---
+++ oldpage/js/virtualgalapagos/iguana/iguana_toolbar.js
@@ -0,0 +1,5 @@
+
+import { Toolbar } from '../toolbar.js'
+class IguanaToolbar extends Toolbar {
+}
+export { IguanaToolbar }
