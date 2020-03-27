4dd9846bc82c4a1f5cb4104b110d064f23cc96f0
Tam Nguyen <tmnguyen@colgate.edu>
2020-03-26 21:43:13 -0400

    Fix webpack errors
    Former-commit-id: e591571f33121442de2b88ea7dec5c8f64552264 [formerly c7f3aa9202be1dadd3beb0c924a71088668de7f0] [formerly 4216274ed2f0ee761b030555487b1aabd70739e9 [formerly 172a7a5d17f1c1073192ffc3f5066bbac9546d2f]]
    Former-commit-id: 145cc602a0c76024ecc505cba2da36fe49c07c78 [formerly 16e2d8c8dada653842bc8498d63189f5f08bd32f]
    Former-commit-id: 2ee842f8a18e75d079098f9e32e240098559cec3

---
+++ src/containers/App.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react'
+
+class App extends Component {
+    render(){
+        return (
+            <div>
+                <p>I am an App</p>
+            </div>
+        )
+    }
+}
+
+export default App
