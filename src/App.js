cbeed5acc15870af7c1f60f3aaeacd12d8684e53
Tam Nguyen <tmnguyen@colgate.edu>
2020-04-10 20:03:18 -0400

    Fix homepage

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import React, {Component} from 'react'
-import Layout from './containers/Layout/Layout'
+import {Route, Switch} from 'react-router-dom'
 import Homepage from './containers/Homepage/Homepage'
+import Layout from './containers/Layout/Layout'
+import SplashScreen from './components/SplashScreen/SplashScreen'
+import SignIn from './containers/SignIn/SignIn'
+
 
 class App extends Component {
     render(){
         return (
             <div>
-                <Layout>
-                    <Homepage />
-                </Layout>
+                <Switch>
+                    <Route path="/" exact component={SplashScreen}/>
+                    <Route path="/authorization" component={SignIn}/>
+                    <Layout>
+                    <Route path="/home" exact component={Homepage}/>
+                    </Layout>
+                </Switch>
             </div>
         )
     }
