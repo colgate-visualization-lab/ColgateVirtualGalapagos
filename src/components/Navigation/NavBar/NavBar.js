bed54dc2f1996cfbd28b5570d46331fb0188293a
Tam Nguyen <tmnguyen@colgate.edu>
2020-04-15 08:55:01 -0400

    Fix Homepage
    Former-commit-id: 1ae1487d3c13f33e3c06a2617cded7b607df18bc [formerly 7a9fe15ff6e9f10669d2fca6ef10b33d5afc549e] [formerly 20764f6cfca616ae54c77748d217e7e0c86e8d85 [formerly 3084bc5bdfbd4caf9eae5c9a1cd949c293ca6238]]
    Former-commit-id: 4d086e82a7cd8ef47af02fd75edbc486b23bfabd [formerly 4f7d7fe66e83b0d15b36a88c711e661d8aec9b31]
    Former-commit-id: 9034e66fb1cb7f2ce0eacb0147cd408a621694ab

--- src/components/Navigation/NavBar/NavBar.js
+++ src/components/Navigation/NavBar/NavBar.js
@@ -2,17 +2,31 @@ import React from 'react'
 import {NavLink} from 'react-router-dom'
 import classes from './Navbar.css'
 import {Navbar, Nav} from 'react-bootstrap'
-import LogoImage from '../../LogoImage/LogoImage'
+import LogoImage from './LogoImage/LogoImage'
 import Backpack from '../../../assets/homepage/backpack.png'
 
 const navbar = () => {
     return (
-        <Navbar expand='lg' className='row'>
-            <Navbar.Brand className="col-8"><NavLink to='/home'><LogoImage/></NavLink></Navbar.Brand>
-            <Nav.Item className="col" style={{padding: '10px'}}>Mysteries</Nav.Item>
-            <Nav.Item className="col"><NavLink to="/gallery">Gallery</NavLink></Nav.Item>
-            <Nav.Item className="col-md-auto"><NavLink to="/authorization">Sign In/Sign Up</NavLink></Nav.Item>
-            <Nav.Item className="col-3"><NavLink to="/home"><img src={Backpack} alt="Backpack" className={classes.Backpack}/></NavLink></Nav.Item>
+        <Navbar expand='lg' fill>
+            <NavLink to='/home'>
+                <Navbar.Brand>
+                    <LogoImage className={classes.Logo}/>
+                </Navbar.Brand>
+            </NavLink>
+            <NavLink to="/home">
+                <Nav.Item className={classes.NavItem}>Mysteries</Nav.Item>
+            </NavLink>
+            <NavLink to="/gallery">
+                <Nav.Item className={classes.NavItem}>Gallery</Nav.Item>
+            </NavLink>
+            <NavLink to="/authorization">
+                <Nav.Item className={classes.NavItem}>Sign In</Nav.Item>
+            </NavLink>
+            <a href="#open-modal">
+                <Nav.Item>
+                    <img src={Backpack} alt="Backpack" className={classes.Backpack}/>
+                </Nav.Item>
+            </a>
         </Navbar>
     )
 }
