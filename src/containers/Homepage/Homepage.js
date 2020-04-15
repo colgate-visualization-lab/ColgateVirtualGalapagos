1f948634daeefd93183880bf5095d205abe6e776
Giancarlo Arcese <giancarloarcese@Giancarlos-MacBook-Pro.local>
2020-04-15 03:58:32 -0400

    Updated Image maps on homepage. Note, you will need to run npm install react-image-mapper for this to work

--- src/containers/Homepage/Homepage.js
+++ src/containers/Homepage/Homepage.js
@@ -5,26 +5,28 @@ import MapImg from '../../assets/homepage/homepage.png'
 import ImageMapper from 'react-image-mapper'
 import MAP from '/Users/giancarloarcese/ColgateVirtualGalapagos/src/components/ImageMaps.js'
 
+
+
+
 class Homepage extends Component {
     constructor(props) {
         super(props)
     
         this.state = {
-             count: 'nothing'
+           
+             msg: 'Click on the highlighted island to travel to the next module!'
         }
     }
     
-enterArea() {
+enterArea(area) {
     this.setState({
-        count: 'something'
+        msg: `${area.name}`
     })
-    console.log(this.state.count)
 }
 leaveArea() {
     this.setState({
-        count: 'back to nothing'
+        msg: 'Click on the highlighted island to travel to the next module!'
     })
-    console.log(this.state.count)
 }
     render(){
         const animation = "animated slideInRight"
@@ -33,31 +35,16 @@ leaveArea() {
             <div className={animation}>
                 <ImageMapper 
                 src={MapImg}
-                width={1000}
+                width={1100}
                 imgWidth={1920}
                 map={MAP}
+                fillColor={"rgba(0, 246, 255, 0.33)"}
                 onMouseEnter={area => this.enterArea(area)}
-				onMouseLeave={area => this.leaveArea(area)}
+				onMouseLeave={() => this.leaveArea()}
                 />
-                <h1 className={classes.Mapheader}> This is {this.state.count}</h1>
+                <h1 className={classes.Mapheader}>{this.state.msg}</h1>
             </div>
-        </div>
-          
-          
-              
-                /* <img src={MapImg} className={classes.Map + animation} useMap="#image-map"/> 
-                <map name="image-map">
-                    <area 
-                        title="Mystery 1" 
-                        href="mysteries/mystery1.html"
-                        coords="390,367,438,342,446,311,497,295,514,275,533,297,553,334,587,367,595,395,601,462,637,
-                        496,662,521,699,549,724,566,730,605,721,655,738,669,755,655,778,674,794,708,814,736,794,781,
-                        780,820,738,854,710,854,674,871,618,894,567,879,491,874,474,846,446,817,458,793,497,742,539,
-                        706,587,698,620,700,646,678,654,655,604,613,584,597,575,557,522,524,497,487,497,456,488,417,
-                        486,386,469,369,415,386" 
-                        shape="poly" />
-                </map>  */
-           
+        </div>  
         )
     }
 }
