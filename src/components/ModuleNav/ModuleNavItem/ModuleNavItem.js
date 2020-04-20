bf57c09762dc20a402e58a785de4e605ef38812d
Tam Nguyen <tmnguyen@colgate.edu>
2020-04-20 02:32:38 -0400

    New Features
    Former-commit-id: 53acaa43566ed9f41a189196c8419149006876e1 [formerly 154cb868220cab43b2289ae2c01fb09be67e0c7f] [formerly 41e77daf37cffab3234d2a5e1923b982cccf752b [formerly fabfdb64b23e3e374accbc6a080773eed3af03ed]]
    Former-commit-id: 1bdb6396f24dd66532549aac7d46c62cef7b58b4 [formerly b1849942903c4c09af3b46be05d4b3a19b450f83]
    Former-commit-id: 8c1d442587a117b356477cb1faec42da23e6677f

---
+++ src/components/ModuleNav/ModuleNavItem/ModuleNavItem.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import classes from '../ModuleNav.css'
+
+const moduleNavItem = (props) => {
+    return (
+        <div className={classes.col + props.animation} onTouchStart={() => this.classList.toggle('hover')}>
+            <div className={classes.container}>
+                <div className={classes.front} style={{backgroundImage: props.background}}>
+                    <div className={classes.inner}>
+                        <p>{props.title}</p>
+                    </div>
+                </div>
+                    <div className={classes.back}>
+                        <div className={classes.inner}>
+                        <p>{props.description}</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default moduleNavItem
--- src/components/Navigation/Backpack/Backpack.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const backpack = () => {
-    return (
-        <div></div>
-    )
-}
-
-export default backpack; 
