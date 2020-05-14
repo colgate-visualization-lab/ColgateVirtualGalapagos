7cea2c1746b964cffe385f2f3161e06430077617
Tam Nguyen <tmnguyen@colgate.edu>
2020-05-14 14:46:37 -0400

    Add eslint config
    Former-commit-id: df681414b972c91c3919615ff25aad2868227a1b [formerly 90eb5f6c57da6c24b9172eed7ea70872e9b6e62b] [formerly c0f9a8b41ed1f86656fbeb3a4c635d22dcfb3fd8 [formerly fb97c7db2fbf90589b94ac5ceefa73c309f5d958]]
    Former-commit-id: 36305654ce3972f39f0f08b74633aef8ce544e7c [formerly ce423794de4a7c841a6029d59054be210e4121b4]
    Former-commit-id: ec723c8c2e9b148cec3ff10a8d169ef3502d3367

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,11 +1,11 @@
-import React from 'react'
+import React from "react"
 
 const footer = () => {
-    return (
-        <footer>
+	return (
+		<footer>
             Footer
-        </footer>
-    )
+		</footer>
+	)
 }
 
 export default footer
--- src/containers/Homepage/Homepage.js
+++ src/containers/Homepage/Homepage.js
@@ -1,93 +1,101 @@
-import React, { Component } from "react"
-import classes from './Homepage.css'
-import ImageMapper from 'react-image-mapper'
-import MAP from '../../components/ImageMap/ImageMaps.js'
-import { Redirect } from 'react-router'
-import BackgroundVideo from '../../components/BackgroundVideo/BackgroundVideo.js'
-import backgroundImage from '../../assets/P3153499.png'
+import React, { Fragment, Component } from "react"
+import classes from "./Homepage.css"
+import ImageMapper from "react-image-mapper"
+import MAP from "../../components/ImageMap/ImageMaps.js"
+import { Redirect } from "react-router"
+import BackgroundVideo from "../../components/BackgroundVideo/BackgroundVideo.js"
+import backgroundImage from "../../assets/P3153499.png"
 
 class Homepage extends Component {
-    constructor(props) {
-        super(props)
-    
-        this.state = {
-             msg: 'Click on the highlighted island to travel to the next module!',
-             route: false,
-             link: "",
-             width: 0
-        }
+	constructor(props) {
+		super(props)
+
+		this.state = {
+			msg: "Click on the highlighted island to travel to the next module!",
+			route: false,
+			link: "",
+			width: 0
+		}
+	}
+
+	//Image Map States & Handlers
+	enterArea(area) {
+		let unlock = `${area._id}`
+		let lockValue = this.props.lockValue
+
+		if (unlock <= lockValue) {
+			this.setState({ msg: `${area.name}`, link: `${area.id}` })
+		}
+
+		else {
+			this.setState({ msg: "this is locked" })
+		}
+	}
+
+	leaveArea() {
+		this.setState({ msg: "Click on the highlighted island to travel to the next module!" })
+	}
+
+	enterModule(area) {
+		let unlock = `${area._id}`
+		let lockValue = this.props.lockValue
+		if (unlock <= lockValue) {
+			this.setState({ route: true })
+		}
+	}
+
+    //<ImmageMapper> cannot be styled with css, so these methods update a width state for responsivness
+    updateDimensions = () => {
+    	let windowWidth = window.innerWidth
+    	if (windowWidth > 1500) {
+    		this.setState({ width: window.innerWidth - 500 })
+    	}
+    	else if (windowWidth > 1300) {
+    		this.setState({ width: window.innerWidth - 400 })
+    	}
+    	else if (windowWidth > 1100) {
+    		this.setState({ width: window.innerWidth - 300 })
+    	}
+    	else {
+    		this.setState({ width: window.innerWidth - 50 })
+    	}
     }
-//Image Map States & Handlers
-enterArea(area) {
-    let unlock = `${area._id}`
-    let lockValue = this.props.lockValue
-if(unlock <= lockValue){
-    this.setState({msg: `${area.name}`,link: `${area.id}`})
+
+    componentDidMount() {
+    	window.addEventListener("resize", this.updateDimensions)
+    	this.updateDimensions()
     }
-else { 
-    this.setState({msg: "this is locked"})
+
+    componentWillUnmount() {
+    	window.removeEventListener("resize", this.updateDimensions)
     }
-}
-leaveArea() {
-    this.setState({msg: 'Click on the highlighted island to travel to the next module!'})
-}
-enterModule(area) {
-    let unlock = `${area._id}`
-    let lockValue = this.props.lockValue
-if (unlock <= lockValue) {
-        this.setState ({route: true})
-    }
-}
-//<ImmageMapper> cannot be styled with css, so these methods update a width state for responsivness
-updateDimensions = () => {
-    let windowWidth = window.innerWidth
-if (windowWidth > 1500) {
-  this.setState({ width: window.innerWidth -500})
-    }
-else if (windowWidth > 1300) {
-    this.setState({ width: window.innerWidth -400})
-    }
-else if (windowWidth > 1100) {
-    this.setState({ width: window.innerWidth -300})
-    }   
-else {
-    this.setState({ width: window.innerWidth -50})
-    }
-}
-componentDidMount() {
-  window.addEventListener('resize', this.updateDimensions);
-  this.updateDimensions()
-}
-componentWillUnmount() {
-  window.removeEventListener('resize', this.updateDimensions);
-}
-    
-render(){
-    const animation = "animated slideInRight"
-    const {msg, route, link, width} = this.state
-    const {MapImg} = this.props
-    if (route) {
-        return <Redirect to={link}/>
-            }
-        return (
-        <Fragment>
-            <img src={backgroundImage} className={classes.videoSubstitute} alt="" /> {/*Background Image for Mobile Divices */}
-            <BackgroundVideo />
-            <div className={`${animation} ${classes.containerFix}`}>
-                <ImageMapper 
-                src={MapImg}
-                width={width}
-                imgWidth={1920}
-                map={MAP}
-                fillColor={"rgba(0, 246, 255, 0.33)"}
-                onMouseEnter={area => this.enterArea(area)}
-                onMouseLeave={() => this.leaveArea()}
-                onClick={(area) => this.enterModule(area)}
-                />
-            </div>
-            <h1 className={classes.Mapheader} style={{width: `${width}px`}}>{msg}</h1>
-        </Fragment>
-        )
+
+    render() {
+    	const animation = "animated slideInRight"
+    	const { msg, route, link, width } = this.state
+    	const { MapImg } = this.props
+    	if (route) {
+    		return <Redirect to={link} />
+    	}
+    	return (
+    		<Fragment>
+    			<img src={backgroundImage} className={classes.videoSubstitute} alt="" /> {/*Background Image for Mobile Divices */}
+    			<BackgroundVideo />
+    			<div className={`${animation} ${classes.containerFix}`}>
+    				<ImageMapper
+    					src={MapImg}
+    					width={width}
+    					imgWidth={1920}
+    					map={MAP}
+    					fillColor={"rgba(0, 246, 255, 0.33)"}
+    					onMouseEnter={area => this.enterArea(area)}
+    					onMouseLeave={() => this.leaveArea()}
+    					onClick={(area) => this.enterModule(area)}
+    				/>
+    			</div>
+    			<h1 className={classes.Mapheader} style={{ width: `${width}px` }}>{msg}</h1>
+    		</Fragment>
+    	)
     }
 }
 export default Homepage
