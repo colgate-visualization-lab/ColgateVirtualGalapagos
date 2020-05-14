6b903373074c4bf43cb9c66b33099f06b56ef07d
Tam Nguyen <tmnguyen@colgate.edu>
2020-05-14 14:46:37 -0400

    Add eslint config

--- src/App.js
+++ src/App.js
@@ -1,43 +1,43 @@
-import React, {Component} from 'react'
-import {Route, Switch} from 'react-router-dom'
-import Homepage from './containers/Homepage/Homepage'
-import Layout from './containers/Layout/Layout'
-import SplashScreen from './components/SplashScreen/SplashScreen'
-import SignIn from './containers/SignIn/SignIn'
-import ModuleNav from './components/ModuleNav/ModuleNav'
-import Gallery from './containers/Gallery/Gallery'
-import MapSantiago from '/Users/giancarloarcese/ColgateVirtualGalapagos/src/assets/homepage/MapSantiago.png'
-import MapFernandina from './assets/MapFernandina.png'
-import MapIsabela from './assets/homepage/homepage.png'
-import MapPinzon from './assets/homepage/MapPinzon.png'
-import MapFloreana from './assets/homepage/MapFloreana.png'
-import MapEspanola from './assets/homepage/MapEspanola.png'
+import React, {Component} from "react"
+import {Route, Switch} from "react-router-dom"
+import Homepage from "./containers/Homepage/Homepage"
+import Layout from "./containers/Layout/Layout"
+import SplashScreen from "./components/SplashScreen/SplashScreen"
+import SignIn from "./containers/SignIn/SignIn"
+import ModuleNav from "./components/ModuleNav/ModuleNav"
+import Gallery from "./containers/Gallery/Gallery"
+//import MapSantiago from "./assets/homepage/MapSantiago.png"
+import MapFernandina from "./assets/MapFernandina.png"
+import MapIsabela from "./assets/homepage/homepage.png"
+import MapPinzon from "./assets/homepage/MapPinzon.png"
+import MapFloreana from "./assets/homepage/MapFloreana.png"
+import MapEspanola from "./assets/homepage/MapEspanola.png"
 
 class App extends Component {
-    render(){
-        return (
-            <div>
-                <Switch>
-                    <Route path="/" exact component={SplashScreen}/>
-                    <Route path="/authorization" component={SignIn}/>
-                    <Layout>
-                        <Route path="/home" 
-                         render={(props) => <Homepage {...props} lockValue={1} MapImg={MapIsabela} />}/>
-                         <Route path="/home2" 
-                         render={(props) => <Homepage {...props} lockValue={2} MapImg={MapFernandina} />}/>
-                           <Route path="/home3" 
-                         render={(props) => <Homepage {...props} lockValue={3} MapImg={MapFloreana} />}/>
-                         <Route path="/home4" 
-                         render={(props) => <Homepage {...props} lockValue={4} MapImg={MapEspanola} />}/>
-                           <Route path="/home5" 
-                         render={(props) => <Homepage {...props} lockValue={5} MapImg={MapPinzon} />}/>
-                         <Route path="/isabella" exact component={ModuleNav}/>
-                         <Route path="/gallery" exact component={Gallery}/>
-                    </Layout>
-                </Switch>
-            </div>
-        )
-    }
+	render(){
+		return (
+			<div>
+				<Switch>
+					<Route path="/" exact component={SplashScreen}/>
+					<Route path="/authorization" component={SignIn}/>
+					<Layout>
+						<Route path="/home" 
+							render={(props) => <Homepage {...props} lockValue={1} MapImg={MapIsabela} />}/>
+						<Route path="/home2" 
+							render={(props) => <Homepage {...props} lockValue={2} MapImg={MapFernandina} />}/>
+						<Route path="/home3" 
+							render={(props) => <Homepage {...props} lockValue={3} MapImg={MapFloreana} />}/>
+						<Route path="/home4" 
+							render={(props) => <Homepage {...props} lockValue={4} MapImg={MapEspanola} />}/>
+						<Route path="/home5" 
+							render={(props) => <Homepage {...props} lockValue={5} MapImg={MapPinzon} />}/>
+						<Route path="/isabella" exact component={ModuleNav}/>
+						<Route path="/gallery" exact component={Gallery}/>
+					</Layout>
+				</Switch>
+			</div>
+		)
+	}
 }
 
 export default App
