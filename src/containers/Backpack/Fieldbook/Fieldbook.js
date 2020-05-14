9bcfbf7123f08d5a18c4c66f78750f768817ab3e
Tam Nguyen <tmnguyen@colgate.edu>
2020-05-14 14:46:37 -0400

    Add eslint config
    Former-commit-id: 05c178be360a3ad5fe4760cd7610664634d2722e [formerly f7212f91d0d21d4aeac695dcedfe971d5fa8cbfb] [formerly aa5020c4c646b92b0939458cd309f3ff94ad0855 [formerly 0c0822041b50bc62c7aff7039ec42b4f0b5f31cb]]
    Former-commit-id: 1204004217e4d059094cbde33cf5012fccfbf7be [formerly 23e096627999e7d23c23f9c0069564015ec1916b]
    Former-commit-id: 16f3af0051adc486214c21b1c0ad1830722657d5

--- src/containers/Backpack/Fieldbook/Fieldbook.js
+++ src/containers/Backpack/Fieldbook/Fieldbook.js
@@ -1,33 +1,33 @@
-import React, { Component } from 'react'
-import classes from './Fieldbook.css'
-import FlipPage from 'react-flip-page'
+import React, { Component } from "react"
+import classes from "./Fieldbook.css"
+import FlipPage from "react-flip-page"
 
 class Fieldbook extends Component {
-    click(){
-        console.log('Clicked')
-    }
+	click(){
+		console.log("Clicked")
+	}
 
-    inputHandler(event){
-        console.log(event.target.value)
-    }
+	inputHandler(event){
+		console.log(event.target.value)
+	}
 
-    render(){
-        return (
-            <FlipPage 
-                orientation="horizontal" 
-                flipOnTouch="true"  
-                uncutPages="true"  
-                perspective="50em" 
-                animationDuration="1000"
-                responsive="true"
-            >
-                <textarea className={classes.textarea} onChange={this.inputHandler} onClick={this.click}
-                value={"Default text"}/>
-                <div className={classes.textarea}>Note 2</div>
-            </FlipPage>
-        )
-    }
+	render(){
+		return (
+			<FlipPage 
+				orientation="horizontal" 
+				flipOnTouch="true"  
+				uncutPages="true"  
+				perspective="50em" 
+				animationDuration="1000"
+				responsive="true"
+			>
+				<textarea className={classes.textarea} onChange={this.inputHandler} onClick={this.click}
+					value={"Default text"}/>
+				<div className={classes.textarea}>Note 2</div>
+			</FlipPage>
+		)
+	}
     
 }
 
-export default Fieldbook;
+export default Fieldbook
