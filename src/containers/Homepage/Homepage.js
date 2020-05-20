5ba7e04efdcce30bea2d38060cd918ef485973b2
Tam Nguyen <tmnguyen@colgate.edu>
2020-05-20 15:48:02 -0400

    Fix assets folder

--- src/containers/Homepage/Homepage.js
+++ src/containers/Homepage/Homepage.js
@@ -3,8 +3,8 @@ import classes from "./Homepage.css"
 import ImageMapper from "react-image-mapper"
 import MAP from "../../components/ImageMap/ImageMaps.js"
 import { Redirect } from "react-router"
+import backgroundImage from "../../assets/images/homepage/P3153499.png"
 //import BackgroundVideo from "../../components/BackgroundVideo/BackgroundVideo.js"
-import backgroundImage from "../../assets/P3153499.png"
 
 class Homepage extends Component {
 	constructor(props) {
@@ -25,9 +25,7 @@ class Homepage extends Component {
 
 		if (unlock <= lockValue) {
 			this.setState({ msg: `${area.name}`, link: `${area.id}` })
-		}
-
-		else {
+		} else {
 			this.setState({ msg: "this is locked" })
 		}
 	}
@@ -49,14 +47,11 @@ class Homepage extends Component {
     	let windowWidth = window.innerWidth
     	if (windowWidth > 1500) {
     		this.setState({ width: window.innerWidth - 500 })
-    	}
-    	else if (windowWidth > 1300) {
+    	} else if (windowWidth > 1300) {
     		this.setState({ width: window.innerWidth - 400 })
-    	}
-    	else if (windowWidth > 1100) {
+    	} else if (windowWidth > 1100) {
     		this.setState({ width: window.innerWidth - 300 })
-    	}
-    	else {
+    	} else {
     		this.setState({ width: window.innerWidth - 50 })
     	}
     }
@@ -75,25 +70,30 @@ class Homepage extends Component {
     	const { msg, route, link, width } = this.state
     	const { MapImg } = this.props
     	if (route) {
-    		return <Redirect to={link} />
+    		return <Redirect to = { link }
+    		/>
     	}
-    	return (
-    		<Fragment>
-    			<img src={backgroundImage} className={classes.videoSubstitute} alt="" /> {/*Background Image for Mobile Divices */}
-    			<div className={`${animation} ${classes.containerFix}`}>
-    				<ImageMapper
-    					src={MapImg}
-    					width={width}
-    					imgWidth={1920}
-    					map={MAP}
-    					fillColor={"rgba(0, 246, 255, 0.33)"}
-    					onMouseEnter={area => this.enterArea(area)}
-    					onMouseLeave={() => this.leaveArea()}
-    					onClick={(area) => this.enterModule(area)}
-    				/>
-    			</div>
-    			<h1 className={classes.Mapheader} style={{ width: `${width}px` }}>{msg}</h1>
-    		</Fragment>
+    	return ( 
+    		<Fragment >
+    			<img 
+    				src = { backgroundImage }
+    				className = { classes.videoSubstitute }
+    				alt = "" 
+    			/> { /*Background Image for Mobile Divices */ } 
+    			<div className = { `${animation} ${classes.containerFix}` } >
+    				<ImageMapper 
+    					src = { MapImg }
+    					width = { width }
+    					imgWidth = { 1920 }
+    					map = { MAP }
+    					fillColor = { "rgba(0, 246, 255, 0.33)" }
+    					onMouseEnter = { area => this.enterArea(area) }
+    					onMouseLeave = {() => this.leaveArea()}
+    					onClick = {(area) => this.enterModule(area)}
+    				/> 
+    			</div> 
+    			<h1 className = { classes.Mapheader } style = {{ width: `${width}px` }}>{ msg }</h1>
+    		</Fragment >
     	)
     }
 }
