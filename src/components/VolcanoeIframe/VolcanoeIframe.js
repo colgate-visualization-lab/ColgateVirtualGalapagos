e9e06ab28bfee1f3134e92dc935631b24b4d7ed8
Giancarlo Arcese <giancarloarcese@Giancarlos-MacBook-Pro.local>
2020-05-23 13:13:59 -0400

    removed local file
    Former-commit-id: e9214050b658d58d615eebedb2d8b88b3c5837d4 [formerly ccd4860fda7a72a60864a42e9934c327e692abb7] [formerly c5ae713c0ed44c9d385bd0c6b701801acd38a43f [formerly de15b2de77feb10f3ab7186c098be22f476e1528]]
    Former-commit-id: a01300c085a6f3c8ade913af4d76cb65a89b1e35 [formerly ed765e7002331530a8499e56b28eeb9225f53a24]
    Former-commit-id: fab82c224245b1104b85bf6c66396727b2050e07

--- src/components/VolcanoeIframe/VolcanoeIframe.js
+++ src/components/VolcanoeIframe/VolcanoeIframe.js
@@ -4,7 +4,7 @@ import Iframe from 'react-iframe'
 function VolcanoeIframe() {
     return (
         <Iframe 
-        url="http://localhost:8000/index.htm?disable-background-preload=true&force_audio=html5"
+        url=""
         height="100%"
         width="100%"
         />
