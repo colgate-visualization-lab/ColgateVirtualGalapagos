07a8c032750c869a12effc26b7b0fb3de5460abb
Giancarlo Arcese <giancarloarcese@Giancarlos-MacBook-Pro.local>
2020-06-02 04:17:21 -0400

    Added next and back feature

--- src/containers/VolcanoModule/VolcanoModule.js
+++ src/containers/VolcanoModule/VolcanoModule.js
@@ -10,7 +10,10 @@ class VolcanoeModule extends Component {
     
         this.state = {
              slideIndex: 0,
-             hidden: "classes.notVisible"
+             revealHidden: {visibility: "hidden"},
+             progressIndex: 0,
+             completed: {visibility: "hidden"},
+             notCompleted: {visibility: "visible"},
         }
     }
 
@@ -35,17 +38,39 @@ prevoptionalSlide() {
     })
 }
 renderHidden() {
-    this.setState({hidden: ""}),
-    console.log("yo")
+    this.setState({revealHidden: {visibility: "visible"}})
+}
+hideHidden() {
+    this.setState({revealHidden: {visibility: "hidden"}})
+}
+updateProgress() {
+    this.setState({progressIndex: this.state.slideIndex + 2})
+}
+backTrack() {
+    if (this.state.progressIndex > this.state.slideIndex) {
+       this.setState({completed: {visibility: "visible"}, notCompleted: {visibility: "hidden"}})
+    }
+    else if (this.state.slideIndex >= this.state.progressIndex) {
+        this.setState({completed: {visibility: "hidden"}, notCompleted: {visibility: "visible"}})
+    }
+}
+forwardTrack() {
+    if (this.state.progressIndex - 2 > this.state.slideIndex) {
+        this.setState({completed: {visibility: "visible"}, notCompleted: {visibility: "hidden"}})
+     }
+     else if (this.state.slideIndex == this.state.progressIndex - 2) {
+         this.setState({completed: {visibility: "hidden"}, notCompleted: {visibility: "visible"}})
+     }
 }
 
     render() {
-        const {slideIndex, hidden} = this.state
+        const {slideIndex, hidden, completed, notCompleted} = this.state
         if (slideIndex == 0) {
             return (
                 <div className={classes.divClass}>
                     <img style={{height: "auto", width: "70%"}} src={logo} />
-                    <button className={classes.buttonClassnext} onClick={() => this.nextSlide()}>Enter Volcano Module</button>
+                    <button style={notCompleted} className={classes.buttonClassnext} onClick={() => {this.updateProgress(); this.nextSlide()}}>Enter Volcano Module</button>
+                    <button style={completed} className={classes.buttonClassnext} onClick={() => {this.nextSlide(); this.forwardTrack()}}>Enter Volcano Module :)</button>
                 </div>
             )
         }
@@ -53,9 +78,10 @@ renderHidden() {
             const Vista1 = "http://virtual.colgate.edu/Cooley/"
             return (
                 <div className={classes.divClass}>
-                        <VolcanoeIframe threeD={Vista1} />
-                    <button className={classes.buttonClassback} onClick={() => this.prevSlide()}>Back</button>
-                    <button className={classes.buttonClassnext} onClick={() => this.nextSlide()}>Next</button>
+                    <VolcanoeIframe threeD={Vista1} />
+                    <button className={classes.buttonClassback} onClick={() => {this.prevSlide(); this.backTrack()}}>Back</button>
+                    <button style={notCompleted} className={classes.buttonClassnext} onClick={() => {this.updateProgress(); this.nextSlide()}}>Next</button>
+                    <button style={completed} className={classes.buttonClassnext} onClick={() => {this.nextSlide(); this.forwardTrack()}}>Next :)</button>
                 </div>
             )
         }
@@ -65,8 +91,9 @@ renderHidden() {
                     <img style={{height: "100%", width: "100%"}} src={Image1} />
                     <img style={{position: "absolute", height: "auto", width: "5%", zIndex: "1", left: "0", top: "0"}} src={magnifyingGlass}
                     onClick={() => this.optionalSlide()}/>
-                    <button className={classes.buttonClassback} onClick={() => this.prevSlide()}>Back</button>
-                    <button className={classes.buttonClassnext} onClick={() => this.nextSlide()}>Next</button>
+                    <button className={classes.buttonClassback} onClick={() => {this.prevSlide(); this.backTrack()}}>Back</button>
+                    <button style={notCompleted} className={classes.buttonClassnext} onClick={() => {this.updateProgress(); this.nextSlide()}}>Next</button>
+                    <button style={completed} className={classes.buttonClassnext} onClick={() => this.nextSlide()}>Next :)</button>
                 </div>
             )
         }
@@ -74,7 +101,7 @@ renderHidden() {
             const splitImage = "https://cdn.knightlab.com/libs/juxtapose/latest/embed/index.html?uid=70e85972-9c07-11ea-a7cb-0edaf8f81e27"
             return (
                 <div className={classes.divClass}>
-                        <VolcanoeIframe threeD={splitImage} />
+                    <VolcanoeIframe threeD={splitImage} />
                     <button className={classes.buttonClassback} onClick={() => this.prevoptionalSlide()}>Back</button>
                 </div>
             )
@@ -83,9 +110,9 @@ renderHidden() {
             const Vista1 = "http://virtual.colgate.edu/Cooley/"
             return (
                 <div className={classes.divClass}>
-                        <VolcanoeIframe threeD={Vista1} />
+                    <VolcanoeIframe threeD={Vista1} />
                     <button className={classes.buttonClassback} onClick={() => this.prevSlide()}>Back</button>
-                    <button className={classes.buttonClassnext} onClick={() => {this.nextSlide(); setTimeout(() => this.renderHidden(), 3000)}}>Next</button>
+                    <button className={classes.buttonClassnext} onClick={() => {this.nextSlide(); this.hideHidden(); setTimeout(() => this.renderHidden(), 3000)}}>Next</button>
                 </div>
             )
         }
@@ -93,9 +120,19 @@ renderHidden() {
             return (
                 <div className={classes.divClass}>
                     <img style={{height: "100%", width: "100%"}} src={Image1} />
-                    <img style={{position: "absolute", height: "auto", width: "5%", zIndex: "1", left: "0", top: "0"}} src={magnifyingGlass}
-                    className={`${hidden}`}
-                    onClick={() => this.optionalSlide()}/>
+                    <img className={classes.temporaryclass} src={magnifyingGlass} src={magnifyingGlass}
+                    style={hidden} onClick={() => this.optionalSlide()}/>
+                    <button className={classes.buttonClassback} onClick={() => this.prevSlide()}>Back</button>
+                    <button className={classes.buttonClassnext} onClick={() => this.nextSlide()}>Next</button>
+                </div>
+            )
+        }
+        else if (slideIndex == 5) {
+            return (
+                <div className={classes.divClass}>
+                    <img style={{height: "100%", width: "100%"}} src={Image1} />
+                    <img className={classes.temporaryclass} src={magnifyingGlass} src={magnifyingGlass}
+                    style={hidden} onClick={() => this.optionalSlide()}/>
                     <button className={classes.buttonClassback} onClick={() => this.prevSlide()}>Back</button>
                     <button className={classes.buttonClassnext} onClick={() => this.nextSlide()}>Next</button>
                 </div>
