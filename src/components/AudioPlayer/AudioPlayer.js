be6e1e9b4c4e87f6d82e2cd1914d49960b8a1f28
Giancarlo <Garcese@colgate.edu>
2020-07-03 03:17:59 -0400

    Please work :(
    Former-commit-id: 00abbea4b5d5e1bb4a590ab326516e538cfd0b94 [formerly d977ee5b8c983f9dc35dd82b15dc2b9da0cecb02] [formerly 9d9a7116511defd07b497804d73b359def280612 [formerly 6af1d1ba0d411efa02ab3572a81a5dbaba61794b]]
    Former-commit-id: 298ec30ed1569cbc1a4a508114f6835a634479fd [formerly bc5770a08479b1363fa9c02d25d6c5bc17479bb2]
    Former-commit-id: cd40f6abc6840c052c57bd3f019950fdbe18a752

---
+++ src/components/AudioPlayer/AudioPlayer.js
@@ -0,0 +1,42 @@
+import React, {useRef, useState} from 'react';
+import ReactPlayer from 'react-player';
+import CircleControls from 'react-player-circle-controls';
+import 'style-loader!css-loader!react-player-circle-controls/dist/styles.css';
+
+const AudioPlayer = (props) => {
+  const player = useRef(null);
+  // const [playing, setPlaying] = useState(false);
+  const [playerState, setPlayerState] = useState({
+    played: 0,
+    loaded: 0
+  });
+
+  const onSeek = amount => {
+    if (player.current) {
+      player.current.seekTo(amount, 'fraction');
+    }
+  };
+
+  return (
+    <div style={{position: "absolute", left: "0", top: "0", width: "128px", zIndex: "1000"}}>
+      <ReactPlayer
+        ref={player}
+        url={props.src}
+        playing={props.playing}
+        height="0"
+        width="0"
+        onProgress={setPlayerState}
+        onEnded={() => props.onEnded()}
+      />
+      <CircleControls
+        played={playerState.played}
+        // loaded={playerState.loaded}
+        playing={props.playing}
+        onSeek={onSeek}
+        onTogglePlaying={() => props.toggleAudio()}
+      />
+    </div>
+  );
+};
+
+export default AudioPlayer
