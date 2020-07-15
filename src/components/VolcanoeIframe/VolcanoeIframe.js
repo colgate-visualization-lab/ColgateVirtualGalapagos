24491dad8ae43f727284c0d4d706a3ea442830bd
Div Chamria <dchamria@colgate.edu>
2020-07-15 19:18:00 -0400

    merged

--- src/components/VolcanoeIframe/VolcanoeIframe.js
+++ src/components/VolcanoeIframe/VolcanoeIframe.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import Iframe from "react-iframe"
+import classes from "./VolcanoeIframe.css"
 
 function VolcanoeIframe(props) {
     return (
-            <Iframe position="absolute" width="100%" height="100%" src={props.src} />
+            <Iframe position="absolute" width="100%" height="100%" className={classes.Iframe} src={props.src} />
     )
 }
 export default VolcanoeIframe
