e0a2bf1c644f3f771282deaa1123804a98acce76
Div Chamria <dchamria@colgate.edu>
2020-08-29 17:05:17 -0400

    beta
    Former-commit-id: faa12ea78e4be72c9e5b1926b00364b512bfdeef [formerly 91db1f9d75425bb1db579b9d6d1422632c533b2d] [formerly 4d84d31fac06a0e334bed26e84c4c0787c9dc76d [formerly de87685c42dad332a809eddb19d47e56f16ef7c9]]
    Former-commit-id: bdd58aba88d7ebd63429fb51083b7ee08c229c78 [formerly 64bac703e5aba6aa3f7c147b103e5370bfa528b6]
    Former-commit-id: 5bcf781ea0b960e6974344225f63bd779c7fd6af

--- src/components/MapComponent/MapComponent.js
+++ src/components/MapComponent/MapComponent.js
@@ -2,22 +2,222 @@ import React, { Component } from "react"
 import { Map, InfoWindow, Marker, GoogleApiWrapper } from "google-maps-react"
 import classes from './MapComponent.css'
 
+const MapStyle = [
+  {
+      "featureType": "water",
+      "elementType": "geometry",
+      "stylers": [
+          {
+              "visibility": "on"
+          },
+          {
+              "color": "#aee2e0"
+          }
+      ]
+  },
+  {
+      "featureType": "landscape",
+      "elementType": "geometry.fill",
+      "stylers": [
+          {
+              "color": "#abce83"
+          }
+      ]
+  },
+  {
+      "featureType": "poi",
+      "elementType": "geometry.fill",
+      "stylers": [
+          {
+              "color": "#769E72"
+          }
+      ]
+  },
+  {
+      "featureType": "poi",
+      "elementType": "labels.text.fill",
+      "stylers": [
+          {
+              "color": "#7B8758"
+          }
+      ]
+  },
+  {
+      "featureType": "poi",
+      "elementType": "labels.text.stroke",
+      "stylers": [
+          {
+              "color": "#EBF4A4"
+          }
+      ]
+  },
+  {
+      "featureType": "poi.park",
+      "elementType": "geometry",
+      "stylers": [
+          {
+              "visibility": "simplified"
+          },
+          {
+              "color": "#8dab68"
+          }
+      ]
+  },
+  {
+      "featureType": "road",
+      "elementType": "geometry.fill",
+      "stylers": [
+          {
+              "visibility": "simplified"
+          }
+      ]
+  },
+  {
+      "featureType": "road",
+      "elementType": "labels.text.fill",
+      "stylers": [
+          {
+              "color": "#5B5B3F"
+          }
+      ]
+  },
+  {
+      "featureType": "road",
+      "elementType": "labels.text.stroke",
+      "stylers": [
+          {
+              "color": "#ABCE83"
+          }
+      ]
+  },
+  {
+      "featureType": "road",
+      "elementType": "labels.icon",
+      "stylers": [
+          {
+              "visibility": "off"
+          }
+      ]
+  },
+  {
+      "featureType": "road.local",
+      "elementType": "geometry",
+      "stylers": [
+          {
+              "color": "#A4C67D"
+          }
+      ]
+  },
+  {
+      "featureType": "road.arterial",
+      "elementType": "geometry",
+      "stylers": [
+          {
+              "color": "#9BBF72"
+          }
+      ]
+  },
+  {
+      "featureType": "road.highway",
+      "elementType": "geometry",
+      "stylers": [
+          {
+              "color": "#EBF4A4"
+          }
+      ]
+  },
+  {
+      "featureType": "transit",
+      "stylers": [
+          {
+              "visibility": "off"
+          }
+      ]
+  },
+  {
+      "featureType": "administrative",
+      "elementType": "geometry.stroke",
+      "stylers": [
+          {
+              "visibility": "on"
+          },
+          {
+              "color": "#87ae79"
+          }
+      ]
+  },
+  {
+      "featureType": "administrative",
+      "elementType": "geometry.fill",
+      "stylers": [
+          {
+              "color": "#7f2200"
+          },
+          {
+              "visibility": "off"
+          }
+      ]
+  },
+  {
+      "featureType": "administrative",
+      "elementType": "labels.text.stroke",
+      "stylers": [
+          {
+              "color": "#ffffff"
+          },
+          {
+              "visibility": "on"
+          },
+          {
+              "weight": 4.1
+          }
+      ]
+  },
+  {
+      "featureType": "administrative",
+      "elementType": "labels.text.fill",
+      "stylers": [
+          {
+              "color": "#495421"
+          }
+      ]
+  },
+  {
+      "featureType": "administrative.neighborhood",
+      "elementType": "labels",
+      "stylers": [
+          {
+              "visibility": "off"
+          }
+      ]
+  }
+]
+
 
 export class MapComponent extends Component{  
+  _mapLoaded(mapProps, map) {
+    map.setOptions({
+       styles: MapStyle
+    })
+  }
     render(){
         return(
           <div className={classes.MapComponent}>            
           <Map           
           google={this.props.google}
-          zoom={8}
-          initialCenter={this.props.location}
-        >
+          zoom={10}
+          initialCenter={this.props.initialLocation}
+          center={this.props.location}
+          onClick={this.mapClicked}
+          onReady={(mapProps, map) => this._mapLoaded(mapProps, map)} 
+          >
 
-    <Marker onClick={this.onMarkerClick}
-            name={'Current location'} />
+            <Marker 
+            name={this.props.markerName}  
+            position={this.props.location} />
 
   
-  </Map></div>
+          </Map></div>
 
         )
     }
--- src/components/ModuleNav/ModuleNav.js
+++ src/components/ModuleNav/ModuleNav.js
@@ -2,11 +2,12 @@ import React from "react"
 import classes from "./ModuleNav.css"
 import ModuleNavItem from "./ModuleNavItem/ModuleNavItem"
 import backgroundimage from "../../assets/images/homepage/P3153499.png"
+import mysteryIntro from "../../assets/videos/MysteryIntro.mp4"
 
 const moduleNav = () => {
 	const content = [
 		{"title": "Volcano", 
-			"description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit.", 
+			"description": "Follow along with Carlos and Adriana as they learn about volcanoes. Explore how volcanic islands form and how they came to be in the Galapagos!", 
 			"background": "url(https://source.unsplash.com/collection/1155333/1600x900)", 
 			"animation": " animated slideInDown",
 			"link": "/volcanomod"
@@ -20,28 +21,28 @@ const moduleNav = () => {
 		}, 
 
 		{"title": "Iguana",
-			"description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit.",
+			"description": "Carlos and Adriana need to learn about iguanas if they want to find the ones in their mystery. Help them learn about the different types of iguanas in the Galapagos and how they got there in the first place.",
 			"background": "url(https://source.unsplash.com/collection/327760/1600x900)", 
 			"animation": " animated slideInDown slower",
 			"link": "/iguana"
 		}, 
 
 		{"title": "Currents",
-			"description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit.",
+			"description": "Currents are key to this iguana mystery! Learn about how currents affect the Galapagos islands and the benefits they provide animals like Carlos and Adriana!",
 			"background": "url(https://source.unsplash.com/collection/319663/1600x900)", 
 			"animation": " animated slideInUp",
 			"link": "/home"
 		}, 
 
 		{"title": "Eruption",
-			"description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit.",
+			"description": "Volcanic eruptions can have a big impact on the animals and plants that live near them. Find out all the ways eruptions take place in the Galapagos.",
 			"background": "url(https://source.unsplash.com/collection/1014/1600x900)", 
 			"animation": " animated slideInUp slow",
 			"link": "/home"
 		}, 
 
 		{"title": "Island Life Cycle",
-			"description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit.",
+			"description": "In order to figure out this mystery, Carlos and Adriana need to find the oldest islands. Learn about what makes an island old and where they are most likely to be in the Galapagos!",
 			"background": "url(https://source.unsplash.com/collection/357786/1600x900)", 
 			"animation": " animated slideInUp slower",
 			"link": "/home"
@@ -49,7 +50,7 @@ const moduleNav = () => {
 	]
 
 	const items = content.map((item, index) => <ModuleNavItem key={index} title={item.title} description={item.description} background={item.background} animation={item.animation} link={item.link}/>)
-
+	
 	return (
 		<div className={classes.backgroundDiv}>
 		<img src={backgroundimage} className={classes.backgroundImage} />
@@ -58,6 +59,7 @@ const moduleNav = () => {
 				{items}
 			</div>
 		</div>
+	
 		</div> 
 	)
 }
