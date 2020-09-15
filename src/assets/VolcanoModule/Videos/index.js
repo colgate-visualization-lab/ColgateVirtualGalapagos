298b143ebdb74647dd3e497cf521f9347985a547
Div Chamria <dchamria@colgate.edu>
2020-09-15 00:09:10 -0400

    fix
    Former-commit-id: 7c16636bb58ff65d7e20d6df3eb452b093d13deb [formerly 778abffb08cd7d53d84bcd554058c3b557bb09d8] [formerly 376cc4a7d6ee09fba5776638c7bb5f66cd5a88b8 [formerly eb3b831798404bcc15c8b2b1f3adb77bd90c958a]]
    Former-commit-id: 46661363b33f215b3e9f983e42fc72eeb5e60070 [formerly b70b09ccbafabb0d5e20df0c0d98a763999fa108]
    Former-commit-id: faa5ed404029d84164ca5eb70bb13b60531e6e73

--- src/assets/VolcanoModule/Videos/index.js
+++
@@ -1,3 +0,0 @@
-export {default as introVideo} from "./VolcanoModuleIntro.mp4"
-export {default as plateTectonics} from "./VolcanoPlateTectonics.mp4"
-export {default as mantlePlumes} from "./VolcanoMantlePlumes.mp4"
