8a9d2203f1ba1c4971a0291f23bd3190e4d1a76a
Desmond Tuiyot <dtuiyot@gmail.com>
2020-10-08 12:54:18 -0400

    added slide 3 video player with video selector.

---
+++ src/components/IguanaData/Slide3InteractiveData.js
@@ -0,0 +1,52 @@
+export const data = [
+  {
+    id: "birdHypothesis",
+    videoSrc:
+      "http://virtualgalapagos.colgate.edu/assets/IguanaModule/ShoreWaves.mp4",
+    audioSrc:
+      "http://virtualgalapagos.colgate.edu/assets/VolcanoModule/Volcano_Compare360.mp3",
+    cardTitle: "Bird Carrying Iguana",
+    cardThumbnail:
+      "https://pmdvod.nationalgeographic.com/NG_Video/993/419/66827_1_1280x720_1024x576_177585731924.jpg",
+  },
+  {
+    id: "pumiceHypothesis",
+    videoSrc:
+      "http://virtualgalapagos.colgate.edu/assets/VolcanoModule/VolcanoModuleIntro.mp4",
+    audioSrc:
+      "http://virtualgalapagos.colgate.edu/assets/VolcanoModule/Volcano_Exploration01.mp3",
+    cardTitle: "Pumice Carrying Iguana",
+    cardThumbnail:
+      "https://c7.alamy.com/comp/F1MGEB/closeup-of-a-land-iguana-sitting-on-lava-gravel-and-pumice-F1MGEB.jpg",
+  },
+  {
+    id: "vegetationRaftHypothesis",
+    videoSrc:
+      "http://virtualgalapagos.colgate.edu/assets/VolcanoModule/VolcanoPlateTectonics.mp4",
+    audioSrc:
+      "http://virtualgalapagos.colgate.edu/assets/VolcanoModule/Volcano_Exploration01.mp3",
+    cardTitle: "Vegetation Raft Carrying Iguana",
+    cardThumbnail:
+      "https://onlinelibrary.wiley.com/cms/asset/143fe60a-0897-43e3-a402-0dbf884379d9/ece35414-fig-0001-m.jpg",
+  },
+  {
+    id: "driftwoodHypothesis",
+    videoSrc:
+      "http://virtualgalapagos.colgate.edu/assets/IguanaModule/IguanaPath.mp4",
+    audioSrc:
+      "http://virtualgalapagos.colgate.edu/assets/VolcanoModule/Volcano_Exploration03.mp3",
+    cardTitle: "Driftwood Carrying Iguana",
+    cardThumbnail:
+      "https://www.santacruzgalapagoscruise.com/wp-content/uploads/2017/12/christmas-iguana-resting.jpg",
+  },
+  {
+    id: "shipHypothesis",
+    videoSrc:
+      "http://virtualgalapagos.colgate.edu/assets/IguanaModule/IguanaRocks.mp4",
+    audioSrc:
+      "http://virtualgalapagos.colgate.edu/assets/VolcanoModule/Volcano_Fernandina01.mp3",
+    cardTitle: "Ship Carrying Iguana",
+    cardThumbnail:
+      "https://www.galapagos.org/wp-content/uploads/2012/01/LandIguana.NorineAudette.2007.jpg",
+  },
+];
