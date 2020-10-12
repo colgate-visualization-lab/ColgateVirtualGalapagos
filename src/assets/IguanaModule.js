0ba915c2e9412146971835e762cbe666d51f2149
Desmond Tuiyot <dtuiyot@gmail.com>
2020-10-12 13:31:23 -0400

    changed slide 3 structure: video selection will appear at the end of the video

--- src/assets/IguanaModule.js
+++ src/assets/IguanaModule.js
@@ -5,17 +5,21 @@ import slide15_spikes from "./slide15Assets/slide15_backSpikes.mp3";
 import slide15_stomach from "./slide15Assets/slide15_stomach.mp3";
 import slide15_tail from "./slide15Assets/slide15_tail.mp3";
 
+import IguanaMystery_Slide3_Intro from "./slide3Assets/IguanaMystery_Slide3_Intro.mp3";
+import IguanaMystery_Slide3_Bird from "./slide3Assets/IguanaMystery_Slide3_Bird.mp3";
+import IguanaMystery_Slide3_Pumice from "./slide3Assets/IguanaMystery_Slide3_Pumice.mp3";
+import IguanaMystery_Slide3_Vegetation from "./slide3Assets/IguanaMystery_Slide3_Vegetation.mp3";
+import IguanaMystery_Slide3_Driftwood from "./slide3Assets/IguanaMystery_Slide3_Driftwood.mp3";
+import IguanaMystery_Slide3_Ship from "./slide3Assets/IguanaMystery_Slide3_Ship.mp3";
+
 import iguanaMysterySlide7 from "./IguanaMystery_Slide7.mp3";
 import iguanaMysterySlide8 from "./IguanaMystery_Slide8.mp3";
 
-export const iguanaAssets = {
-  // slide 15 assets
-  slide15_initial,
-  slide15_head,
-  slide15_face,
-  slide15_spikes,
-  slide15_stomach,
-  slide15_tail,
-  iguanaMysterySlide7,
-  iguanaMysterySlide8,
+// prettier-ignore
+export const iguanaAssets = {  
+  slide15_initial, slide15_head, slide15_face, slide15_spikes, slide15_stomach, slide15_tail,
+  iguanaMysterySlide7, iguanaMysterySlide8,
+  IguanaMystery_Slide3_Intro, 
+  IguanaMystery_Slide3_Bird, IguanaMystery_Slide3_Pumice,
+  IguanaMystery_Slide3_Vegetation, IguanaMystery_Slide3_Driftwood, IguanaMystery_Slide3_Ship
 };
