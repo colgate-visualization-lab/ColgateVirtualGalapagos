fbba292611af98bd7c15dc04c04ef54cb1475313
Desmond Tuiyot <dtuiyot@gmail.com>
2020-10-20 11:33:35 -0400

    Added video selector button animation - it slides up when hovering over video (very basic). Replaced the selector button to simpler version

---
+++ src/components/Slide3/Slide3BottomVideoSelector.js
@@ -0,0 +1,24 @@
+import React, { useEffect } from "react";
+import classes from "./Slide3.css";
+
+
+const Slide3BottomVideoSelector = ({data, onSrcChange}) => {
+    useEffect(()=>{
+        console.log(data[0].cardTitle)
+    })
+
+
+
+    return (
+    <div className={classes.bottomVideoSelector}>
+        {data.map((datum)=>(
+            <button class={`${classes.videoSelectorButton} ${classes.button} `}  onClick={() => {
+                onSrcChange(datum);
+                console.log(datum.cardTitle);
+              }} type="button" key={datum.id}>{datum.cardTitle}</button>
+        ))}
+    </div>
+    )
+}
+
+export default Slide3BottomVideoSelector;
