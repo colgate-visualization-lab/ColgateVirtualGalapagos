c043243f4a78d399bb09d7e204fdcb605924d906
Desmond Tuiyot <dtuiyot@gmail.com>
2020-10-22 15:09:41 -0400

    refactored slide 8 slide

---
+++ src/components/IguanaSlide8/IguanaSlide8.js
@@ -0,0 +1,57 @@
+import React from "react";
+import PropTypes from "prop-types";
+import {
+  ReactCompareSlider,
+  ReactCompareSliderImage,
+} from "react-compare-slider";
+import Popup from "../../components/Popup/Popup";
+
+const IguanaSlide8 = ({ content }) => {
+  return (
+    <div>
+      <ReactCompareSlider
+        onlyHandleDraggable={true}
+        itemOne={
+          <div>
+            <ReactCompareSliderImage
+              src={content.url1}
+              alt="adult marine iguana with baby"
+            />
+            <Popup
+              description={content.popupText.landIguanaHead}
+              top="20%"
+              left="50%"
+            />
+            <Popup
+              description={content.popupText.landIguanaBody}
+              top="20%"
+              left="74%"
+            />
+          </div>
+        }
+        itemTwo={
+          <div>
+            <ReactCompareSliderImage
+              src={content.url2}
+              alt="smiling land iguana"
+            />
+            <Popup
+              description={content.popupText.marineIguanaBody}
+              top="25%"
+              left="13%"
+            />
+            <Popup
+              description={content.popupText.marineIguanaTail}
+              top="35%"
+              left="40%"
+            />
+          </div>
+        }
+      />
+    </div>
+  );
+};
+
+IguanaSlide8.propTypes = {};
+
+export default IguanaSlide8;
