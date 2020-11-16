7cbf194400895f5f458684672baf062ba4257854
Desmond Tuiyot <dtuiyot@gmail.com>
2020-11-16 15:17:38 -0500

    Added GeneMutationsTab which will hold buttons corresponding to mutations that the user found. Also changed GeneTextbox to GeneMutationsTextbox

---
+++ src/components/GeneMutationsTab/GeneMutationsTab.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const GeneMutationsTab = ({}) => {
+  return <h1>This is the selected genes tab</h1>;
+};
+
+export default GeneMutationsTab;
--- src/components/GeneTextbox/GeneTextbox.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const GeneTextbox = ({ geneDescription }) => {
-  return <h1>{geneDescription}</h1>;
-};
-
-export default GeneTextbox;
