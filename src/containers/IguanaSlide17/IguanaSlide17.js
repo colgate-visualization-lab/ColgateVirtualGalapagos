9300b775d76a0e0b495131b015627974b0ea1ea9
Desmond Tuiyot <dtuiyot@gmail.com>
2020-11-16 15:02:06 -0500

    changed slide17 width and alignment + changed GeneTextbox alignment
    Former-commit-id: 3984e150b5bd8496cb66b813a8ee186d6002f48a [formerly c831b5959d257762f5a2839f43c1adcafee7faca] [formerly cfc3cbb3ff2ab9edc1138b67cadbdec5623d2e08 [formerly 4bca2e081222b0d60dc96d9e21715042d4e325f1]]
    Former-commit-id: ff46bd487aa50d0ea3fad626a1f982f76da7c509 [formerly 0e5e9e05fe71d91bd3b308c203a7474c73f11545]
    Former-commit-id: 348cd15e4974ebd4358d0b07ed7e60bfd76f0eb5

--- src/containers/IguanaSlide17/IguanaSlide17.js
+++ src/containers/IguanaSlide17/IguanaSlide17.js
@@ -12,7 +12,7 @@ export const IguanaSlide17 = ({ content }) => {
         <h1>Find the Mutation!</h1>
       </div>
       <div className={classes.dummyDnaDiv}>11234dsfgdsfgfd</div>
-      <div>
+      <div style={{ alignSelf: "start" }}>
         <GeneTextbox geneDescription={content.geneDescriptions[selectedGene]} />
       </div>
       <div>11234dsfgdsfgfd</div>
