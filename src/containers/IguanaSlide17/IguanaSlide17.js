54d7a4b25e5ddabe75668a4c76d8ceecd7ed41b5
Desmond Tuiyot <dtuiyot@gmail.com>
2020-11-16 14:59:24 -0500

    added sample data for gene descriptions. Created selectedGene state to control what GeneTextbox shows as its text

--- src/containers/IguanaSlide17/IguanaSlide17.js
+++ src/containers/IguanaSlide17/IguanaSlide17.js
@@ -1,9 +1,11 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import GeneTextbox from "../../components/GeneTextbox";
 import classes from "./IguanaSlide17.css";
 
 export const IguanaSlide17 = ({ content }) => {
+  const [selectedGene, setSelectedGene] = useState("scaleColorGene");
+
   return (
     <div className={classes.container}>
       <div>
@@ -11,7 +13,7 @@ export const IguanaSlide17 = ({ content }) => {
       </div>
       <div className={classes.dummyDnaDiv}>11234dsfgdsfgfd</div>
       <div>
-        <GeneTextbox geneDescription="This is a gene descriptor" />
+        <GeneTextbox geneDescription={content.geneDescriptions[selectedGene]} />
       </div>
       <div>11234dsfgdsfgfd</div>
     </div>
