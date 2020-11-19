269952c5b547b92da997e4612e564f0b253b4df4
Desmond Tuiyot <dtuiyot@gmail.com>
2020-11-19 17:06:42 -0500

    colors are now passed imported from a constants file and passed down as props to BasePairGroup

--- src/components/BasePairs/constants.js
+++ src/components/BasePairs/constants.js
@@ -1,8 +1,8 @@
 export const BASE = {
-  ADENINE: { name: "adenine", color: "#601A4A" },
-  THYMINE: { name: "thymine", color: "#EE442F" },
-  GUANINE: { name: "guanine", color: "#63ACBE" },
-  CYTOSINE: { name: "cytosine", color: "#F9F4EC" },
+  ADENINE: { name: "adenine", color: "#601A4A", complement: "#EE442F" },
+  THYMINE: { name: "thymine", color: "#EE442F", complement: "#601A4A" },
+  GUANINE: { name: "guanine", color: "#63ACBE", complement: "#F9F4EC" },
+  CYTOSINE: { name: "cytosine", color: "#F9F4EC", complement: "#63ACBE" },
 };
 
 // specify only top half of the base pair, in groups of 5
