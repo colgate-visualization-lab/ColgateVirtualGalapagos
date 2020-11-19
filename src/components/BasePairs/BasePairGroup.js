e81a709daf278193cc857cba62c814539bf0f73c
Desmond Tuiyot <dtuiyot@gmail.com>
2020-11-19 16:50:46 -0500

    added constants.js file to BasePairs (will probably refactor later). Defined BASES and BASE_PAIRS constants that will serve as input to create the dna
    Former-commit-id: 47464dd74b043b9c4bf638d747706156c53efd9a [formerly 93c457d0650da33ac2a15967b21f54c795528a6e] [formerly 69a1e95f4ce25ed5b86abc21812a85d93ffb9cce [formerly 926ee41eec3c5de4d4502efb7f1719339a6984d5]]
    Former-commit-id: 87b90150ce54a49b1db8fee8bbf8bfbc95e38cd9 [formerly 0921b7a11a778c83fbea982125cb3ba51cf8e5eb]
    Former-commit-id: b5a9b860a6b76aded9b55d56dd8d4f3cde8beac2

--- src/components/BasePairs/BasePairGroup.js
+++ src/components/BasePairs/BasePairGroup.js
@@ -1,21 +1,18 @@
 import React from "react";
-import BasePairs from "./BasePairs";
 
 const BasePairGroup = ({ xTranslateDistance, classes }) => {
   //prettier-ignore
   const basePairProperties = [
-    { id:"leftTop", x:"31.24", y:"10.98", width:"2.48",height:"4.83",},
-    { id:"leftBottom", x:"31.24", y:"17", width:"2.48",height:"4.83",},
-    { id:"midLeftTop", x:"36.8", y:"7.45", width:"2.48",height:"8.24",},
-    { id:"midLeftBottom", x:"36.8", y:"16.88", width:"2.48",height:"8.24",},
-    { id:"midTop", x:"42.08", y:"3.76", width:"2.48",height:"11.93",},
-    { id:"midBottom", x:"42.08", y:"16.88", width:"2.48",height:"11.93",},
-
-    { id:"midRightTop", x:"47.46", y:"7.52", width:"2.48",height:"8.24",},
-    { id:"midRightBottom", x:"47.46", y:"16.95", width:"2.48",height:"8.24",},
-    { id:"rightTop", x:"52.32", y:"10.98", width:"2.48",height:"4.83",},
-    { id:"rightBottom", x: "52.32",  y: "17", width: "2.48",  height: "4.83",},
-    
+    { id:"leftTop", x:"31.6", y:"11", width:"3",height:"5",},
+    { id:"leftBottom", x:"31.6", y:"17", width:"3",height:"5",},
+    { id:"midLeftTop", x:"36.6", y:"7", width:"3",height:"9",},
+    { id:"midLeftBottom", x:"36.6", y:"17", width:"3",height:"9",},
+    { id:"midTop", x:"41.6", y:"5", width:"3",height:"11",},
+    { id:"midBottom", x:"41.6", y:"17", width:"3",height:"11",},
+    { id:"midRightTop", x:"46.6", y:"7", width:"3",height:"9",},
+    { id:"midRightBottom", x:"46.6", y:"17", width:"3",height:"9",},
+    { id:"rightTop", x:"51.6", y:"10.98", width:"3",height:"5",},
+    { id:"rightBottom", x: "51.6",  y: "17", width: "3",  height: "5",},
   ];
 
   return (
@@ -30,109 +27,6 @@ const BasePairGroup = ({ xTranslateDistance, classes }) => {
           height={base.height}
         />
       ))}
-    </g>
-  );
-
-  return (
-    <g id="basePairGroup" transform={`translate(${xTranslateDistance})`}>
-      {/* <BasePair
-          id="rightBottom"
-          pairClass={classes.cls1}
-          x="52.32"
-          y="17"
-          width="2.48"
-          height="4.83"
-        /> */}
-      {basePairProperties.map((base) => (
-        <rect
-          id={base.id}
-          className={classes.cls1}
-          x={base.x}
-          y={base.y}
-          width={base.width}
-          height={base.height}
-        />
-      ))}
-      <rect
-        id="rightBottom"
-        className={classes.cls1}
-        x="52.32"
-        y="17"
-        width="2.48"
-        height="4.83"
-      />
-      <rect
-        id="rightTop"
-        className={classes.cls2}
-        x="52.32"
-        y="10.98"
-        width="2.48"
-        height="4.83"
-      />
-      <rect
-        id="midRightBottom"
-        className={classes.cls3}
-        x="47.46"
-        y="16.95"
-        width="2.48"
-        height="8.62"
-      />
-      <rect
-        id="midRightTop"
-        className={classes.cls4}
-        x="47.46"
-        y="7.52"
-        width="2.48"
-        height="8.24"
-      />
-      <rect
-        id="midBottom"
-        className={classes.cls4}
-        x="42.08"
-        y="16.88"
-        width="2.48"
-        height="12.08"
-      />
-      <rect
-        id="midTop"
-        className={classes.cls3}
-        x="42.08"
-        y="3.76"
-        width="2.48"
-        height="11.93"
-      />
-      <rect
-        id="midLeftBottom"
-        className={classes.cls2}
-        x="36.8"
-        y="16.88"
-        width="2.48"
-        height="8.62"
-      />
-      <rect
-        id="midLeftTop"
-        className={classes.cls1}
-        x="36.8"
-        y="7.45"
-        width="2.48"
-        height="8.24"
-      />
-      <rect
-        id="leftBottom"
-        className={classes.cls3}
-        x="31.24"
-        y="17"
-        width="2.48"
-        height="4.83"
-      />
-      <rect
-        id="leftTop"
-        className={classes.cls4}
-        x="31.24"
-        y="10.98"
-        width="2.48"
-        height="4.83"
-      />
     </g>
   );
 };
