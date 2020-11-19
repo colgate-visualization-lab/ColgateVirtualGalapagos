a46fb21df12b0e3d0e3ad072b5ae4a8a097923de
Desmond Tuiyot <dtuiyot@gmail.com>
2020-11-19 17:06:42 -0500

    colors are now passed imported from a constants file and passed down as props to BasePairGroup
    Former-commit-id: 6367d5ebb3030a2fe78d5aac671b03e21c14dea3 [formerly 0e9f0aed668a635ef83ebec0b242608c320d110a] [formerly 88a39b85c8f6d5896d1a224caa560d9782564018 [formerly 269952c5b547b92da997e4612e564f0b253b4df4]]
    Former-commit-id: d83c45973413b84ef874cfd30729c433398a8717 [formerly b555eff0711216de31adb2ce5b046fdb024ee4e6]
    Former-commit-id: 6a398060e318c4a5e7beb2680147996f1e563a77

--- src/components/BasePairs/BasePairGroup.js
+++ src/components/BasePairs/BasePairGroup.js
@@ -1,25 +1,35 @@
-import React from "react";
+import React, { useEffect } from "react";
 
-const BasePairGroup = ({ xTranslateDistance, classes }) => {
+const BasePairGroup = ({ xTranslateDistance, classes, basePairs }) => {
   //prettier-ignore
   const basePairProperties = [
-    { id:"leftTop", x:"31.6", y:"11", width:"3",height:"5",},
-    { id:"leftBottom", x:"31.6", y:"17", width:"3",height:"5",},
-    { id:"midLeftTop", x:"36.6", y:"7", width:"3",height:"9",},
-    { id:"midLeftBottom", x:"36.6", y:"17", width:"3",height:"9",},
-    { id:"midTop", x:"41.6", y:"5", width:"3",height:"11",},
-    { id:"midBottom", x:"41.6", y:"17", width:"3",height:"11",},
-    { id:"midRightTop", x:"46.6", y:"7", width:"3",height:"9",},
-    { id:"midRightBottom", x:"46.6", y:"17", width:"3",height:"9",},
-    { id:"rightTop", x:"51.6", y:"10.98", width:"3",height:"5",},
-    { id:"rightBottom", x: "51.6",  y: "17", width: "3",  height: "5",},
+    { id:"leftTop", x:"31.6", y:"11", width:"3",height:"5", fill:basePairs[0].color},
+    { id:"leftBottom", x:"31.6", y:"17", width:"3",height:"5", fill:basePairs[0].complement},
+
+    { id:"midLeftTop", x:"36.6", y:"7", width:"3",height:"9", fill:basePairs[1].color},
+    { id:"midLeftBottom", x:"36.6", y:"17", width:"3",height:"9",fill:basePairs[1].complement},
+
+    { id:"midTop", x:"41.6", y:"5", width:"3",height:"11", fill:basePairs[2].color},
+    { id:"midBottom", x:"41.6", y:"17", width:"3",height:"11",fill:basePairs[2].complement},
+
+    { id:"midRightTop", x:"46.6", y:"7", width:"3",height:"9", fill:basePairs[3].color},
+    { id:"midRightBottom", x:"46.6", y:"17", width:"3",height:"9",fill:basePairs[3].complement},
+
+    { id:"rightTop", x:"51.6", y:"10.98", width:"3",height:"5", fill:basePairs[4].color},
+    { id:"rightBottom", x: "51.6",  y: "17", width: "3",  height: "5",fill:basePairs[4].complement},
   ];
+
+  useEffect(() => {
+    console.log(basePairProperties);
+  });
 
   return (
     <g id="basePairGroup" transform={`translate(${xTranslateDistance})`}>
       {basePairProperties.map((base) => (
         <rect
+          key={base.id}
           id={base.id}
+          style={{ fill: base.fill }}
           className={classes.cls1}
           x={base.x}
           y={base.y}
