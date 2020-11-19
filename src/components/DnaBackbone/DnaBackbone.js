7c2fb826d3de5402c636ff2a97bbb450d41ee1c2
Desmond Tuiyot <dtuiyot@gmail.com>
2020-11-18 19:44:09 -0500

    added DnaBackbone, which renders dna backbone, whose length is changeable
    Former-commit-id: 9df88fd8469e0c3d62218c71848f3adee5a23918 [formerly e65af0be8c436fa0782285f8af32cfea5bcced6e] [formerly 3407c8c5a416b1f6b0fcdb133c501567748d072c [formerly 70536725910b4711613668e27b4f661ce0b5c845]]
    Former-commit-id: 350c0ea69e9b0f11539fa98b4ace1ae93100c842 [formerly c414663380d0de02b815453c1ae4aeb905dc7a05]
    Former-commit-id: fd5132bd7eba27b87e6fd9f1deb1cc09be3594a3

---
+++ src/components/DnaBackbone/DnaBackbone.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+import DnaBackbonePiece from "../DnaBackbonePiece";
+
+const DnaBackbone = ({ numGroups }) => {
+  let xTranslateDistances = [];
+
+  const createTransformDistances = () => {
+    for (let x = 0; x < numGroups; x++) {
+      xTranslateDistances.push(43.2 * x);
+    }
+  };
+  createTransformDistances();
+
+  return (
+    <g id="dnaBackbone">
+      {xTranslateDistances.map((distance, index) => (
+        <DnaBackbonePiece key={index} xTranslateDistance={distance} />
+      ))}
+    </g>
+  );
+};
+export default DnaBackbone;
