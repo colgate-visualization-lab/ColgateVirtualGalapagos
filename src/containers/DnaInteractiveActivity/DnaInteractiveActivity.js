38c3342054837d90f163eaf7f6a6aa9628b68e08
Desmond Tuiyot <dtuiyot@gmail.com>
2020-11-19 12:59:55 -0500

    wrapped dna and its title in a div and rendered two of them on a page. They are still static

--- src/containers/DnaInteractiveActivity/DnaInteractiveActivity.js
+++ src/containers/DnaInteractiveActivity/DnaInteractiveActivity.js
@@ -4,9 +4,15 @@ import DnaBackbone from "../../components/DnaBackbone";
 import BasePairs from "../../components/BasePairs";
 import classes from "./DnaInteractiveActivity.css";
 
-const DnaInteractiveActivity = () => {
+const DnaInteractiveActivity = ({ label }) => {
   const spBackboneStyle = {
     fill: "#f7931e",
+    border: "1px solid white",
+  };
+
+  const svgStyle = {
+    border: "1px solid white",
+    float: "left",
   };
 
   const basePairsStyle = {
@@ -18,14 +24,35 @@ const DnaInteractiveActivity = () => {
 
   const numGroups = 6;
 
+  const viewBox = [10, 0, 282.32, 32.56];
+  const viewBox2 = [10, 0, 846.96, 97.68];
+
   return (
-    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 302.32 32.56">
-      <title>dna_extended</title>
-      <g id="dna">
-        <DnaBackbone numGroups={numGroups + 1} />
-        <BasePairs classes={classes} numGroups={numGroups} />
-      </g>
-    </svg>
+    <div className={classes.dnaDiv}>
+      <h2
+        style={{ flexBasis: "10%", fontSize: "1.4rem", paddingRight: "20px" }}
+      >
+        {label}
+      </h2>
+      <div style={{ minWidth: "400px" }}>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox={viewBox}
+          // style={svgStyle}
+          preserveAspectRatio="xMidYMid meet"
+          width="1000"
+          height="115.34"
+        >
+          <g
+            id="dna"
+            // transform={`translate(0,${yTranslateDistance})`}
+          >
+            <DnaBackbone numGroups={numGroups + 1} />
+            <BasePairs classes={classes} numGroups={numGroups} />
+          </g>
+        </svg>
+      </div>
+    </div>
   );
 };
 
