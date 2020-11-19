c0f0a7997104477934e69ac480ef341fbf49186b
Desmond Tuiyot <dtuiyot@gmail.com>
2020-11-19 16:05:48 -0500

    changed layouts, styling for slide 17 + refactored BasePairGroup

--- src/components/BasePairs/BasePairGroup.js
+++ src/components/BasePairs/BasePairGroup.js
@@ -2,6 +2,37 @@ import React from "react";
 import BasePairs from "./BasePairs";
 
 const BasePairGroup = ({ xTranslateDistance, classes }) => {
+  //prettier-ignore
+  const basePairProperties = [
+    { id:"leftTop", x:"31.24", y:"10.98", width:"2.48",height:"4.83",},
+    { id:"leftBottom", x:"31.24", y:"17", width:"2.48",height:"4.83",},
+    { id:"midLeftTop", x:"36.8", y:"7.45", width:"2.48",height:"8.24",},
+    { id:"midLeftBottom", x:"36.8", y:"16.88", width:"2.48",height:"8.24",},
+    { id:"midTop", x:"42.08", y:"3.76", width:"2.48",height:"11.93",},
+    { id:"midBottom", x:"42.08", y:"16.88", width:"2.48",height:"11.93",},
+
+    { id:"midRightTop", x:"47.46", y:"7.52", width:"2.48",height:"8.24",},
+    { id:"midRightBottom", x:"47.46", y:"16.95", width:"2.48",height:"8.24",},
+    { id:"rightTop", x:"52.32", y:"10.98", width:"2.48",height:"4.83",},
+    { id:"rightBottom", x: "52.32",  y: "17", width: "2.48",  height: "4.83",},
+    
+  ];
+
+  return (
+    <g id="basePairGroup" transform={`translate(${xTranslateDistance})`}>
+      {basePairProperties.map((base) => (
+        <rect
+          id={base.id}
+          className={classes.cls1}
+          x={base.x}
+          y={base.y}
+          width={base.width}
+          height={base.height}
+        />
+      ))}
+    </g>
+  );
+
   return (
     <g id="basePairGroup" transform={`translate(${xTranslateDistance})`}>
       {/* <BasePair
@@ -12,6 +43,16 @@ const BasePairGroup = ({ xTranslateDistance, classes }) => {
           width="2.48"
           height="4.83"
         /> */}
+      {basePairProperties.map((base) => (
+        <rect
+          id={base.id}
+          className={classes.cls1}
+          x={base.x}
+          y={base.y}
+          width={base.width}
+          height={base.height}
+        />
+      ))}
       <rect
         id="rightBottom"
         className={classes.cls1}
