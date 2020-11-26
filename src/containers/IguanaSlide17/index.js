0db5c41600a0892eb9515eb7ac4211c7e6de768c
Desmond Tuiyot <dtuiyot@gmail.com>
2020-11-26 10:43:48 -0500

    now passing down IguanaSlide17 onClick through context instead of prop drilling it

--- src/containers/IguanaSlide17/index.js
+++ src/containers/IguanaSlide17/index.js
@@ -1,4 +1,6 @@
 import IguanaSlide15 from "../IguanaSlide15/IguanaSlide15";
 import { IguanaSlide17 } from "./IguanaSlide17";
+import { OnBaseClickContext } from "./OnBaseClickContext";
 
 export default IguanaSlide17;
+export { OnBaseClickContext };
