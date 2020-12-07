6257a132a3c84dabe7e5e5315c3a8eef6cc6933c
Desmond Tuiyot <dtuiyot@gmail.com>
2020-12-07 11:45:23 -0500

    360_comparison slides - removed div around Iframe and applied class directly to them

--- src/containers/IguanaSlide3/IguanaSlide3VideoSelector.js
+++ src/containers/IguanaSlide3/IguanaSlide3VideoSelector.js
@@ -3,26 +3,44 @@ import PropTypes from "prop-types";
 
 import classes from "./IguanaSlide3.css";
 
-const Slide3VideoSelector = ({ data, onSrcChange }) => {
+const Slide3VideoSelector = ({
+  data,
+  onSrcChange,
+  watched,
+  videoInteractionDisabled,
+}) => {
   useEffect(() => {
     console.log(data[0].cardTitle);
   });
 
   return (
     <div className={classes.videoSelector}>
-      {data.map((datum) => (
-        <button
-          className={`${classes.videoSelectorButton} ${classes.button} `}
-          onClick={() => {
-            onSrcChange(datum);
-            console.log(datum.cardTitle);
-          }}
-          type="button"
-          key={datum.id}
-        >
-          {datum.cardTitle}
-        </button>
-      ))}
+      {videoInteractionDisabled && (
+        <p className={classes.title}>Choose a Hypothesis to Test</p>
+      )}
+
+      <div className={classes.videoSelectorButtonGroup}>
+        {data.map((datum) => (
+          <div key={datum.id}>
+            <button
+              className={`${classes.videoSelectorButton} ${classes.button} `}
+              onClick={() => {
+                onSrcChange(datum);
+                console.log(datum.cardTitle);
+              }}
+              type="button"
+            >
+              {datum.cardTitle}
+            </button>
+
+            {videoInteractionDisabled && watched.has(datum.id) && (
+              <div className={classes.watchedDiv}>
+                <p className={classes.watchedIndicator}>Watched</p>
+              </div>
+            )}
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
--- src/containers/Layout/Layout.js
+++ src/containers/Layout/Layout.js
@@ -1,24 +1,30 @@
-import React, { Component, Fragment } from "react"
-import NavBar from "../../components/Navigation/NavBar/NavBar"
-import Modal from "../../components/UI/Modal/Modal"
-import Backpack from "../Backpack/Backpack"
+import React, { Component, Fragment } from "react";
+import NavBar from "../../components/Navigation/NavBar/NavBar";
+import Modal from "../../components/UI/Modal/Modal";
+import Backpack from "../Backpack/Backpack";
 //import BackgroundVideo from "../../components/BackgroundVideo/BackgroundVideo.js"
 
 class Layout extends Component {
-
-	render(){
-		return (
-			<Fragment>
-				<NavBar />
-				<main style={{height: "100%", overflow: "hidden", position: "relative", flex: "1"}}>
-					{this.props.children}
-					<Modal>
-						<Backpack />
-					</Modal>
-				</main>
-			</Fragment>
-		)
-	}
+  render() {
+    return (
+      <Fragment>
+        <NavBar />
+        <main
+          style={{
+            height: "100%",
+            overflow: "hidden",
+            position: "relative",
+            flex: "1",
+          }}
+        >
+          {this.props.children}
+          <Modal>
+            <Backpack />
+          </Modal>
+        </main>
+      </Fragment>
+    );
+  }
 }
 
-export default Layout
+export default Layout;
