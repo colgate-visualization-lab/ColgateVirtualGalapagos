debc3d058b348f86aa434cb7669357b540cc9b93
Desmond Tuiyot <dtuiyot@gmail.com>
2020-12-11 12:38:33 -0500

    changed parts of MainContent.js to use Material UI - specifically video and 360video components
    Former-commit-id: 4a3f624a5d0eb8f5be7adab04d7a2c59fc27059b [formerly 2345f235f95b7803f453967b57e495d92ac07a07] [formerly 3cb7a893d33ea310dd77e5c61d4773ebc8b05db6 [formerly 08c6446cae67566b0194e9c5f1f0014029d8c6ea]]
    Former-commit-id: 191f4291ab868f1e1039cc4ba2ed06154a319ab4 [formerly e19fbbc5b451d009428fc586fe8ed41e76fb24da]
    Former-commit-id: 44bea0f300abc2e561c0ea2241fecc4caf9e6a2d

--- src/containers/ControlButtons/ControlButtons.js
+++ src/containers/ControlButtons/ControlButtons.js
@@ -1,6 +1,8 @@
 import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
+import Button from "@material-ui/core/Button";
+import Grid from "@material-ui/core/Grid";
 
 import { Back, Next } from "../../assets/VolcanoModule";
 import classes from "./ControlButtons";
@@ -9,40 +11,62 @@ import classes from "./ControlButtons";
 //  conditionally render - back button won't render on first slide
 export default function ControlButtons(props) {
   return (
-    <div>
-      {props.hasPrev && (
-        <Link to={props.prevSlide}>
-          <img
-            className="prev"
-            src={Back}
-            style={{
-              width: props.width,
-              height: "auto",
-              position: "absolute",
-              zIndex: 10,
-              bottom: props.bottom,
-              left: props.left,
-            }}
-          />
-        </Link>
-      )}
-      {props.hasNext && (
-        <Link to={props.nextSlide}>
-          <img
-            className="next"
-            src={Next}
-            style={{
-              width: props.width,
-              height: "auto",
-              position: "absolute",
-              zIndex: 10,
-              bottom: props.bottom,
-              right: props.right,
-            }}
-          />
-        </Link>
-      )}
-    </div>
+    <>
+      <Grid item xs={1}>
+        {props.hasPrev && (
+          <Button
+            color="primary"
+            variant="outlined"
+            size="small"
+            component={Link}
+            to={props.prevSlide}
+          >
+            Previous
+          </Button>
+          // <Link to={props.prevSlide}>
+          //   <img
+          //     className="prev"
+          //     src={Back}
+          //     style={{
+          //       width: props.width,
+          //       height: "auto",
+          //       position: "absolute",
+          //       zIndex: 10,
+          //       bottom: props.bottom,
+          //       left: props.left,
+          //     }}
+          //   />
+          // </Link>
+        )}
+      </Grid>
+      <Grid item xs={1}>
+        {props.hasNext && (
+          <Button
+            color="primary"
+            variant="outlined"
+            size="small"
+            component={Link}
+            to={props.nextSlide}
+          >
+            Next
+          </Button>
+          // <Link to={props.nextSlide}>
+          //   <img
+          //     className="next"
+          //     src={Next}
+          //     style={{
+          //       width: props.width,
+          //       height: "auto",
+          //       position: "absolute",
+          //       zIndex: 10,
+          //       bottom: props.bottom,
+          //       right: props.right,
+          //     }}
+          //   />
+          // </Link>
+        )}
+      </Grid>
+    </>
   );
 }
 
