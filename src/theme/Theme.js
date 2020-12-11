260e9460ee3783ebf1e07906704533cd977a9e0a
Desmond Tuiyot <dtuiyot@gmail.com>
2020-12-11 13:29:39 -0500

    Added Theme file, and created new NavBar based on Material UI.

---
+++ src/theme/Theme.js
@@ -0,0 +1,17 @@
+import { createMuiTheme } from "@material-ui/core/styles";
+
+export default createMuiTheme({
+  palette: {
+    primary: {
+      main: "#000",
+    },
+    secondary: {
+      main: "#fff",
+    },
+  },
+  typography: {
+    h6: {
+      fontWeight: 300,
+    },
+  },
+});
