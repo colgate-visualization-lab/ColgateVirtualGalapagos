66c3a4aab2861ef0badefa8f28e840963474fb8e
Desmond Tuiyot <dtuiyot@gmail.com>
2020-12-11 14:22:12 -0500

    added tabs for the various links - haven't found an equivalent for the backpack dropdown.

--- src/components/Navigation/NavBarMaterialUI/NavBar.js
+++ src/components/Navigation/NavBarMaterialUI/NavBar.js
@@ -2,11 +2,27 @@ import React from "react";
 import { makeStyles } from "@material-ui/styles";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
-import Typography from "@material-ui/core/Typography";
+import Tabs from "@material-ui/core/Tabs";
+import Tab from "@material-ui/core/Tab";
+import { Link } from "react-router-dom";
+
+import { logo } from "../../../assets/Misc";
+import { transform } from "../../../assets/VolcanoModule";
 
 const useStyles = makeStyles((theme) => ({
   toolbarMargin: {
     ...theme.mixins.toolbar,
+  },
+  logo: {
+    height: "2.5em",
+  },
+  tabContainer: {
+    marginLeft: "auto",
+  },
+  tab: {
+    ...theme.typography.tab,
+    minWidth: 10,
+    marginLeft: "25px", // we use fixed units here to create consistency regardless of screen size
   },
 }));
 
@@ -15,8 +31,34 @@ const NavBar = () => {
   return (
     <React.Fragment>
       <AppBar position="fixed" color="primary">
-        <Toolbar>
-          <Typography variant="h6">The Virtual Galapagos</Typography>
+        <Toolbar disableGutters>
+          <img alt="website logo" className={classes.logo} src={logo} />
+          <Tabs className={classes.tabContainer}>
+            <Tab
+              className={classes.tab}
+              component={Link}
+              to="/home"
+              label="Mysteries"
+            />
+            <Tab
+              className={classes.tab}
+              component={Link}
+              to="/gallery"
+              label="Gallery"
+            />
+            <Tab
+              className={classes.tab}
+              component={Link}
+              to="/authorization"
+              label="Sign In"
+            />
+            <Tab
+              className={classes.tab}
+              component={Link}
+              to="/fieldbook"
+              label="Fieldbook"
+            />
+          </Tabs>
         </Toolbar>
       </AppBar>
       <div className={classes.toolbarMargin} />
--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -1,4 +1,4 @@
-// import NavBar from "./NavBar/NavBar"
+// import NavBar from "./NavBar/NavBar";
 import NavBar from "./NavBarMaterialUI/NavBar";
 
 export default NavBar;
