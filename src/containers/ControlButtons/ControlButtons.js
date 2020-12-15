1b8db858107fba814dd562ea1a5e33b3d19c0772
Desmond Tuiyot <dtuiyot@gmail.com>
2020-12-15 14:38:30 -0500

    refactored control button - ControlButtons now returns a Grid Item that's placed on the outer Grid Container

--- src/containers/ControlButtons/ControlButtons.js
+++ src/containers/ControlButtons/ControlButtons.js
@@ -3,15 +3,33 @@ import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
 import Button from "@material-ui/core/Button";
 import Grid from "@material-ui/core/Grid";
+import { makeStyles } from "@material-ui/styles";
 
-import { Back, Next } from "../../assets/VolcanoModule";
-import classes from "./ControlButtons";
+// import { Back, Next } from "../../assets/VolcanoModule";
+
+const useStyles = makeStyles((theme) => ({
+  // BUTTON STYLES
+  buttonsContainer: {
+    position: "relative",
+    width: "90vw",
+    color: "primary",
+    // backgroundColor: "lawngreen",
+    height: theme.typography.pxToRem(30),
+  },
+}));
 
 // changed ControlButtons to use Links instead of button - they also
 //  conditionally render - back button won't render on first slide
 export default function ControlButtons(props) {
+  const classes = useStyles();
   return (
-    <>
+    <Grid
+      item
+      xs={12}
+      container
+      justify="space-between"
+      className={classes.buttonsContainer}
+    >
       <Grid item xs={1}>
         {props.hasPrev && (
           <Button
@@ -38,7 +56,7 @@ export default function ControlButtons(props) {
           </Button>
         )}
       </Grid>
-    </>
+    </Grid>
   );
 }
 
