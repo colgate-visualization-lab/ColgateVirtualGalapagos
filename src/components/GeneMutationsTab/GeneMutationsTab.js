7c2d3bc0ec2e77fcbdbbd7b1a9e6a43f51e6286c
Desmond Tuiyot <dtuiyot@gmail.com>
2021-01-14 15:31:45 -0500

    restarting the dna slide - have done the basic layout of the different components in slide17

--- src/components/GeneMutationsTab/GeneMutationsTab.js
+++ src/components/GeneMutationsTab/GeneMutationsTab.js
@@ -1,26 +1,34 @@
 import React, { useEffect } from "react";
+import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
 
-import classes from "./GeneMutationsTab.css";
+import GeneDescription from "./GeneDescription";
+
+const useStyles = makeStyles((theme) => ({
+  foundCount: {
+    color: "khaki",
+  },
+}));
 
 const GeneMutationsTab = ({ geneMutations, onClick }) => {
-  // useEffect(() => {
-  //   console.log(geneMutations);
-  // });
+  const classes = useStyles();
+  const geneDescription =
+    "Marine iguanas have black scales, while green iguanas have bright green scales";
   return (
-    <>
-      {Object.keys(geneMutations).map((mutation) => (
-        <button
-          onClick={() => {
-            onClick(mutation);
-          }}
-          key={geneMutations[mutation].id}
-          type="button"
-          className={classes.button}
-        >
-          {geneMutations[mutation].name}
-        </button>
-      ))}
-    </>
+    <Grid container item xs={12}>
+      <Grid item xs={12}>
+        <Typography variant="h6" component="h4">
+          Mutations Found: <span className={classes.foundCount}>4/10</span>
+        </Typography>
+      </Grid>
+      <Grid item xs={12}>
+        <GeneDescription
+          geneName="Scale Color Gene"
+          geneDescription={geneDescription}
+        />
+      </Grid>
+    </Grid>
   );
 };
 
