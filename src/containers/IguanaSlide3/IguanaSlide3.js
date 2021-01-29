67b533aa3c2c9f89a22ababfed18855e11a51108
Desmond Tuiyot <dtuiyot@gmail.com>
2021-01-29 15:08:58 -0500

    got the popup buttons working - they pop up onMouseMove
    Former-commit-id: 6e616a8d2ebf64da2503c6eb889ebccae596f968 [formerly d6aa17700c49430f088041a590ea82d9506e50e6] [formerly 663f95969821c5bea2b2085b74e41a8872e2cc67 [formerly 346467feb4f6dd1700218b899fe296152dfa0847]]
    Former-commit-id: c5d820401364f59906d066d2d8934f0108a67885 [formerly 64a8ac0ba593d9ec72e9ae4e5d903e4477a3432d]
    Former-commit-id: 5193cf4b4682f4dbf7a77fa27bcbec707fae9fc3

--- src/containers/IguanaSlide3/IguanaSlide3.js
+++ src/containers/IguanaSlide3/IguanaSlide3.js
@@ -1,61 +1,112 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useCallback } from "react";
 import ReactPlayer from "react-player";
+import Slide from "@material-ui/core/Slide";
 import { makeStyles } from "@material-ui/core/styles";
 
-import IntermissionScreen from "./IntermissionScreen";
+// import IntermissionScreen from "./IntermissionScreen";
 import IntermissionModal from "./IntermissionModal";
+import PopupButtons from "./PopupButtons";
 
 const useStyles = makeStyles((theme) => ({
   playerWrapper: {
     position: "relative",
     display: "flex",
     justifyContent: "center",
-    aligntems: "center",
+    alignItems: "center",
     width: "100%",
     margin: "auto",
     height: "100%",
     flexDirection: "column",
   },
+
+  mouseMoveDiv: {
+    position: "absolute",
+    width: "100%",
+    height: "100%",
+  },
+
+  popupButtonsVisible: {
+    width: "100%",
+    position: "absolute",
+    bottom: "10%",
+  },
+
+  popupButtonsHidden: {
+    width: "100%",
+    position: "absolute",
+    bottom: "100%",
+    transition: "bottom 2s",
+  },
 }));
 
 const IguanaSlide3 = ({ content }) => {
+  React.useEffect(() => {
+    console.log("rerendered");
+  });
   const classes = useStyles();
 
   const [src, setSrc] = useState(content.src);
-  const [intermission, setIntermission] = useState(true);
+  const [intermission, setIntermission] = useState(false);
+  const [playbackEnded, setPlaybackEnded] = useState(true);
+  const [popupButtonsClass, setPopupButtonsClass] = useState(
+    classes.popupButtonsVisible
+  );
+  const [popupTimeout, setPopupTimeout] = useState(null);
+  const [checked, setChecked] = React.useState(false);
 
   const handlePlaybackEnded = () => {
     setIntermission(true);
+    setPlaybackEnded(true);
   };
 
   const handleOnClick = (newSrc) => {
-    setIntermission(false);
     setSrc(newSrc);
+    setIntermission(false);
+    setPlaybackEnded(false);
   };
 
   const handleClose = () => {
     setIntermission(false);
+    setPlaybackEnded(true);
   };
 
-  const VideoPlayer = () => (
-    <div className={classes.playerWrapper}>
+  const handleMouseMove = (e) => {
+    e.preventDefault();
+    setChecked(true);
+    (() => {
+      clearTimeout(popupTimeout);
+      setPopupTimeout(setTimeout(() => setChecked(false), 3000));
+    })();
+  };
+
+  return !intermission ? (
+    // <VideoPlayer />
+    <div
+      className={classes.playerWrapper}
+      onMouseMove={(e) => {
+        handleMouseMove(e);
+      }}
+    >
       <ReactPlayer
         position="relative"
         width="100%"
         height="100%"
         url={src}
         controls
-        playing={!intermission}
+        playing={!playbackEnded}
         onEnded={handlePlaybackEnded}
-        // onPlay={handlePlaybackStarted}
       />
+      <Slide direction="up" in={checked} mountOnEnter unmountOnExit>
+        <div className={popupButtonsClass}>
+          <PopupButtons
+            hypotheses={content.data}
+            onClick={handleOnClick}
+            // popupButtonsClass={popupButtonsClass}
+          />
+        </div>
+      </Slide>
     </div>
-  );
-
-  return !intermission ? (
-    <VideoPlayer />
   ) : (
-    // <IntermissionScreen hypotheses={content.data} onClick={handleOnClick} />
     <IntermissionModal
       hypotheses={content.data}
       onClick={handleOnClick}
--- src/containers/IguanaSlide3/IntermissionModal.js
+++ src/containers/IguanaSlide3/IntermissionModal.js
@@ -5,26 +5,58 @@ import Button from "@material-ui/core/Button";
 import Modal from "@material-ui/core/Modal";
 import Backdrop from "@material-ui/core/Backdrop";
 import Fade from "@material-ui/core/Fade";
+import IconButton from "@material-ui/core/IconButton";
+import CancelIcon from "@material-ui/icons/Cancel";
 import { makeStyles } from "@material-ui/core/styles";
 
 const useStyles = makeStyles((theme) => ({
-  intermissionRoot: {
+  root: {
     padding: theme.spacing(2),
+    position: "relative",
+    width: "50%",
+    height: "50%",
+    display: "flex",
+    justifyItems: "center",
+    alignContent: "center",
+    [theme.breakpoints.down("xs")]: {
+      padding: theme.spacing(1),
+      width: "80%",
+      height: "50%",
+    },
+    [theme.breakpoints.down("sm")]: {
+      // padding: theme.spacing(1),
+      width: "80%",
+      height: "50%",
+    },
+  },
+
+  background: {
+    position: "absolute",
+    top: 0,
+    left: 0,
     backgroundImage:
       "url('http://virtualgalapagos.colgate.edu/assets/IguanaModule/Images/IguanaSlide3Intro.JPG')",
     backgroundSize: "cover",
     backgroundRepeat: "no-repeat",
     backgroundPosition: "center",
-    backgroundBlendMode: "lighten",
-    backgroundColor: "black",
-    width: "80%",
-    height: "80%",
+    backgroundColor: theme.palette.grey["800"],
+    opacity: "0.5",
+    width: "100%",
+    height: "100%",
+  },
+
+  content: {
+    position: "relative",
   },
 
   title: {
-    fontSize: "1.7rem",
+    fontSize: "0.9rem",
     fontWeight: "bold",
     textAlign: "center",
+    [theme.breakpoints.down("xs")]: {
+      fontSize: "0.85rem",
+      // textAlign: "left",
+    },
   },
 
   img: {
@@ -35,31 +67,43 @@ const useStyles = makeStyles((theme) => ({
   },
 
   button: {
-    height: "4em",
-  },
-
-  hypothesisTitle: {
-    fontSize: "0.7rem",
-    fontWeight: "bolder",
+    height: "100%",
   },
 
   description: {
-    fontSize: "0.65rem",
+    fontSize: "0.5rem",
     fontWeight: "bold",
     [theme.breakpoints.down("xs")]: {
-      fontSize: ".8rem",
+      fontSize: "0.4rem",
     },
   },
+
   modal: {
     display: "flex",
     alignItems: "center",
     justifyContent: "center",
   },
+
+  cancelButton: {
+    position: "absolute",
+    top: 0,
+    right: 0,
+  },
 }));
 
 const IntermissionModal = ({ hypotheses, onClick, handleClose, open }) => {
   const classes = useStyles();
-  //   const [open, setOpen] = React.useState(openIntermission);
+
+  const CloseButton = () => (
+    <IconButton
+      aria-label="cancel"
+      className={classes.cancelButton}
+      color="secondary"
+      onClick={handleClose}
+    >
+      <CancelIcon />
+    </IconButton>
+  );
 
   return (
     <Modal
@@ -75,39 +119,43 @@ const IntermissionModal = ({ hypotheses, onClick, handleClose, open }) => {
       }}
     >
       <Fade in={open}>
-        <Grid
-          container
-          alignContent="center"
-          justify="center"
-          className={classes.intermissionRoot}
-          spacing={1}
-        >
-          <Grid item xs={12}>
-            <Typography component="h1" className={classes.title}>
-              Select A Hypothesis To Explore
-            </Typography>
-          </Grid>
-          {hypotheses.map((item, index) => (
-            <Grid item xs={12} sm={4} md={2} key={index}>
-              <Button
-                variant="contained"
-                color="secondary"
-                fullWidth
-                className={classes.button}
-                onClick={() => {
-                  onClick(item.videoSrc);
-                }}
-              >
-                <Grid item xs={12}>
-                  <Typography className={classes.description}>
-                    {" "}
-                    {item.description}
-                  </Typography>
-                </Grid>
-              </Button>
+        <div className={classes.root}>
+          <div className={classes.background} />
+          <Grid
+            container
+            alignContent="center"
+            justify="flex-start"
+            spacing={1}
+            className={classes.content}
+          >
+            <CloseButton />
+            <Grid item xs={12}>
+              <Typography component="h1" className={classes.title}>
+                Select A Hypothesis To Explore
+              </Typography>
             </Grid>
-          ))}
-        </Grid>
+            {hypotheses.map((item, index) => (
+              <Grid item xs={12} md={6} key={index}>
+                <Button
+                  variant="contained"
+                  color="secondary"
+                  fullWidth
+                  className={classes.button}
+                  onClick={() => {
+                    onClick(item.videoSrc);
+                  }}
+                >
+                  <Grid item xs={12}>
+                    <Typography className={classes.description}>
+                      {" "}
+                      {item.description}
+                    </Typography>
+                  </Grid>
+                </Button>
+              </Grid>
+            ))}
+          </Grid>
+        </div>
       </Fade>
     </Modal>
   );
