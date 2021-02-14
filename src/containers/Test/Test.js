212b267a69f5891e678d0524b7dcc947401958a6
Giancarlo <Garcese@colgate.edu>
2021-02-13 19:17:21 -0500

    Figured out dynamic routing

--- src/containers/Test/Test.js
+++ src/containers/Test/Test.js
@@ -1,9 +1,21 @@
 import React from 'react'
+import {Link} from "react-router-dom"
+import {useParams} from "react-router"
+import IntroSlide from "../../components/VolcanoSlides/Intro Slide/IntroSlide"
 
-export default function Test() {
-    return (
-        <div style={{backgroundColor: "orange", height: "100%"}}>asdfsdf
-            <button>hello</button>
+export default function Test(props) {
+
+    let { id } = useParams();
+    if (id == "slide1") {
+        return(
+        <div style={{backgroundColor: "orange", height: "100%"}}> 
+            Go fuck yourself!
         </div>
-    )
+        )}
+    else {
+    return (
+        <div style={{backgroundColor: "orange", height: "100%"}}>   {id} 
+                <IntroSlide />
+        </div>
+    )}
 }
