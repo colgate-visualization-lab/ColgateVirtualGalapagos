89dbe134f29e7338eed3b61e7512a191bfb03f1d
Giancarlo <Garcese@colgate.edu>
2021-02-13 22:27:15 -0500

    made some changes

--- src/components/VolcanoSlides/VolcanoSlides.js
+++ src/components/VolcanoSlides/VolcanoSlides.js
@@ -1,11 +1,25 @@
-import React from 'react'
+import React, {Fragment} from 'react'
 import {Link} from "react-router-dom"
+import classes from "./VolcanoSlides.css"
+import {
+    introVideo, summaryVolcano, plateTectonics, mantlePlumes, Back, Next, TerrainMap, 
+    magnifyingGlass, Ferd, santaCruz, VolcanoCompare, tectonicPlatesMap, convergent, 
+    convergent2, convergent2fix, divergent, transform, seaMounts, eruptionDates, TerrainOval, 
+    TerrainOvalCorrect, TerrainOvalWrong, XImage, DnDAnswers, DnD, terrainMap2, volcanoSlider1, 
+    eruptionAudio, volcanoCompare360, terrainMap3, fernandina1, santaCruz1, terrainMap4, 
+    terrainMapAges1, terrainMapAges2, otherVolcanoes1, tectonicPlates1, platesGame1, 
+    tectonicPlates2, southAmerica1, terrainMap6, plumePlacement1, plumeYes1, plumeNo1, 
+    whereNext1, Exploration01, Exploration02, Exploration03, Exploration04, Exploration05,
+    Vista, Vista2, Vista3, Vista4, Vista5,
+  } from "../../assets/VolcanoModule";
 
 export default function VolcanoSlides(props) {
 
     if (props.id == 0) {
         return (
-            <Link to="./1">slide 0</Link>
+            <Fragment>
+                <video className={classes.center} src={introVideo} autoPlay muted controls controlsList="nodownload" type="video/mp4"/> 
+            </Fragment>
         )
     }
     else if (props.id == 1) {
@@ -20,17 +34,17 @@ export default function VolcanoSlides(props) {
     }
     else if (props.id == 3) {
         return (
-            <Link to="./0">slide 2</Link>
+            <Link to="./0">slide 3</Link>
         )
     }
     else if (props.id == 4) {
         return (
-            <Link to="./0">slide 2</Link>
+            <Link to="./0">slide 4</Link>
         )
     }
     else if (props.id == 5) {
         return (
-            <Link to="./0">slide 2</Link>
+            <Link to="./0">slide 5</Link>
         )
     }
 }
