8adc8159712de8a7e067b401cd55b7fe8237a4e1
Desmond Tuiyot <dtuiyot@gmail.com>
2021-02-18 17:30:52 -0500

    added pen tool

--- src/components/DrawAreaIcons/index.js
+++ src/components/DrawAreaIcons/index.js
@@ -1,5 +1,6 @@
 import PenIcon from "./PenIcon";
 import EraserIcon from "./EraserIcon";
 import TextBoxIcon from "./TextBoxIcon";
+import LineIcon from "./LineIcon";
 
-export { PenIcon, EraserIcon, TextBoxIcon };
+export { PenIcon, EraserIcon, TextBoxIcon, LineIcon };
