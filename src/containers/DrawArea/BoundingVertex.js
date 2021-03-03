2d8ab5481d81b40a24c0d630a3aab32cf59f3ded
Desmond Tuiyot <dtuiyot@gmail.com>
2021-03-02 20:19:04 -0500

    added outline around straight line when selected

--- src/containers/DrawArea/BoundingVertex.js
+++ src/containers/DrawArea/BoundingVertex.js
@@ -4,7 +4,7 @@ const BoundingVertex = ({ cx, cy, parent, handleMouseDown }) => {
   return (
     <rect
       fill="white"
-      stroke="blue"
+      stroke="#7D7DF1"
       onMouseDown={(e) => handleMouseDown(e, parent)}
       x={cx - 5}
       y={cy - 5}
