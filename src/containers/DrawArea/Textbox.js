667946ad37766a7bfd476922aeb5e8676a1cac89
Desmond Tuiyot <dtuiyot@gmail.com>
2021-03-07 16:46:58 -0500

    Started new DrawArea element - old one got too complicated to navigate and reason around
    Former-commit-id: 1c3958f706a7e061af1a2d5d1c134d7a9707818c [formerly f4892ce035f90dc80a092a53533212b62df05f1b] [formerly c1db15fe4655e3433d2f8ee7676da346ebe8ba92 [formerly cc372fec43187666a3adfcabe5462ed7294792f4]]
    Former-commit-id: 8729b6a6fce9b44166cbbc4ffa23ab1b98ab1851 [formerly 2e03227f238f838bb684032497ae5cd0e66c192e]
    Former-commit-id: 320bc4909365092fe0fbc2dca290829ddb1cb1a2

--- src/containers/DrawArea/Textbox.js
+++ src/containers/DrawArea/Textbox.js
@@ -31,7 +31,7 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-const Textbox = ({ textboxDetails, index, handleSelect }) => {
+const Textbox = ({ textboxDetails, index, handleSelect, handleDelete }) => {
   const [position, selected, translate] = [
     textboxDetails.get("position"),
     textboxDetails.get("selected"),
