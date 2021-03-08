00c526e2923717049b7d1e1b214989099b54c433
Desmond Tuiyot <dtuiyot@gmail.com>
2021-03-08 15:22:23 -0500

    Added resizing control vertex to line
    Former-commit-id: 891f70c1337dc6fd14d272b6e16ced9e0e3fde1d [formerly a23cb6e0a5459a2cff502a9114bc35e11e6fcd4f] [formerly 492c7bbebcd2588bc79900ae26334d570ed9b45a [formerly 2a9ea65d56bc70df7f65e0c1693735d3cde97504]]
    Former-commit-id: 1b544abcdc994cb2d1c903973c7d42c7574f4e6c [formerly cc95ce1d832fcd8f0b28b51c788036f10f257c5f]
    Former-commit-id: 900464fa04c42ce8c89a3fa8750c9711c54787da

--- src/containers/DrawAreaAlternate/Line.js
+++ src/containers/DrawAreaAlternate/Line.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 
+import ResizingVertex from "./ResizingVertex";
+
 const useStyles = makeStyles(() => ({
   line: {
     strokeWidth: "3",
@@ -48,6 +50,12 @@ const Line = ({ element }) => {
         />
       )}
       <line x1={x1} y1={y1} x2={x2} y2={y2} className={classes.line} />
+      {selected && (
+        <>
+          <ResizingVertex cx={x1} cy={y1} />
+          <ResizingVertex cx={x2} cy={y2} />
+        </>
+      )}
     </g>
   );
 };
