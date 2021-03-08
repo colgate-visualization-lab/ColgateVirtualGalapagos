f03c3a40e6714c61c57403179072e4757a5d3d69
Desmond Tuiyot <dtuiyot@gmail.com>
2021-03-08 01:07:29 -0500

    merged all branches to master
    Former-commit-id: 8a0312fd35c6863af0fee6f1669f54d5de003a62 [formerly f09fb4d52dc8b3e9c33d358913d65259a291da8d] [formerly 4f17cae9254f33febf6c036c5a88f5d0120b0e9f [formerly e5644a3e1c32ab7b8d799481c3f797ecfa72b30e]]
    Former-commit-id: 75cb30d1d213942a001bb889c0474ab3f16b8612 [formerly c2d5a6cd7f118a8a807d25bb99c9790cb2bc286b]
    Former-commit-id: f8d6e8f85bb2c707d16518218b124199c6b43be5

--- src/assets/Misc.js
+++ src/assets/Misc.js
@@ -1,6 +1,6 @@
 export const video =
   "https://virtualgalapagos.colgate.edu/assets/misc/splashvideo.mp4";
-export const Backpack =
+export const backpack =
   "https://virtualgalapagos.colgate.edu/assets/misc/backpack.png";
 export const logo = "https://virtualgalapagos.colgate.edu/assets/misc/logo.png";
 export const fieldbook =
