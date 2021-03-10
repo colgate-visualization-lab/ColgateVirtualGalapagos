42fae1fce730b14610a9aca277f1bcc25bd03a0e
Desmond Tuiyot <dtuiyot@gmail.com>
2021-03-10 11:55:03 -0500

    textbox selected and focused states now work - focused is when one is typing, and selection on a focused textbox won't work. Selected state allows you to transform the textbox

--- src/components/DrawAreaIcons/index.js
+++ src/components/DrawAreaIcons/index.js
@@ -7,8 +7,8 @@ import SelectIcon from "./SelectIcon";
 
 const toolbarIcons = [
   { name: "select", component: <SelectIcon /> },
-  { name: "pencil", component: <PencilIcon /> },
-  { name: "eraser", component: <EraserIcon /> },
+  // { name: "pencil", component: <PencilIcon /> },
+  // { name: "eraser", component: <EraserIcon /> },
   { name: "textbox", component: <TextBoxIcon /> },
   { name: "line", component: <LineIcon /> },
 ];
