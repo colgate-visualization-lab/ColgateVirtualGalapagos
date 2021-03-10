65d90b80f5ac1a1fd1123816ba9c5471919a7f66
Desmond Tuiyot <dtuiyot@gmail.com>
2021-03-09 19:57:31 -0500

    Added textbox + can resize and move the textbox

--- src/containers/DrawAreaAlternate/Textbox.js
+++ src/containers/DrawAreaAlternate/Textbox.js
@@ -1,29 +1,39 @@
-import React, { useState } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 
+import ResizingVertex from "./ResizingVertex";
+
 const useStyles = makeStyles((theme) => ({
-  textDiv: {},
+  boundingRect: {
+    stroke: "#246AF2",
+    strokeDasharray: "4",
+    strokeWidth: "2px",
+    fill: "transparent",
+  },
+  textDiv: {
+    position: "relative",
+    overflowY: "visible",
+    background: "transparent",
+  },
   text: {
-    position: "absolute",
-    top: (styleProps) => styleProps.top + styleProps.translateY,
-    left: (styleProps) => styleProps.left + styleProps.translateX,
-
-    textAlign: "center",
-    backgroundColor: "transparent",
+    height: "100%",
+    width: "100%",
+    fontSize: "1rem",
+    fontFamily: "'Roboto Mono', monospace",
     padding: theme.spacing(0),
-    border: "transparent",
-
+    resize: "none",
+    background: "transparent",
+    border: "none",
     "&:hover": {
-      outline: "none",
-      border: "2px dashed #246AF2",
-      borderRadius: "5px",
       cursor: "default",
+      outline: "none",
+      border: "transparent",
+      background: "transparent",
     },
-
     "&:focus": {
       outline: "none",
-      border: "2px solid #246AF2",
-      borderRadius: "5px",
+      background: "transparent",
+      border: "transparent",
     },
   },
   input: {
@@ -31,25 +41,63 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-const Textbox = () => {
+const Textbox = ({ x1, y1, x2, y2, selected }) => {
+  const [value, setValue] = useState("");
+  const ref = useRef();
+
+  useEffect(() => {
+    if (selected) ref.current.focus();
+  });
+
+  const styleProps = {
+    left: x1,
+    top: y1,
+    width: Math.abs(x2 - x1),
+    height: Math.abs(y2 - y1),
+  };
   const classes = useStyles(styleProps);
 
+  const handleChange = (e) => {
+    e.preventDefault();
+    setValue(e.target.value);
+  };
+
   return (
-    <div className={classes.textDiv}>
-      <input
-        onMouseOver={(e) => {
-          e.preventDefault();
-        }}
-        onMouseDown={(e) => {
-          e.preventDefault();
-        }}
-        onChange={(e) => {
-          e.preventDefault();
-        }}
-        className={classes.text}
-        value="This is a textbox"
+    <g>
+      <foreignObject
+        x={x1}
+        y={y1}
+        height={styleProps.height}
+        width={styleProps.width}
+        className={classes.textDiv}
+      >
+        <textarea
+          // onMouseDown={(e) => {
+          //   e.preventDefault();
+          // }}
+          // disabled={selected}
+          ref={ref}
+          className={classes.text}
+          value={value}
+          onChange={handleChange}
+        />
+      </foreignObject>
+      {/* {selected && ( */}
+      <rect
+        x={x1}
+        y={y1}
+        height={styleProps.height}
+        width={styleProps.width}
+        className={classes.boundingRect}
       />
-    </div>
+      <>
+        <ResizingVertex cx={x1} cy={y1} />
+        <ResizingVertex cx={x1} cy={y2} />
+        <ResizingVertex cx={x2} cy={y1} />
+        <ResizingVertex cx={x2} cy={y2} />
+      </>
+      {/* )} */}
+    </g>
   );
 };
 
