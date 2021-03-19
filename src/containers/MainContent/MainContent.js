4df232d715209e1879d960e0772bdbe4785653c1
Desmond Tuiyot <dtuiyot@gmail.com>
2021-03-18 21:34:24 -0400

    refactored some code from MainContent (Iguana Module Content)
    Former-commit-id: a49be0541fa075b83a4eb32a6fa9ecc686a6ddb2 [formerly 0e37a5cd9705d7be41c297789e3854f523e3be4e] [formerly 8b1754fb1634d29e4435e545db122c1667d3310c [formerly acd0bc26bab12833b273c6696096fce4e804b109]]
    Former-commit-id: 943c5568b7633ceb949001caf080ef153e01922b [formerly 5e5d140ec05e0315c0b206c25cadf52bee19b959]
    Former-commit-id: 42799bada2680ef47978168dc6e26eb8e99e4b2c

--- src/containers/MainContent/MainContent.js
+++ src/containers/MainContent/MainContent.js
@@ -12,18 +12,6 @@ import IguanaSlide17 from "../IguanaSlide17";
 import AudioPlayerHandler from "../../components/AudioPlayer/AudioPlayerHandler";
 
 const useStyles = makeStyles((theme) => ({
-  //  CONTENT CONTAINER STYLING  - container that surrounds
-  //   main content of the page - basically whatever's above the
-  //   PREV and NEXT buttons
-  contentContainer: {
-    position: "relative",
-    width: "100%",
-
-    // height is 100% of parent container minus the total height of the PREV and NEXT buttons (plus a little space)
-    height: `calc(100%  -  ${theme.typography.pxToRem(40)})`,
-    // backgroundColor: "lavender",
-  },
-
   // VIDEO STYLING
   video: {
     minHeight: "400px",
@@ -45,120 +33,82 @@ const useStyles = makeStyles((theme) => ({
     width: "100%",
     height: "100%",
   },
-}));
 
-// Slide Content Container Component - i.e. everything above the
-//  PREV/NEXT buttons and below the navbar
-const SlideContainer = (props) => (
-  <Grid
-    item
-    xs={12}
-    container
-    justify="center"
-    alignItems="center"
-    {...props}
-  />
-);
+  iframe360Comparison: {
+    width: "50%",
+  },
+}));
 
 function MainContent({ content }) {
   const classes = useStyles();
   if (content.type === "image") {
     return (
-      <SlideContainer className={classes.contentContainer}>
+      <>
         <img src={content.url} className={`iguana ${classes.img}`} />
-      </SlideContainer>
+      </>
     );
   } else if (content.type === "video") {
     return (
-      <SlideContainer className={classes.contentContainer}>
+      <>
         <video src={content.url} className={classes.video} controls />
-      </SlideContainer>
+      </>
     );
   } else if (content.type === "video360") {
     return (
       <>
         <AudioPlayerHandler src={content.audioSrc} />
-
-        <SlideContainer className={classes.contentContainer}>
-          <Iframe className={classes.iframe360} src={content.url} />
-        </SlideContainer>
+        <Iframe className={classes.iframe360} src={content.url} />
       </>
     );
   } else if (content.type === "interactive_image") {
     return (
       <>
-        <SlideContainer className={classes.contentContainer}>
-          <IguanaSlide15 classes={classes} content={content} />
-        </SlideContainer>
+        <IguanaSlide15 classes={classes} content={content} />
       </>
     );
   } else if (content.type === "image_comparison") {
     return (
       <>
         <AudioPlayerHandler src={content.audioSrc} />
-
-        <SlideContainer className={classes.contentContainer}>
-          <IguanaSlide8 content={content} />
-        </SlideContainer>
-        {/* <SlideContainer xs={6} className={classes.contentContainer}>
-          <IguanaSlide8 url={content.url1} popupText={content.landIguanaText} />
-        </SlideContainer>
-        <SlideContainer xs={6} className={classes.contentContainer}>
-          <IguanaSlide8
-            url={content.url2}
-            popupText={content.marineIguanaText}
-          />
-        </SlideContainer> */}
+        <IguanaSlide8 content={content} />
       </>
     );
   } else if (content.type === "360_comparison") {
     return (
       <>
         <AudioPlayerHandler src={content.audioSrc} />
-
-        <SlideContainer xs={6} className={classes.contentContainer}>
+        <Grid item xs={6} style={{ height: "100%" }}>
           <Iframe src={content.url1} className={classes.iframe360} />
-        </SlideContainer>
-
-        <SlideContainer xs={6} className={classes.contentContainer}>
+        </Grid>
+        <Grid item xs={6} style={{ height: "100%" }}>
           <Iframe src={content.url2} className={classes.iframe360} />
-        </SlideContainer>
+        </Grid>
       </>
     );
   } else if (content.type === "slide3InteractiveVideo") {
     return (
       <>
-        <SlideContainer className={classes.contentContainer}>
-          <IguanaSlide3 content={content} imgClass={classes.img} />
-        </SlideContainer>
+        <IguanaSlide3 content={content} imgClass={classes.img} />
       </>
     );
   } else if (content.type === "dnaInteractiveActivity") {
     return (
       <>
         <AudioPlayerHandler src={content.audioSrc} />
-
-        <SlideContainer className={classes.contentContainer}>
-          <IguanaSlide17 content={content} />
-        </SlideContainer>
+        <IguanaSlide17 content={content} />
       </>
     );
   } else if (content.type === "Slide12DnDInteractive") {
     return (
       <>
-        {/* <AudioPlayerHandler src={content.audioSrc} /> */}
-
-        <SlideContainer className={classes.contentContainer}>
-          <IguanaSlide12 content={content} />
-        </SlideContainer>
+        <AudioPlayerHandler src={content.audioSrc} />
+        <IguanaSlide12 content={content} />
       </>
     );
   } else {
     return (
       <>
-        <SlideContainer className={classes.contentContainer}>
-          <h1>THIS SLIDE HASN'T BEEN CREATED YET</h1>
-        </SlideContainer>
+        <h1>THIS SLIDE HASN'T BEEN CREATED YET</h1>
       </>
     );
   }
