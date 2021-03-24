36680f3f33a198ab7595df22af9962536503c447
Giancarlo <Garcese@colgate.edu>
2021-03-24 19:57:30 -0400

    Newest Volcano with sign in
    Former-commit-id: 4a37214530819d4e2dd0142997442d9fddb4efae [formerly b2ac3675d474a444b1ffbe2586d3d326083ade04] [formerly d23d1788171fc86270f54afb6079e36b411e932c [formerly afe86f833c63d1408b447718d211c4fa079aa2d5]]
    Former-commit-id: 434b027a73346c0a61985ad07d20bfc64a0e606c [formerly ac00e1b766898b0d1604ce6f6ce098007f47ff53]
    Former-commit-id: 8b73e3838d4dffa4a7f2d387683cfd809042434f

--- src/components/VolcanoSlides/VolcanoSlides.js
+++ src/components/VolcanoSlides/VolcanoSlides.js
@@ -1,4 +1,4 @@
-import React, {Fragment, useState, useRef} from 'react'
+import React, {Fragment, useState, useRef, useEffect} from 'react'
 import {Link} from "react-router-dom"
 import classes from "./VolcanoSlides.css"
 import Popup from "reactjs-popup";
@@ -20,80 +20,149 @@ import { CSSTransition } from 'react-transition-group';
 import './VolcanoSlides.css';
 import ImageMapper from "react-image-mapper";
 import MAP from "../../containers/VolcanoModule/ImageMap.js";
+import Button from '@material-ui/core/Button';
+import { SettingsEthernet } from '@material-ui/icons';
+import { Divider } from '@material-ui/core';
 
 export default function VolcanoSlides(props) {
 
-    //State
-    const [test, setTest] = useState(false)
+    //State. Any state set here does not reset when changing slides. 
+    //Include a seperate file to handle slide-specific state when possible. Like DND
+    const [slide21state, setSlide21State] = useState(null);
 
     //Refs
+    const slide21ref = useRef(null);
+
+    //Methods
+    const slide21Handler = (area) => {
+        if (area.id == 1) {
+            props.setSlideChange("21wrong")
+        }
+        else if (area.id == 2) {
+            props.setSlideChange("22")
+        }
+    }
 
     //Styles
     const popup = {margin: "auto", padding: "20px", background: "rgba(0, 0, 0, 0.762)", borderRadius: "5px", zIndex: "2", width: "auto", position: "absolute"}
 
+    //Lifecycle
+    useEffect(() => {
+        if (props.id == 21) {
+            setTimeout(() => setSlide21State(slide21ref.current.clientWidth), 100);
+            window.addEventListener("resize", () => setSlide21State(slide21ref.current.clientWidth))
+        }
+        else {
+            window.removeEventListener("resize", () => setSlide21State(slide21ref.current.clientWidth))
+        }
+        return () => {
+            // Clean up event listeners here
+        }
+    }, [props.id]);
+
     if (props.id == 0) {
         return (
-            <video className={classes.center} src={introVideo} autoPlay muted controls controlsList="nodownload" type="video/mp4"/> 
+            <video className={classes.center} src={introVideo} autoPlay controls controlsList="nodownload" type="video/mp4"/> 
         )
     }
     else if (props.id == 1) {
         return (
-            <IframeCompoment class={classes.vista} src={Vista}/>
+            <div>
+                <audio className={classes.audioPlayer} src={Exploration01} autoPlay controls controlsList="nodownload"/>
+                <IframeCompoment class={classes.vista} src={Vista}/>
+            </div>
         )
     }
     else if (props.id == 2) {
         return (
-            <IframeCompoment class={classes.vista} src={Vista2}/>
+            <div>
+                <audio className={classes.audioPlayer} src={Exploration02} autoPlay controls controlsList="nodownload"/>
+                <IframeCompoment class={classes.vista} src={Vista2}/>
+            </div>
         )
     }
     else if (props.id == 3) {
         return (
-            <IframeCompoment class={classes.vista} src={Vista3}/>
+            <div>
+                <audio className={classes.audioPlayer} src={Exploration03} autoPlay controls controlsList="nodownload"/>
+                <IframeCompoment class={classes.vista} src={Vista3}/>
+            </div>
         )
     }
     else if (props.id == 4) {
         return (
-            <IframeCompoment class={classes.vista} src={Vista4}/>
+            <div>
+                <audio className={classes.audioPlayer} src={Exploration04} autoPlay controls controlsList="nodownload"/>
+                <IframeCompoment class={classes.vista} src={Vista4}/>
+            </div>
         )
     }
     else if (props.id == 5) {
         return (
-            <IframeCompoment class={classes.vista} src={Vista5}/>
+            <div>
+                <audio className={classes.audioPlayer} src={Exploration04} autoPlay controls controlsList="nodownload"/>
+                <IframeCompoment class={classes.vista} src={Vista5}/>
+            </div>
         )
     }
     else if (props.id == 6) {
         return (
-            <div className={classes.imgContainer}>
-                <img className={classes.imgFill} src={TerrainMap}/>
-                <img className={classes.magnifying} onClick={() => props.setSlideChange("./6optional")} src={magnifyingGlass}/>
+            <div>
+                <audio className={classes.audioPlayer} src={terrainMap2} autoPlay controls controlsList="nodownload"/>
+                <div className={classes.imgContainer}>
+                    <img className={classes.imgFill} src={TerrainMap}/>
+                    <div className={classes.hypothesis}>Click on the magnifying glass!</div>
+                    <img className={classes.magnifying} onClick={() => props.setSlideChange("./6optional")} src={magnifyingGlass}/>
+                </div>
             </div>
         )
     }
     else if (props.id == "6optional") {
         return ( //Width of this needs to get fixed eventually
+            <div>
+            <audio className={classes.audioPlayer} src={volcanoSlider1} autoPlay controls controlsList="nodownload"/>
             <div className={classes.sliderDiv}>
                 <ImageSlider image1={santaCruz} image2={Ferd} leftLabelText="Fernandina" rightLabelText="Santa Cruz"/>
+            </div>
             </div>
         )
     }
     else if (props.id == 7) {
         return (
             <div>
+                <audio className={classes.audioPlayer} src={volcanoCompare360} autoPlay controls controlsList="nodownload"/>
                 <IframeCompoment class={classes.leftVista} src={Vista} />
                 <IframeCompoment class={classes.rightVista} src={Vista4} />
             </div>
         )
     }
     else if (props.id == 8) {
+        const button = {position: "absolute",
+            zIndex: "1",
+            top: "55%",
+            left: "7%",}
         return (
+            <div>
+            <audio className={classes.audioPlayer} src={terrainMap3} autoPlay controls controlsList="nodownload"/>
             <div className={classes.imgContainer}>
                 <img className={classes.imgFill} src={TerrainMap}/>
-                    <img className={classes.magnifying} onClick={() => props.setSlideChange("./8optional1")} src={magnifyingGlass}/>
+                <div className={classes.hypothesis}>
+                    <Button 
+                        style={button}
+                        variant="contained" 
+                        color="secondary"
+                        onClick={() => props.setSlideChange("./8optional1")}
+                        > 
+                        Compare Observations</Button>
+                </div>
+            </div>
             </div>
         )
     }
     else if (props.id == "8optional1") {
         return (
+            <div>
+            <audio className={classes.audioPlayer} src={fernandina1} autoPlay controls controlsList="nodownload"/>
             <div className={classes.imgContainer}>
                 <img className={classes.imgFill} src={Ferd}/>
                 <Popup trigger={<button className={`${classes.dot} ${classes.dot4}`}></button>} 
@@ -115,10 +184,13 @@ export default function VolcanoSlides(props) {
                     </div>
                 </Popup>
             </div>
+            </div>
         )
     }
     else if (props.id == "8optional2") {
         return (
+            <div>
+            <audio className={classes.audioPlayer} src={santaCruz1} autoPlay controls controlsList="nodownload"/>
             <div className={classes.imgContainer}>
                 <img className={classes.imgFill} src={santaCruz}/>
                 <Popup trigger={<button className={`${classes.dot} ${classes.dot7}`}></button>}
@@ -140,34 +212,58 @@ export default function VolcanoSlides(props) {
                     </div>
                 </Popup>
             </div>
+            </div>
         )
     }
     else if (props.id == 9) {
         return (
+            <div>
+            <audio className={classes.audioPlayer} src={terrainMap4} autoPlay controls controlsList="nodownload"/>
             <div className={classes.imgContainer}>
                 <img className={classes.imgFill} src={TerrainMap}/>
-                <button className={classes.magnifying}>What's a hypothesis?</button>
+                <div className={classes.hypothesis}>What's a hypothesis? (click the circle) </div>
+                <Popup trigger={<button className={`${classes.dot} ${classes.dothypothesis}`}></button>}
+                contentStyle={popup} arrow={false} position="right center"> 
+                    <div>
+                        A <i>Hypothesis</i> is a statement that explains why something
+                        happens. It is usually backed up by evidence or data that you have
+                        observed or collected beforehand. If the data and experiments
+                        agree with your idea, it supports you hypthesis. If the results of
+                        the tests disagree with your idea, then it refutes your hypohtesis
+                        indicating that it isn't quite right. Then your hypothesis needs
+                        to be adjusted. Part of science is testing to see what doesn't
+                        work, fixing it, and then retesting.
+                    </div>
+                </Popup>
+            </div>
             </div>
         )
     }
     else if (props.id == 10) {
         return (
+            <div>
+            <audio className={classes.audioPlayer} src={terrainMapAges1} autoPlay controls controlsList="nodownload"/>
             <div className={classes.imgContainer}>
                 <img className={classes.imgFill} src={TerrainMap} />
                 <DndLayout volcano10={true} />
+            </div>
             </div>
         )
     }
     else if (props.id == 11) {
         return (
+            <div>
+                <audio className={classes.audioPlayer} src={terrainMapAges2} autoPlay controls controlsList="nodownload"/>
             <div className={classes.imgContainer}>
-                <img className={classes.imgFill} src={TerrainMap}/>
-                <button className={classes.magnifying}>What's a hypothesis?</button>
+                <img className={classes.imgFill} src={DnDAnswers}/>
+            </div>
             </div>
         )
     }
     else if (props.id == 12) {
         return (
+            <div>
+            <audio className={classes.audioPlayer} src={otherVolcanoes1} autoPlay controls controlsList="nodownload"/>
             <div className={classes.imgContainer}>
                     <img className={classes.imgFill} src={VolcanoCompare}/>
                     <Popup trigger={<button className={`${classes.dot} ${classes.dot10}`}></button>}
@@ -218,72 +314,143 @@ export default function VolcanoSlides(props) {
                             along the coast volcanoes erupt regularly!
                         </div>
                     </Popup>
-                </div>
+            </div>
+            </div>
         )
     }
     else if (props.id == 13) {
         return (
-            <video className={classes.center} src={plateTectonics} autoPlay muted controls controlsList="nodownload"  type="video/mp4"/> 
+            <video className={classes.center} src={plateTectonics} autoPlay controls controlsList="nodownload"  type="video/mp4"/> 
         )
     }
     else if (props.id == 14) {
         return (
-            <Fragment>
-                <button className={classes.magnifying}>What's a hypothesis?</button>
-                <img className={classes.center} src={TerrainMap}/>
-            </Fragment>
+            <div>
+                <audio className={classes.audioPlayer} src={tectonicPlates1} autoPlay controls controlsList="nodownload"/>
+            <img className={classes.center} src={tectonicPlatesMap}/>
+            </div>
         )
     }
     else if (props.id == 15) {
         return (
-            <Fragment>
-                <button className={classes.magnifying}>What's a hypothesis?</button>
-                <img className={classes.center} src={TerrainMap}/>
-            </Fragment>
+            <div>
+            <audio className={classes.audioPlayer} src={platesGame1} autoPlay controls controlsList="nodownload"/>
+                <DndLayout volcano10={true} />
+            </div>
         )
     }
     else if (props.id == 16) {
         return (
-            <Fragment>
-                <button className={classes.magnifying}>What's a hypothesis?</button>
-                <img className={classes.center} src={TerrainMap}/>
-            </Fragment>
+                <div>
+                <audio className={classes.audioPlayer} src={tectonicPlates2} autoPlay controls controlsList="nodownload"/>
+                <img className={classes.center} src={tectonicPlatesMap}/>
+                </div>
         )
     }
     else if (props.id == 17) {
         return (
-            <video className={classes.center} src={mantlePlumes} autoPlay muted controls controlsList="nodownload"  type="video/mp4"/> 
+            <video className={classes.center} src={mantlePlumes} autoPlay controls controlsList="nodownload"  type="video/mp4"/> 
         )
     }
     else if (props.id == 18) {
         return (
+            <div>
+            <audio className={classes.audioPlayer} src={southAmerica1} autoPlay controls controlsList="nodownload"/>
             <div className={classes.imgContainer}>
                 <img className={classes.imgFill} src={seaMounts}/>
+            </div>
             </div>
         )
     }
     else if (props.id == 19) {
+        const button = {position: "absolute", zIndex: "1", top: "55%", left: "7%",}
         return (
-            <Fragment>
-                <button className={classes.magnifying}>What's a hypothesis?</button>
-                <img className={classes.center} src={TerrainMap}/>
-            </Fragment>
+            <div>
+                <audio className={classes.audioPlayer} src={terrainMap6} autoPlay controls controlsList="nodownload"/>
+            <div className={classes.imgContainer}>
+                <img className={classes.imgFill} src={TerrainMap}/>
+                <Button 
+                    style={button}
+                    variant="contained" 
+                    color="secondary"
+                    onClick={() => props.setSlideChange(20)}
+                    > 
+                    Eruption Dates</Button>
+            </div>
+            </div>
         )
     }
     else if (props.id == 20) {
         return (
-            <Fragment>
-                <button className={classes.magnifying}>What's a hypothesis?</button>
-                <img className={classes.center} src={TerrainMap}/>
-            </Fragment>
+            <div>
+                <audio className={classes.audioPlayer} src={eruptionAudio} autoPlay controls controlsList="nodownload"/>
+            <div className={classes.imgContainer}>
+                <img className={classes.imgFill} src={eruptionDates}/>
+            </div>
+            </div>
         )
     }
     else if (props.id == 21) {
         return (
-            <div className={`${classes.imgContainer} ${classes.imageMapper}`}>    {/* Addional class needed cause image mapper is annoying */}
-                <ImageMapper src={TerrainOval} width={1000} imgWidth={2117} map={MAP} fillColor={"rgba(0, 246, 255, 0.33)"} 
-                onClick={(area) => this.imageFunction(area)}/>
+            <div>
+                <audio className={classes.audioPlayer} src={plumePlacement1} autoPlay controls controlsList="nodownload"/>
+                <img ref={slide21ref} src={TerrainOval} className={classes.center} style={{visibility: "hidden"}} />
+                <div className={classes.imageMapper}>   
+                    <ImageMapper src={TerrainOval} width={slide21state + 15} imgWidth={2117} map={MAP} fillColor={"rgba(0, 246, 255, 0.33)"} 
+                    onClick={(area) => slide21Handler(area)}/>
+                </div>
             </div>
+        )
+    }
+    else if (props.id == "21wrong") {
+        const button = {flexGrow: "1"}
+        return (
+            <div>
+            <audio className={classes.audioPlayer} src={plumeNo1} autoPlay controls controlsList="nodownload"/>
+            <div className={classes.imgContainer}>
+                <div style={{display: "flex", position: "absolute", left: "0", bottom: "20px", height: "20%", flexDirection: "column"}}>
+                    <Button 
+                    style={button}
+                    variant="contained" 
+                    color="secondary"
+                    onClick={() => props.setSlideChange(20)}> 
+                    Eruption Dates</Button>
+                    <Button 
+                    style={button}
+                    variant="contained" 
+                    color="secondary"
+                    onClick={() => props.setSlideChange(11)}> 
+                    Island Ages</Button>
+                </div>
+                <img className={classes.imgFill} src={TerrainOvalWrong}/>
+            </div>
+            </div>
+        )
+    }
+    else if (props.id == 22) {
+        return (
+            <div>
+            <audio className={classes.audioPlayer} src={plumeYes1} autoPlay controls controlsList="nodownload"/> 
+            <div className={classes.imgContainer}>
+                <img className={classes.imgFill} src={TerrainOvalCorrect}/>
+            </div>
+            </div>
+        )
+    }
+    else if (props.id == 23) {
+        return (
+            <div>
+            <audio className={classes.audioPlayer} src={whereNext1} autoPlay controls controlsList="nodownload"/> 
+            <div className={classes.imgContainer}>
+                This is the end!
+                <img className={classes.imgFill} src={TerrainMap}/>
+            </div>
+            </div>
+        )
+    }
+    else if (props.id == 24) {
+        return (
+            <video className={classes.center} src={summaryVolcano} autoPlay controls controlsList="nodownload"  type="video/mp4" />
         )
     }
     else {
