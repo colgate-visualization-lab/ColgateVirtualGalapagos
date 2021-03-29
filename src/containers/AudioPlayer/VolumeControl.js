c3b6134cf627fa851c49006281b97efcb4e81d2c
Desmond Tuiyot <dtuiyot@gmail.com>
2021-03-29 15:11:25 -0400

    changed VolumeControl from popup vertical slider to persistent horizontal slider

--- src/containers/AudioPlayer/VolumeControl.js
+++ src/containers/AudioPlayer/VolumeControl.js
@@ -9,91 +9,41 @@ import { makeStyles } from "@material-ui/core/styles";
 // import VolumeMuteIcon from "@material-ui/icons/VolumeMuteOutlined";
 // import VolumeDownIcon from "@material-ui/icons/VolumeDownOutlined";
 
-const useStyles = makeStyles((theme) => {
-  const widthTransition = theme.transitions.create("width", {
-    easing: theme.transitions.easing.easeInOut,
-    duration: theme.transitions.duration.leavingScreen,
-  });
-  return {
-    volumeDiv: {
-      display: "flex",
-      alignItems: "center",
-      transition: widthTransition,
-      width: theme.typography.pxToRem(48),
-      "&:hover": {
-        transition: widthTransition,
-        width: theme.typography.pxToRem(160),
-      },
-    },
+const useStyles = makeStyles((theme) => ({
+  volumeDiv: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+  },
 
-    sliderDiv: {
-      height: theme.typography.pxToRem(160),
-    },
-
-    paper: {
-      padding: theme.spacing(1, 0.5),
-    },
-  };
-});
+  sliderDiv: {
+    width: theme.typography.pxToRem(120),
+    display: "flex",
+    alignItems: "center",
+  },
+}));
 
 const VolumeControl = ({ muted, handleMute, volume, handleVolumeChange }) => {
-  const [anchorEl, setAnchorEl] = useState(null);
   const classes = useStyles();
 
-  const handleShowVolumeSlider = (event) => {
-    console.log(event.currentTarget);
-    setAnchorEl(event.currentTarget);
-  };
-
-  const handleCloseVolumeSlider = () => {
-    setAnchorEl(null);
-  };
-  const slider = Boolean(anchorEl);
-
   return (
-    <div
-      onMouseEnter={handleShowVolumeSlider}
-      onMouseLeave={handleCloseVolumeSlider}
-    >
-      <IconButton
-        aria-owns={open ? "volume-popover" : undefined}
-        aria-haspopup="true"
-        onClick={handleMute}
-        color="primary"
-      >
-        {!muted ? <VolumeUpIcon /> : <VolumeOffIcon />}
-      </IconButton>
-      <Popover
-        id="volume-popover"
-        className={classes.popover}
-        classes={{
-          paper: classes.paper,
-        }}
-        open={slider}
-        anchorEl={anchorEl}
-        onClose={handleCloseVolumeSlider}
-        anchorOrigin={{
-          vertical: "top",
-          horizontal: "center",
-        }}
-        transformOrigin={{
-          vertical: "bottom",
-          horizontal: "center",
-        }}
-        disableRestoreFocus
-      >
-        <div className={classes.sliderDiv}>
-          <Slider
-            min={0}
-            max={1}
-            step={0.01}
-            orientation="vertical"
-            value={volume}
-            onChange={handleVolumeChange}
-            aria-labelledby="volume slider"
-          />
-        </div>
-      </Popover>
+    <div className={classes.volumeDiv}>
+      <div>
+        <IconButton onClick={handleMute} color="primary">
+          {!muted ? <VolumeUpIcon /> : <VolumeOffIcon />}
+        </IconButton>
+      </div>
+
+      <div className={classes.sliderDiv}>
+        <Slider
+          min={0}
+          max={1}
+          step={0.01}
+          value={volume}
+          onChange={handleVolumeChange}
+          aria-labelledby="volume slider"
+        />
+      </div>
     </div>
   );
 };
