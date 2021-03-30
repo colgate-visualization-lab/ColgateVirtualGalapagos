182141be6c3afd436a1de90a914bab7ebcc8c89f
Giancarlo <Garcese@colgate.edu>
2021-03-30 17:56:29 -0400

    moved volcano module to seperate render, sidebar and content fed from volcano data, buttons still not working
    Former-commit-id: c622a48ec272a8f9d7035384c9d2afd0e3ef182f [formerly db7da94c7141c56d2c80b5f0e32525f2dc17c99f] [formerly 4ea025a6d96255923be79926b8b368d9ab601c42 [formerly 6eeb0b70c5c690b0333dd62576cd54f492f925c5]]
    Former-commit-id: df9d681606281fe1216a9f3830eb2e39406fbf0e [formerly e58ea72ea2c64f8f25819850bc92c266e37f09b5]
    Former-commit-id: 49936a8ae4dca47d002d912a01c5589438d45fa4

--- src/components/VolcanoData/VolcanoData.js
+++ src/components/VolcanoData/VolcanoData.js
@@ -1,144 +1,195 @@
-const volcanoData = [
-    {
-      id: "1",
-      title: "Volcano Intro",
-      type: "video",
-      url: "https://virtualgalapagos.colgate.edu/assets/VolcanoModule/VolcanoModuleIntro.mp4",
+import { slide3Data } from "../IguanaData/slide3Data";
+import { slide15Data } from "../IguanaData/slide15Data";
+import { slide17Data } from "../IguanaData/slide17Data";
+
+const iguanaData = [
+  {
+    id: "1",
+    title: "Iguana_Endemic01",
+    type: "video",
+    url:
+      "http://virtualgalapagos.colgate.edu/assets/IguanaModule/Video/IguanaModuleIntro.mp4", // change to pull from server later
+  },
+  {
+    id: "2",
+    title: "Iguana_Carried01",
+    type: "image",
+    url:
+      "http://virtualgalapagos.colgate.edu/assets/IguanaModule/Images/SouthAmericaCoast.png",
+  },
+  {
+    id: "3",
+    title: "Iguana_Carried02",
+    type: "slide3InteractiveVideo",
+    src:
+      "http://virtualgalapagos.colgate.edu/assets/IguanaModule/Video/IguanaSlide3Intro.mp4",
+    imageSrc:
+      "http://virtualgalapagos.colgate.edu/assets/IguanaModule/Images/IguanaSlide3Intro.JPG",
+    interSrc:
+      "http://virtualgalapagos.colgate.edu/assets/IguanaModule/Images/IguanaSlide3Intro.JPG",
+    data: slide3Data,
+  },
+  {
+    id: "4",
+    title: "Iguana_Pumice01",
+    type: "video360",
+    audioSrc:
+      "http://virtualgalapagos.colgate.edu/assets/IguanaModule/Audio/IguanaMystery_Slide4.wav",
+    url:
+      "http://virtualgalapagos.colgate.edu/assets/IguanaModule/360Videos/IguanaPath/", //360
+  },
+  {
+    id: "5",
+    title: "Iguana_Pumice02",
+    type: "video",
+    url:
+      "http://virtualgalapagos.colgate.edu/assets/IguanaModule/Video/IguanaPumiceCut.mp4", // will change to pull from server
+  },
+  {
+    id: "6",
+    title: "Iguana_Comparison01",
+    type: "video360",
+    audioSrc:
+      "http://virtualgalapagos.colgate.edu/assets/IguanaModule/Audio/IguanaMystery_Slide6.wav",
+    url:
+      "http://virtualgalapagos.colgate.edu/assets/IguanaModule/360Videos/Shorewaves/",
+  },
+  {
+    id: "7",
+    title: "Iguana_Comparison02",
+    type: "360_comparison",
+    url1:
+      "http://virtualgalapagos.colgate.edu/assets/IguanaModule/360Videos/IguanaLoop/",
+    url2:
+      "http://virtualgalapagos.colgate.edu/assets/IguanaModule/360Videos/IguanaPath/",
+    audioSrc:
+      "http://virtualgalapagos.colgate.edu/assets/IguanaModule/Audio/IguanaMystery_Slide7.wav",
+  },
+  {
+    id: "8",
+    title: "Iguana_Comparison03",
+    type: "image_comparison",
+    url1:
+      "http://virtualgalapagos.colgate.edu/assets/IguanaModule/LandIguanaSmiling01.png", //slider
+    url2:
+      "http://virtualgalapagos.colgate.edu/assets/IguanaModule/MarineIguanaWithBabies.png",
+    audioSrc:
+      "http://virtualgalapagos.colgate.edu/assets/IguanaModule/Audio/IguanaMystery_Slide8.wav",
+    landIguanaText: {
+      landIguanaHead:
+        "Land iguanas are usually more yellow or brown in color to blend in with the rocks and plants found inland.",
+      landIguanaBody:
+        "Land iguanas are often larger and weigh more than marine iguanas.",
     },
-    {
-      id: "2",
-      title: "Caldara Endtrail 360",
-      type: "360_video",
-      url: "https://virtualgalapagos.colgate.edu/assets/VolcanoModule/360Videos/Caldara_Endtrail/index.htm",
+    marineIguanaText: {
+      marineIguanaBody: "Marine iguanas have flat tails to help them swim!",
+      marineIguanaTail:
+        "Marine iguanas are darker in color to match the black basaltic rocks found in near the coast.",
     },
-    {
-      id: "3",
-      title: "Caldara Shotfive 360",
-      type: "360_video",
-      url: "https://virtualgalapagos.colgate.edu/assets/VolcanoModule/360Videos/Caldara_Shotfive/index.htm",
-    },
-    {
-      id: "4",
-      title: "Stillwater 360",
-      type: "360_video",
-      url: "https://virtualgalapagos.colgate.edu/assets/VolcanoModule/360Videos/Stillwater/index.htm",
-    },
-    {
-      id: "5",
-      title: "Cactus 360",
-      type: "360_video",
-      url: "https://virtualgalapagos.colgate.edu/assets/VolcanoModule/360Videos/Cactus_Final/index.htm",
-    },
-    {
-      id: "6",
-      title: "Plazacliff 360",
-      type: "360_video",
-      url: "https://virtualgalapagos.colgate.edu/assets/VolcanoModule/360Videos/PlazaCliff/index.htm",
-    },
-    {
-      id: "7",
-      title: "Iguana_Carried02",
-      type: "image",
-      url: "https://virtualgalapagos.colgate.edu/assets/VolcanoModule/TerrainMap-Labelled.png",
-    },
-    {
-      id: "8",
-      title: "Iguana_Comparison03",
-      type: "image_comparison",
-      url1:
-        "http://virtualgalapagos.colgate.edu/assets/IguanaModule/LandIguanaSmiling01.png", //slider
-      url2:
-        "http://virtualgalapagos.colgate.edu/assets/IguanaModule/MarineIguanaWithBabies.png",
-      audioSrc: "http://virtualgalapagos.colgate.edu/assets/IguanaModule/Audio/IguanaMystery_Slide8.wav",
-      popupText: {
-        landIguanaHead: "Land iguanas are usually more yellow or brown in color to blend in with the rocks and plants found inland.",
-        landIguanaBody: "Land iguanas are often larger and weigh more than marine iguanas.",
-        marineIguanaBody: "Marine iguanas have flat tails to help them swim!",
-        marineIguanaTail: "Marine iguanas are darker in color to match the black basaltic rocks found in near the coast."
-      }
-    },
-    // {
-    //   id: "9",
-    //   title: "Iguana_Comparison04",
-    //   type: "image",
-    //   url:
-    //     "http://virtualgalapagos.colgate.edu/assets/IguanaModule/LandIguanaOnRocks.png", //slider
-    // },
-    {
-      id: "10",
-      title: "Iguana_PhyloTree01",
-      type: "video",
-      url:
-        "http://virtualgalapagos.colgate.edu/assets/IguanaModule/ShoreWaves.mp4", //360
-    },
-    {
-      id: "11",
-      title: "Iguana_PhyloTree02",
-      type: "video",
-      url:
-        "http://virtualgalapagos.colgate.edu/assets/IguanaModule/Video/IguanaPhyloTree.mp4", // will change to pull from server whiteboard
-    },
-    //need to build,
-    {
-      id: "12",
-      title: "Iguana_PhyloTree05",
-      not_done: true,
-    },
-    {
-      id: "13",
-      title: "Iguana_Evolution01",
-      not_done: true,
-    },
-    {
-      id: "14",
-      title: "Iguana_Evolution02",
-      type: "video",
-      url: "http://virtualgalapagos.colgate.edu/assets/IguanaModule/Video/IguanaWhiteboardEvolution.mp4"
-    },
-    {
-      id: "15",
-      title: "Iguana_Evolution03",
-      type: "interactive_image",
-      url:
-        "https://keep.google.com/u/1/media/v2/1Qap6axeshCXiPdl0r3vhK1-K54n1_aIV_SuJiYxsbvbDNX1835Xp9qVnw7ftrA/15VVpQTnL-CD1LG4nNx4U3BHvTdJmLjfICf5GDWtoU4SucWfcTaLFmYqDv8x1BA?accept=image/gif,image/jpeg,image/jpg,image/png,image/webp,audio/aac&sz=1600",
-      audioSrc: "http://virtualgalapagos.colgate.edu/assets/IguanaModule/Audio/Slide15_initial.mp3",
-    },
-    {
-      id: "16",
-      title: "Iguana_Evolution04",
-      type: "video",
-      url:
-        "http://virtualgalapagos.colgate.edu/assets/IguanaModule/ShoreWaves.mp4", //360
-    },
-    {
-      id: "17",
-      title: "Iguana_Evolution05",
-      not_done: true,
-    },
-    {
-      id: "18",
-      title: "Iguana_Scales02",
-      type: "video",
-      url:
-        "http://virtualgalapagos.colgate.edu/assets/IguanaModule/ShoreWaves.mp4", //360
-    },
-    {
-      id: "19",
-      title: "Iguana_Scales03",
-      type: "video",
-      url:
-        "http://virtualgalapagos.colgate.edu/assets/IguanaModule/ShoreWaves.mp4", //360
-    },
-    {
-      id: "20",
-      title: "Iguana_PhyloTree06",
-      not_done: true,
-    },
-    {
-      id: "21",
-      title: "Iguana_PinkIggy01",
-      not_done: true,
-    },
-  ];
-  export default volcanoData;
-  
+  },
+  // {
+  //   id: "9",
+  //   title: "Iguana_Comparison04",
+  //   type: "image",
+  //   url:
+  //     "http://virtualgalapagos.colgate.edu/assets/IguanaModule/LandIguanaOnRocks.png", //slider
+  // },
+  {
+    id: "10",
+    title: "Iguana_PhyloTree01",
+    type: "video360",
+    audioSrc:
+      "http://virtualgalapagos.colgate.edu/assets/IguanaModule/Audio/IguanaMystery_Slide10.wav",
+    url:
+      "http://virtualgalapagos.colgate.edu/assets/IguanaModule/360Videos/LandIguana/index.htm",
+  },
+  {
+    id: "11",
+    title: "Iguana_PhyloTree02",
+    type: "video",
+    url:
+      "http://virtualgalapagos.colgate.edu/assets/IguanaModule/Video/IguanaPhyloTree.mp4", // will change to pull from server whiteboard
+  },
+  //need to build,
+  {
+    id: "12",
+    title: "Iguana_PhyloTree05",
+    type: "Slide12DnDInteractive",
+    backgroundUrl:
+      "http://virtualgalapagos.colgate.edu/assets/IguanaModule/Images/IguanaSlide12DnDBackgroundGreySquare.png",
+  },
+  {
+    id: "13",
+    title: "Iguana_Evolution01",
+    type: "video360",
+    audioSrc:
+      "http://virtualgalapagos.colgate.edu/assets/IguanaModule/Audio/IguanaMystery_Slide13.wav",
+    url:
+      "http://virtualgalapagos.colgate.edu/assets/IguanaModule/360Videos/IguanaVegetation/index.htm",
+  },
+  {
+    id: "14",
+    title: "Iguana_Evolution02",
+    type: "video",
+    url:
+      "http://virtualgalapagos.colgate.edu/assets/IguanaModule/Video/IguanaWhiteboardEvolution.mp4",
+  },
+  {
+    id: "15",
+    title: "Iguana_Evolution03",
+    type: "interactive_image",
+    url:
+      "http://virtualgalapagos.colgate.edu/assets/IguanaModule/MarineGreenIguanaComparison.jpg",
+    audioSrc:
+      "http://virtualgalapagos.colgate.edu/assets/IguanaModule/Audio/Slide15_initial.mp3",
+    data: slide15Data,
+  },
+  {
+    id: "16",
+    title: "Iguana_Evolution04",
+    type: "video360",
+    audioSrc:
+      "http://virtualgalapagos.colgate.edu/assets/IguanaModule/Audio/IguanaMystery_Slide16.wav",
+    url:
+      "http://virtualgalapagos.colgate.edu/assets/IguanaModule/360Videos/IguanaVegetation/index.htm",
+  },
+  {
+    id: "17",
+    title: "Iguana_Evolution05",
+    type: "dnaInteractiveActivity",
+    audioSrc:
+      "http://virtualgalapagos.colgate.edu/assets/IguanaModule/Audio/IguanaMystery_Slide17.wav",
+    data: slide17Data,
+  },
+  {
+    id: "18",
+    title: "Iguana_Scales02",
+    type: "video360",
+    audioSrc:
+      "http://virtualgalapagos.colgate.edu/assets/IguanaModule/Audio/IguanaMystery_Slide18.wav",
+    url:
+      "http://virtualgalapagos.colgate.edu/assets/IguanaModule/360Videos/SantiagoBeach/index.htm", //360
+  },
+  {
+    id: "19",
+    title: "Iguana_Scales03",
+    type: "video360",
+    audioSrc:
+      "http://virtualgalapagos.colgate.edu/assets/IguanaModule/Audio/IguanaMystery_Slide19.wav",
+    url:
+      "http://virtualgalapagos.colgate.edu/assets/IguanaModule/360Videos/Shorewaves/",
+  },
+  {
+    id: "20",
+    title: "Iguana_PhyloTree06",
+    not_done: true,
+  },
+  {
+    id: "21",
+    title: "Iguana_PinkIggy01",
+    type: "video",
+    url:
+      "http://virtualgalapagos.colgate.edu/assets/IguanaModule/Video/IguanaLastSlide%20.mp4",
+  },
+];
+export default iguanaData;
