f885b503aa90144ecc548d43d0aa179e129b2fe9
Desmond Tuiyot <dtuiyot@gmail.com>
2021-04-01 15:51:21 -0400

    refactored the /home render with map
    Former-commit-id: b4a761d042925b0e64f0c8be20f13bf8956d40b9 [formerly 2a55a2eed122e2e27f7dc631488f0f317772c018] [formerly 95f7369155653209605b6321b6bb0f732572a9a4 [formerly a871dc9f2c47e37c88231f70a9b615a46ba7ff79]]
    Former-commit-id: 4faa2fd62ef6851572d87844349b517e19bcd2b8 [formerly 6e5511aeba82289c12bfc05bdfa9b216d2d58cef]
    Former-commit-id: 98e70e5950387c415609b27e6f21bec0cedd3df8

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { Route, Switch } from "react-router-dom";
 import { ThemeProvider } from "@material-ui/styles";
 import Layout from "./containers/Layout/Layout";
 import SignIn from "./containers/Authorization/SignIn";
-import SignUp from "./containers/Authorization/SignUp"
+import SignUp from "./containers/Authorization/SignUp";
 import LoadingScreen from "./containers/LoadingScreen/LoadingScreen";
 import {
   MapFernadina,
@@ -12,32 +12,33 @@ import {
   MapFloreana,
   MapEspanola,
 } from "./assets/Homepage";
-// import MapFernandina from "https://virtualgalapagos.colgate.edu/assets/homepage/MapFernandina.png"
-// import MapIsabela from "https://virtualgalapagos.colgate.edu/assets/homepage/homepage.png"
-// import MapPinzon from "https://virtualgalapagos.colgate.edu/assets/homepage/MapPinzon.png"
-// import MapFloreana from "https://virtualgalapagos.colgate.edu/assets/homepage/MapFloreana.png"
-// import MapEspanola from "https://virtualgalapagos.colgate.edu/assets/homepage/MapEspanola.png"
 import Homepage from "./containers/Homepage/Homepage";
 import SplashScreen from "./components/SplashScreen/SplashScreen";
 import Gallery from "./containers/Gallery/Gallery";
 import ModuleNav from "./components/ModuleNav/ModuleNav";
 import VolcanoLayout from "./containers/VolcanoLayout/VolcanoLayout";
-// import IguanaModule from "./components/IguanaModule/IguanaModule";
 import ExtraSelect from "./components/ExtraSelect/ExtraSelect";
-// New volcano Module
 import VolcanoSlides from "./components/VolcanoSlides/VolcanoSlides";
 import theme from "./theme/Theme";
-import Test from "./containers/Test/Test"
-import NewNote from "./containers/Backpack/Fieldbook/Note/NewNote"
-import Fieldbook from "./containers/Backpack/Fieldbook/Fieldbook"
-import Settings from "./components/Settings/Settings"
+import Test from "./containers/Test/Test";
+import NewNote from "./containers/Backpack/Fieldbook/Note/NewNote";
+import Fieldbook from "./containers/Backpack/Fieldbook/Fieldbook";
+import Settings from "./components/Settings/Settings";
 import ModuleContainer from "./containers/MainContent";
 import data from "./assets/IguanaData/IguanaData.js";
-import volcanodata from "./components/VolcanoData/VolcanoData.js"
+import volcanodata from "./components/VolcanoData/VolcanoData.js";
 
 const VolcanoModule = lazy(() =>
   import("./containers/VolcanoModule/VolcanoModule")
 );
+
+const mapImages = [
+  MapIsabela,
+  MapFernadina,
+  MapFloreana,
+  MapEspanola,
+  MapPinzon,
+];
 
 class App extends Component {
   render() {
@@ -50,36 +51,19 @@ class App extends Component {
             <Layout>
               {" "}
               {/*Layout sets up navbar and the main tag that takes up the rest of the screen */}
-              <Route
-                path="/home"
-                render={(props) => (
-                  <Homepage {...props} lockValue={1} MapImg={MapIsabela} />
-                )}
-              />
-              <Route
-                path="/home2"
-                render={(props) => (
-                  <Homepage {...props} lockValue={2} MapImg={MapFernandina} />
-                )}
-              />
-              <Route
-                path="/home3"
-                render={(props) => (
-                  <Homepage {...props} lockValue={3} MapImg={MapFloreana} />
-                )}
-              />
-              <Route
-                path="/home4"
-                render={(props) => (
-                  <Homepage {...props} lockValue={4} MapImg={MapEspanola} />
-                )}
-              />
-              <Route
-                path="/home5"
-                render={(props) => (
-                  <Homepage {...props} lockValue={5} MapImg={MapPinzon} />
-                )}
-              />{" "}
+              {mapImages.map((mapImage, index) => (
+                <Route
+                  key={index}
+                  path={`/home${index > 0 ? index + 1 : ""}`}
+                  render={(props) => (
+                    <Homepage
+                      {...props}
+                      lockValue={index + 1}
+                      MapImg={mapImage}
+                    />
+                  )}
+                />
+              ))}
               {/* Each "home" unlocks new mystery */}
               <Route path="/isabella" exact component={ModuleNav} />
               <Route path="/volcanomod" exact component={VolcanoModule} />
@@ -88,25 +72,36 @@ class App extends Component {
               {/* <Route path="/iguana" exact component={IguanaModule} /> */}
               <Route path="/extras" exact component={ExtraSelect} />
               {/* route for iguana module slides */}
-			        <Route path="/oldvolcano/:id" exact 
-                render={(props) => <Test {...props}  />}
+              <Route
+                path="/oldvolcano/:id"
+                exact
+                render={(props) => <Test {...props} />}
               />
-
-              <Route path="/iguana/:slide_id"  exact 
-                render={(props) => <ModuleContainer {...props} module={"iguana"} data={data} />}
+              <Route
+                path="/iguana/:slide_id"
+                exact
+                render={(props) => (
+                  <ModuleContainer {...props} module={"iguana"} data={data} />
+                )}
               />
-              <Route path="/volcano/:slide_id"  exact 
-                render={(props) => <ModuleContainer {...props} module={"volcano"} data={volcanodata} />}
+              <Route
+                path="/volcano/:slide_id"
+                exact
+                render={(props) => (
+                  <ModuleContainer
+                    {...props}
+                    module={"volcano"}
+                    data={volcanodata}
+                  />
+                )}
               />
-              
               {/* path="/iguana/:slide_id" component={ModuleContainer} /> */}
-   
-			  <Route path="/fieldbook" component={Fieldbook} />
-			  <Route path="/settings" component={Settings}/>
-			  <Route path="/authorization" component={SignIn}/>
-			  <Route path="/signup" component={SignUp}/>
-			  <Route path="/newnote" component={NewNote}/>
-			  <Route path="/note/:id" />
+              <Route path="/fieldbook" component={Fieldbook} />
+              <Route path="/settings" component={Settings} />
+              <Route path="/authorization" component={SignIn} />
+              <Route path="/signup" component={SignUp} />
+              <Route path="/newnote" component={NewNote} />
+              <Route path="/note/:id" />
             </Layout>
           </Suspense>
         </Switch>
