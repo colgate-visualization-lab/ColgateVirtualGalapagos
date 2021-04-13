f07d8cc369a4acba3a2d607b1a769b70cf35194b
Desmond Tuiyot <dtuiyot@gmail.com>
2021-04-13 13:58:43 -0400

    laid out the menu and toolbar sort of correctly

--- src/components/PhyloTreeMenu/PhyloTreeMenu.js
+++ src/components/PhyloTreeMenu/PhyloTreeMenu.js
@@ -1,26 +1,26 @@
 import React from "react";
 import Paper from "@material-ui/core/Paper";
+import Grid from "@material-ui/core/Grid";
 import { makeStyles } from "@material-ui/core/styles";
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    display: "flex",
-    alignItems: "center",
-    // height: "auto",
-    // position: "absolute",
-    // top: 10,
-    // right: 10,
     zIndex: 100,
     padding: theme.spacing(1),
-    // marginRight: theme.spacing(2),
     backgroundColor: "#FFF",
-    borderRadius: "5px",
+  },
+  container: {
+    marginLeft: theme.spacing(2),
   },
 }));
 
 const PhyloTreeMenu = ({ children }) => {
   const classes = useStyles();
-  return <Paper className={classes.root}>{children}</Paper>;
+  return (
+    <Grid container justify="flex-start" className={classes.container}>
+      <Paper className={classes.root}>{children}</Paper>
+    </Grid>
+  );
 };
 
 export default PhyloTreeMenu;
