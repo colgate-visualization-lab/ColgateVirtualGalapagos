cebeea41788984584bedc047c84e0524dca74255
Desmond Tuiyot <dtuiyot@gmail.com>
2021-04-14 00:22:21 -0400

    layout for Action buttons for PhyloTreeSlide redone

--- src/components/PhyloTreeMenu/PhyloTreeMenu.js
+++ src/components/PhyloTreeMenu/PhyloTreeMenu.js
@@ -1,25 +1,26 @@
 import React from "react";
-import Paper from "@material-ui/core/Paper";
+import Typography from "@material-ui/core/Typography";
 import Grid from "@material-ui/core/Grid";
 import { makeStyles } from "@material-ui/core/styles";
 
 const useStyles = makeStyles((theme) => ({
   root: {
     zIndex: 100,
-    padding: theme.spacing(1),
-    backgroundColor: "#FFF",
-    // minWidth: "25em",
   },
-  container: {
-    marginLeft: theme.spacing(2),
+  title: {
+    fontSize: "0.85rem",
+    fontWeight: "bold",
   },
 }));
 
 const PhyloTreeMenu = ({ children }) => {
   const classes = useStyles();
   return (
-    <Grid container justify="flex-start" className={classes.container}>
-      <Paper className={classes.root}>{children}</Paper>
+    <Grid container justify="flex-start" spacing={1} className={classes.root}>
+      <Grid item xs={12}>
+        <Typography className={classes.title}>Actions</Typography>
+      </Grid>
+      {children}
     </Grid>
   );
 };
