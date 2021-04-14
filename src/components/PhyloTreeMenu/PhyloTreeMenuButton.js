8c09ba2a53805d8b14c38059f331bdbefb3d55c6
Desmond Tuiyot <dtuiyot@gmail.com>
2021-04-14 00:22:21 -0400

    layout for Action buttons for PhyloTreeSlide redone
    Former-commit-id: 88e552ee8e4231ed5e03e9e373748b50d266a976 [formerly 8133cb765af6c0a1c6d083467ea1ce0b6c792e6d] [formerly 8bc12b5d2bd5963333af5e52463964b868eae426 [formerly cebeea41788984584bedc047c84e0524dca74255]]
    Former-commit-id: 64e523a8c9fd8174749b244a53530c7dfda67dfa [formerly ff720b8b4d486ba6588b339098c7f21b41823401]
    Former-commit-id: ac9a25478c063816026aee5cc18f8742c99a65fe

--- src/components/PhyloTreeMenu/PhyloTreeMenuButton.js
+++ src/components/PhyloTreeMenu/PhyloTreeMenuButton.js
@@ -1,17 +1,18 @@
 import React from "react";
 import PropTypes from "prop-types";
 import Button from "@material-ui/core/Button";
+import Grid from "@material-ui/core/Grid";
 import { makeStyles } from "@material-ui/core/styles";
 
 const useStyles = makeStyles((theme) => ({
   button: {
     padding: theme.spacing(0.5, 1),
-    margin: theme.spacing(0, 1),
-    fontSize: "0.7rem",
+    // margin: theme.spacing(0, 1),
+    fontSize: "0.8rem",
     fontWeight: "bold",
     borderRadius: 5,
     textTransform: "none",
-    backgroundColor: "rgb(249,249,249)",
+    backgroundColor: "rgb(239,239,239)",
     color: "#000",
     "&:hover": {
       backgroundColor: "rgb(220,220,220)",
@@ -22,14 +23,17 @@ const useStyles = makeStyles((theme) => ({
 const PhyloTreeMenuButton = ({ handleClick, label }) => {
   const classes = useStyles();
   return (
-    <Button
-      variant="contained"
-      size="small"
-      className={classes.button}
-      onClick={handleClick}
-    >
-      {label}
-    </Button>
+    <Grid item xs={12}>
+      <Button
+        fullWidth
+        variant="text"
+        size="small"
+        className={classes.button}
+        onClick={handleClick}
+      >
+        {label}
+      </Button>
+    </Grid>
   );
 };
 
