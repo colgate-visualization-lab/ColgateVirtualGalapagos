4a7c38f95daca9bd1485673bb4e513a73d03d72d
Desmond Tuiyot <dtuiyot@gmail.com>
2021-04-17 17:25:48 -0400

    refactored Drawer header into its own component

--- src/containers/SlideDrawer/index.js
+++ src/containers/SlideDrawer/index.js
@@ -1,6 +1,7 @@
 import SlideDrawer from "./SlideDrawer";
 import DrawerIcon from "./DrawerIcon";
 import DrawerContent from "./DrawerContent";
+import DrawerHeader from "./DrawerHeader";
 
 export default SlideDrawer;
-export { DrawerIcon, DrawerContent };
+export { DrawerIcon, DrawerContent, DrawerHeader };
