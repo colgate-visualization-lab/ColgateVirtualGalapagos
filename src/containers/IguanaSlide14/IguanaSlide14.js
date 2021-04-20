815c91d3a5758d7875276cf2e50bbd1670934cdf
Desmond Tuiyot <dtuiyot@gmail.com>
2021-04-20 16:53:35 -0400

    code for iguana image map
    Former-commit-id: a8788c6a1617cb012e0e9eb552ec74e7bcaa43c8 [formerly 53b6069677392c69acc8e3b4ec2aa33ec0c2d417] [formerly 8e884323d21754e04b6d657520ca1582cdb49935 [formerly 1f8fda8ba4f7466fb735bd2dbcc2a8c4fc732e84]]
    Former-commit-id: 62d6b82f57d778442ba8913de160b646b26a2594 [formerly 8930b1b2f18acd3cddbb04f91ec181985afd7e41]
    Former-commit-id: c79c54dc0954c30498b8b98467618d1f8b549806

--- src/containers/IguanaSlide14/IguanaSlide14.js
+++ src/containers/IguanaSlide14/IguanaSlide14.js
@@ -1,11 +1,8 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core";
-import {
-  ReactCompareSlider,
-  ReactCompareSliderImage,
-} from "react-compare-slider";
+import { ReactCompareSlider } from "react-compare-slider";
 
-import CenteredImage from "./CenteredImage";
+import MappedImage from "./MappedImage";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -13,34 +10,24 @@ const useStyles = makeStyles((theme) => ({
     height: "100%",
     border: "3px solid lightgrey",
   },
-  imgContainer: {
-    display: "flex",
-    width: "100%",
-    height: "100%",
-    alignItems: "center",
-    // backgroundColor: "white",
-    // justifyItems: "center",
-  },
-  img: {
-    width: "100%",
-    height: "auto",
-  },
 }));
 
 const IguanaSlide14 = ({ content }) => {
   const marine = {
     alt: "Marine Iguana",
     src: content.imgSrc.src2,
+    width: 4029,
+    height: 1716,
   };
   const green = {
     alt: "Green Iguana",
     src: content.imgSrc.src1,
+    width: 4178,
+    height: 1627,
   };
 
-  const containerProps = {
-    style: {
-      backgroundColor: "white",
-    },
+  const containerStyle = {
+    backgroundColor: "white",
   };
 
   const classes = useStyles();
@@ -48,17 +35,17 @@ const IguanaSlide14 = ({ content }) => {
     <ReactCompareSlider
       onlyHandleDraggable={true}
       itemOne={
-        <CenteredImage
-          id="Marine Iguana"
-          imgProps={marine}
-          containerProps={containerProps}
+        <MappedImage
+          img={green}
+          map={content.data.greenIguanaMap}
+          containerStyle={containerStyle}
         />
       }
       itemTwo={
-        <CenteredImage
-          id="Green Iguana"
-          imgProps={green}
-          containerProps={containerProps}
+        <MappedImage
+          img={marine}
+          map={content.data.marineIguanaMap}
+          containerStyle={containerStyle}
         />
       }
       className={classes.root}
