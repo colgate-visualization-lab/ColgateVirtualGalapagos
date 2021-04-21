356e1d5555d72c8c7358999a2166f065f8c04da8
Desmond Tuiyot <dtuiyot@gmail.com>
2021-04-20 22:35:18 -0400

    new code for PhyloTreeSlide
    Former-commit-id: 7c336b9432bd495208729d53c80b16f50ccc2d68 [formerly c4ed4ad411e0b30dc90c4bd89e75171e3eb2905e] [formerly 60231657cea54d0a32e2816aa766ecd33321a7b0 [formerly 1b042e185168b8b1ba906874916b67d275f732f0]]
    Former-commit-id: 6ac8054c2b85363e9de6b4e7cdfdb808bd194c16 [formerly c14520a90c75f69c0f924816d1467c257a90d033]
    Former-commit-id: 768973989ed99fc56999ee529ad53595d5c82268

--- src/containers/PhyloTreeDnD/PhyloTreeDnD.js
+++ src/containers/PhyloTreeDnD/PhyloTreeDnD.js
@@ -1,63 +1,64 @@
-import React, { useContext } from "react"
-import Grid from "@material-ui/core/Grid"
-import { DndProvider } from "react-dnd"
-import { HTML5Backend } from "react-dnd-html5-backend"
+import React, { useContext } from "react";
+import Grid from "@material-ui/core/Grid";
+import { DndProvider } from "react-dnd";
+import { HTML5Backend } from "react-dnd-html5-backend";
 
-import IguanaDropTarget from "./IguanaDropTarget"
-import IguanaDragSource from "./IguanaDragSource"
-import PhyloTreeDnDMenu from "./PhyloTreeDnDMenu"
-import { MainActivityArea } from "../../components/PhyloTree"
-import PhyloTreeSidebar from "../../components/PhyloTreeSidebar"
-import { Slide11Context } from "../../contexts"
-import useTreeDnD from "../../hooks/useTreeDnD"
+import DragSourcePanel from "./DragSourcePanel";
+import IguanaDropTarget from "./IguanaDropTarget";
+import IguanaDragSource from "./IguanaDragSource";
+import PhyloTreeDnDMenu from "./PhyloTreeDnDMenu";
+import { MainActivityArea } from "../../components/PhyloTree";
+import PhyloTreeSidebar from "../../components/PhyloTreeSidebar";
+import { Slide11Context } from "../../contexts";
+import useTreeDnD from "../../hooks/useTreeDnD";
 
-import { Box, delay } from "./utils"
+import { Box, delay } from "./utils";
 
 const PhyloTreeDnD = () => {
-  const content = useContext(Slide11Context)
+  const content = useContext(Slide11Context);
   // prettier-ignore
   const { iguanaNames, iguanaNamesPlacement
 	} = content.phyloTreeData
 
-  const [state, dispatch] = useTreeDnD(iguanaNames, iguanaNamesPlacement)
+  const [state, dispatch] = useTreeDnD(iguanaNames, iguanaNamesPlacement);
 
   // prettier-ignore
   let { draggedNames, undraggedNames, correctnessIndicatorVisible, completedTreeVisible,
 	} = state
 
   const handleDrop = (droppedName, index) => {
-    const currentBox = draggedNames[index]
-    dispatch({ type: "UPDATE_UNDRAGGED_NAMES", droppedName, currentBox })
-    dispatch({ type: "UPDATE_DRAGGED_NAMES", droppedName, index, currentBox })
-    dispatch({ type: "SET_TREE_CORRECTNESS_INDICATOR", value: false })
-    dispatch({ type: "SET_COMPLETE_TREE_STATUS", value: false })
-  }
+    const currentBox = draggedNames[index];
+    dispatch({ type: "UPDATE_UNDRAGGED_NAMES", droppedName, currentBox });
+    dispatch({ type: "UPDATE_DRAGGED_NAMES", droppedName, index, currentBox });
+    dispatch({ type: "SET_TREE_CORRECTNESS_INDICATOR", value: false });
+    dispatch({ type: "SET_COMPLETE_TREE_STATUS", value: false });
+  };
 
   const handleCheckTree = () => {
-    dispatch({ type: "SET_TREE_CORRECTNESS_INDICATOR", value: true })
-  }
+    dispatch({ type: "SET_TREE_CORRECTNESS_INDICATOR", value: true });
+  };
 
   const handleShowTree = async () => {
-    console.log(content.id)
-    dispatch({ type: "SET_TREE_CORRECTNESS_INDICATOR", value: false })
-    dispatch({ type: "SET_COMPLETE_TREE_STATUS", value: true })
-    await delay(200)
-    dispatch({ type: "SET_UNDRAGGED_NAMES", names: [] })
-    await delay(200)
-    dispatch({ type: "SHOW_COMPLETED_TREE", id: content.id })
-  }
+    console.log(content.id);
+    dispatch({ type: "SET_TREE_CORRECTNESS_INDICATOR", value: false });
+    dispatch({ type: "SET_COMPLETE_TREE_STATUS", value: true });
+    await delay(200);
+    dispatch({ type: "SET_UNDRAGGED_NAMES", names: [] });
+    await delay(200);
+    dispatch({ type: "SHOW_COMPLETED_TREE", id: content.id });
+  };
 
   const handleResetTree = async () => {
-    dispatch({ type: "SET_TREE_CORRECTNESS_INDICATOR", value: false })
-    dispatch({ type: "SET_COMPLETE_TREE_STATUS", value: false })
-    await delay(200)
+    dispatch({ type: "SET_TREE_CORRECTNESS_INDICATOR", value: false });
+    dispatch({ type: "SET_COMPLETE_TREE_STATUS", value: false });
+    await delay(200);
     dispatch({
       type: "SET_DRAGGED_NAMES",
       names: iguanaNamesPlacement.map((box) => new Box(box)),
-    })
-    await delay(300)
-    dispatch({ type: "SET_UNDRAGGED_NAMES", names: iguanaNames })
-  }
+    });
+    await delay(300);
+    dispatch({ type: "SET_UNDRAGGED_NAMES", names: iguanaNames });
+  };
 
   return (
     <DndProvider backend={HTML5Backend}>
@@ -65,32 +66,32 @@ const PhyloTreeDnD = () => {
         <Grid container spacing={0}>
           <PhyloTreeSidebar>
             <Grid item>
-              <IguanaDragSource
+              <DragSourcePanel
                 undraggedNames={undraggedNames}
                 completedTreeVisible={completedTreeVisible}
               />
             </Grid>
-            <Grid item>
+            {/* <Grid item>
               <PhyloTreeDnDMenu
                 handleCheckTree={handleCheckTree}
                 handleResetTree={handleResetTree}
                 handleShowTree={handleShowTree}
                 completedTreeVisible={completedTreeVisible}
               />
-            </Grid>
+            </Grid> */}
           </PhyloTreeSidebar>
           <MainActivityArea>
-            <IguanaDropTarget
+            {/* <IguanaDropTarget
               content={content}
               draggedNames={draggedNames}
               correctnessIndicatorVisible={correctnessIndicatorVisible}
               handleDrop={handleDrop}
-            />
+            /> */}
           </MainActivityArea>
         </Grid>
       </Grid>
     </DndProvider>
-  )
-}
+  );
+};
 
-export default PhyloTreeDnD
+export default PhyloTreeDnD;
