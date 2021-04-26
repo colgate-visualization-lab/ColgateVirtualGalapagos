68c6d48958e2cf0ef2f73fe2e416acd95f27fff0
Desmond Tuiyot <dtuiyot@gmail.com>
2021-04-26 00:04:11 -0400

    small changes with store.js

--- src/store.js
+++ src/store.js
@@ -1,13 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { throttle } from "lodash";
 
-// import modulesReducer from "./slices/modulesSlice";
 import slideReducer from "./slices/slideSlice";
 import { saveState } from "./slices/localStorage";
 
 const store = configureStore({
   reducer: {
-    // module: modulesReducer,
     slide: slideReducer,
   },
 });
@@ -18,7 +16,6 @@ store.subscribe(
   throttle(() => {
     saveState({
       slide: store.getState().slide,
-      // module: store.getState().module,
     });
   }, 1000)
 );
