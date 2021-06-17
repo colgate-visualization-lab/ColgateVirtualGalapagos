96ad53b8caf7fe73e0c21471aa9b58710d340b19
nhnguyen251 <nhnguyen@colgate.edu>
2021-06-17 15:30:52 -0400

    commit on 'bubble speech'

--- src/atomic-design/templates/ChatBubble.tsx
+++ src/atomic-design/templates/ChatBubble.tsx
@@ -5,23 +5,31 @@ import { ValidBgColors, ValidTransitions } from "../../types";
 export interface ChatBubbleProps {
   color?: ValidBgColors;
   children?: React.ReactNode;
-  transition?: ValidTransitions;
+  // transition?: ValidTransitions;
   className?: string;
 }
 
 export function ChatBubble({
   children,
-  color = "bg-primary-dark",
-  transition = "none",
+  color = "bg-primary",
+  // transition = "none",
   className,
 }: ChatBubbleProps) {
   const classes = classNames(
     className,
-    "rounded-full px-8 py-8 w-1/2 rotate-30 transform origin-top-right",
-    "rounded-full h-12 w-12 bg-gray-200 rotate-45 transform origin-top-right",
-    `${color || ""}`,
-    `${(transition !== "none" && transition) || ""}`
+    // "w-screen fixed flex items-baseline justify-center",
+    // `${color || ""}`
+
   );
-  return <div className={classes}>{children}</div>;
+  return(
+  <div className={classes}>
+      <div className="rounded-full px-8 py-7 max-w-sm bg-gray-200">
+        {children}
+      </div>
+      <div className="rounded-full h-10 w-10 bg-gray-200 transform rotate-45 origin-right"></div>  
+      <div className="rounded-full h-7 w-7 bg-gray-200 transform rotate-90 origin-top"></div>  
+        
+  </div>
+  );
 }
 export default ChatBubble;
