1f4adb09afa40fdc66800c10e87a9ad76609c7c6
sandesh <chapagainsandesh88@gmail.com>
2021-06-17 10:01:26 -0400

    allow skipping intro

--- src/pages/Introduction.tsx
+++ src/pages/Introduction.tsx
@@ -1,14 +1,29 @@
 import React from "react";
+import { Text } from "../atomic-design/atoms";
+import Button from "../atomic-design/atoms/Button/Button";
 import { Character } from "../atomic-design/organisms";
 import GameBar from "../atomic-design/templates/GameBar";
 import Page from "../atomic-design/templates/Page";
+import { useTransitionContext } from "../contexts/TransitionContext";
 import Islands from "../test/Islands";
 const birdSheet = "/sprites/bird.png";
 const blueBirdSheet = "/sprites/blue_bird.png";
 
 export default function Introduction() {
+  const { startTransition } = useTransitionContext();
+
   return (
     <Page className="bg-ocean-texture">
+      <div className="fixed top-2 right-2 z-40">
+        <Button
+          size="lg"
+          variant="wooden"
+          onClick={() => startTransition("/character_select")}
+        >
+          <Text text="Skip Intro" color="text-dark" />
+        </Button>
+      </div>
+
       <Islands className="h-4/6 w-full fixed top-0 left-0 p-10" />
       <GameBar className="h-2/6">
         <div className="absolute left-0 transform -translate-x-1/2">
