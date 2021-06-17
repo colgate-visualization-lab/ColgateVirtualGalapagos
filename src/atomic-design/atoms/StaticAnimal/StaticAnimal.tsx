8ab9b04de9ecf48d07f2cbc28962b1dd2eaaf77d
sandesh <chapagainsandesh88@gmail.com>
2021-06-17 09:59:03 -0400

    add new animals

--- src/atomic-design/atoms/StaticAnimal/StaticAnimal.tsx
+++ src/atomic-design/atoms/StaticAnimal/StaticAnimal.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import BoobyHead from "./BoobyHead";
 import LavaLizard from "./LavaLizard";
 import Penguin from "./Penguin";
+import SeaLion from "./SeaLion";
+import Turtle from "./Turtle";
 
 export function StaticAnimal({
   species,
   className,
 }: {
-  species: "penguin" | "lava lizard" | "booby head";
+  species: "penguin" | "lava lizard" | "booby head" | "turtle" | "sea lion";
   className?: string;
 }) {
   const classes = className + " pointer-events-none";
@@ -19,6 +21,10 @@ export function StaticAnimal({
       return <LavaLizard className={classes} />;
     case "booby head":
       return <BoobyHead className={classes} />;
+    case "turtle":
+      return <Turtle className={classes} />;
+    case "sea lion":
+      return <SeaLion className={classes} />;
   }
 }
 
