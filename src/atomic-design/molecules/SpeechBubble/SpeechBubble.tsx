18f20d929f2c46dabd93cd3afc018c602e74b5dd
sandesh <chapagainsandesh88@gmail.com>
2021-06-25 14:35:35 -0400

    allow speech with inputs

--- src/atomic-design/molecules/SpeechBubble/SpeechBubble.tsx
+++ src/atomic-design/molecules/SpeechBubble/SpeechBubble.tsx
@@ -3,8 +3,21 @@ import React, { useState } from "react";
 import Howler from "react-howler";
 import Text, { TextProps } from "../../atoms/Text/Text";
 import { AiFillPlayCircle, AiFillPauseCircle } from "react-icons/ai";
+import { ValidBgColors, ValidTextColors } from "../../../types";
+import TextBox from "../../atoms/TextBox/TextBox";
 
-export interface SpeechBubbleProps extends TextProps {
+const validInputs = ["text", "dropdown", "password"] as const;
+
+type ValidInputTypes = typeof validInputs[number];
+type FieldType = `${string}:${ValidInputTypes}`;
+
+const isField = (x: any): x is FieldType => {
+  if (typeof x !== "string") return false;
+  const fieldType: any = x.split(":").pop();
+  return fieldType && validInputs.includes(fieldType);
+};
+
+export interface SpeechBubbleProps extends Omit<TextProps, "color"> {
   className?: string;
   position?:
     | "left"
@@ -15,6 +28,10 @@ export interface SpeechBubbleProps extends TextProps {
     | "bottom right";
   audio?: string;
   inputTest?: string;
+  color?: ValidBgColors;
+  textColor?: ValidTextColors;
+  inputFields?: FieldType[];
+  onInputChange?: Function;
 }
 
 export default function SpeechBubble({
@@ -22,11 +39,17 @@ export default function SpeechBubble({
   position = "right",
   audio,
   inputTest,
+  color = "bg-primary-light",
+  textColor = "text-dark",
+  text,
+  inputFields,
+  onInputChange,
   ...rest
 }: SpeechBubbleProps) {
   const classes = classNames(
     className,
-    "absolute rounded-full z-40 min-w-80 transform bg-primary-light animate-fade-in",
+    color,
+    "absolute rounded-full z-40 min-w-80 transform scale-100 animate-fade-in transition-normal",
     {
       "right-0 translate-x-full top-0 -translate-y-full": position === "right",
       "-translate-y-full": position === "left",
@@ -46,13 +69,52 @@ export default function SpeechBubble({
       position === "bottom left",
   });
   const [playAudio, setAudio] = useState(false);
+  let chunkedText: Array<FieldType | string> = text?.split("<<>>") || [];
+  if (inputFields && inputFields.length) {
+    chunkedText = chunkedText.reduce(
+      (acc: Array<FieldType | string>, elem, i) => [
+        ...acc,
+        elem,
+        inputFields[i] || "",
+      ],
+      []
+    );
+  }
+
   return (
     <div className={classes}>
       <div className="relative py-3 px-10">
-        <Text {...rest} color="text-dark" />
-        {inputTest &&(
-          <input className="border border-transparent focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"/>
-     
+        {chunkedText?.map((txt: FieldType | string) => {
+          if (isField(txt)) {
+            const [varName, inputType] = txt.split(":");
+            switch (inputType) {
+              case "text":
+              case "password":
+                return (
+                  <TextBox
+                    key={varName}
+                    type={inputType}
+                    name={varName}
+                    onChange={onInputChange}
+                  />
+                );
+              case "dropdown":
+                return (
+                  <select key={varName} name={varName}>
+                    <option>Dummy Option 1</option>
+                    <option>Dummy Option 2</option>
+                  </select>
+                );
+            }
+          } else if (txt) {
+            return <Text key={txt} text={txt} color={textColor} {...rest} />;
+          } else {
+            return null;
+          }
+        })}
+
+        {inputTest && (
+          <input className="border border-transparent focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent" />
         )}
         {audio && (
           <>
@@ -78,10 +140,10 @@ export default function SpeechBubble({
             )}
           </>
         )}
-   
+
         <div className={bubbleClasses}>
-          <div className="rounded-full h-10 w-10 bg-primary-light"></div>
-          <div className="rounded-full h-7 w-7 bg-primary-light"></div>
+          <div className={"rounded-full h-10 w-10 " + color}></div>
+          <div className={"rounded-full h-7 w-7 " + color}></div>
         </div>
       </div>
     </div>
