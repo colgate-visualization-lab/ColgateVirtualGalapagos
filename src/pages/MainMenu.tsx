27fc94107e5b064394a14933bc626f1ea8a7ab35
sandesh <chapagainsandesh88@gmail.com>
2021-06-25 14:32:43 -0400

    add settings

--- src/pages/MainMenu.tsx
+++ src/pages/MainMenu.tsx
@@ -4,13 +4,13 @@ import Page from "../atomic-design/templates/Page";
 import { useTransitionContext } from "../contexts/TransitionContext";
 import { toTitleCase } from "../utils";
 
-const menuOptions = ["tutorial", "introduction", "mysteries"];
+const menuOptions = ["tutorial", "introduction", "mysteries", "settings"];
 
 export default function MainMenu() {
   const { startTransition } = useTransitionContext();
   return (
     <Page transition="animate-fade-in" color="bg-primary-dark">
-      <div className="flex flex-col h-52 justify-between">
+      <div className="flex flex-col h-64 justify-between">
         {menuOptions.map((option) => (
           <Button
             key={option}
