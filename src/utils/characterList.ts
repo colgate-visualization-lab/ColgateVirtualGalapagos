ac69a8bf504ca1d05f2af15480c2d42a606e0877
sandesh <chapagainsandesh88@gmail.com>
2021-06-25 14:31:00 -0400

    rename characters

--- src/utils/characterList.ts
+++ src/utils/characterList.ts
@@ -1,13 +1,13 @@
 import { CharacterType } from "../contexts/GameContext";
 
-const characters: CharacterType[] = [
+const characterList: CharacterType[] = [
   {
-    name: "lordOfLight",
-    displayName: "Lord Of Light",
+    name: "carlos",
+    displayName: "Carlos",
     quotes: ["I will fight for you!"],
     situationalQuotes: {
       pick: [
-        "Thanks for picking me as your buddy! I'd be glad to show you around.",
+        "Hi, I am Carlos. Thanks for picking me as your buddy! I'd be glad to show you around.",
       ],
     },
     spriteConfig: {
@@ -18,12 +18,12 @@ const characters: CharacterType[] = [
     },
   },
   {
-    name: "blueBirdie",
-    displayName: "Blue Birdie",
-    quotes: ["I am blue and will assist you."],
+    name: "adriana",
+    displayName: "Adriana",
+    quotes: ["I am blue, and I will assist you."],
 
     situationalQuotes: {
-      pick: ["Blue birdie is glad you picked her as your buddy!"],
+      pick: ["Adriana is glad you picked her as your buddy!"],
     },
     spriteConfig: {
       fileName: "/sprites/blue_bird.png",
@@ -34,28 +34,13 @@ const characters: CharacterType[] = [
     },
   },
   {
-    name: "lordOfLight2",
-    displayName: "Lord Of Light II",
-    quotes: ["I will also fight for you"],
-    situationalQuotes: {
-      pick: [
-        "I thought the second Lord would go unnoticed. You shall not regret this.",
-      ],
-    },
-    spriteConfig: {
-      fileName: "/sprites/attacking_soldier.png",
-      bounds: { x: 0, y: 0, width: 516, height: 306 },
-      frame: { width: 172, height: 153 },
-      speed: 200,
-    },
-  },
-
-  {
-    name: "greenBirdie",
-    displayName: "Green Birdie",
+    name: "alberto",
+    displayName: "Alberto",
     quotes: ["I am green and I fly."],
     situationalQuotes: {
-      pick: ["I'll fly around and help you spot things!"],
+      pick: [
+        "Hi there. I am Alberto. I'll fly around and help you spot things!",
+      ],
     },
     spriteConfig: {
       fileName: "/sprites/bird.png",
@@ -66,4 +51,4 @@ const characters: CharacterType[] = [
     },
   },
 ];
-export default characters;
+export default characterList;
