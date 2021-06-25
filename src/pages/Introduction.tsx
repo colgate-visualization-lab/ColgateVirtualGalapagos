76ce95849e391c2714aa3292d86b4000a4b93238
sandesh <chapagainsandesh88@gmail.com>
2021-06-25 14:33:25 -0400

    add demo conversation

--- src/pages/Introduction.tsx
+++ src/pages/Introduction.tsx
@@ -1,16 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import { Text } from "../atomic-design/atoms";
 import Button from "../atomic-design/atoms/Button/Button";
 import { Character } from "../atomic-design/organisms";
+import Conversation, {
+  LineType,
+} from "../atomic-design/templates/Conversation";
 import GameBar from "../atomic-design/templates/GameBar";
 import Page from "../atomic-design/templates/Page";
 import { useTransitionContext } from "../contexts/TransitionContext";
 import Islands from "../test/Islands";
+import { Island } from "../test/islandsInfo";
 const birdSheet = "/sprites/bird.png";
 const blueBirdSheet = "/sprites/blue_bird.png";
 
 export default function Introduction() {
   const { startTransition } = useTransitionContext();
+
+  const [selectedIsland, setSelectedIsland] = useState<Island["name"]>();
 
   return (
     <Page>
@@ -24,9 +30,99 @@ export default function Introduction() {
         </Button>
       </div>
 
-      <Islands className="h-4/6 w-full fixed top-0 left-0 p-10" />
-      <GameBar className="h-2/6">
-        <div className="absolute left-0 transform -translate-x-1/2">
+      <Islands
+        selectedIsland={selectedIsland}
+        className="h-9/12 w-full fixed top-0 left-1/2 transform -translate-x-1/2 p-10"
+      />
+      <GameBar className="h-3/12">
+        <Conversation
+          onCheckPoint={(line: LineType, index: number) => {
+            console.log(index);
+            index === 2
+              ? setSelectedIsland("isabela")
+              : setSelectedIsland(undefined);
+          }}
+          characters={["carlos", "adriana"]}
+          script={[
+            {
+              speaker: "carlos",
+              speech:
+                "Hey Adriana, won't you just look at that sky! It's like a dream out here at night.",
+              audio: "/audio/welcome.mp3",
+            },
+            {
+              speaker: "adriana",
+              speech:
+                "Hey, can I ask you somethin’? You know, without you gettin’ all defensive and stuff.",
+              audio: "/audio/welcome.mp3",
+            },
+            {
+              speaker: "carlos",
+              speech: "Hold on, lets check out these modules on Isabela first.",
+              audio: "/audio/welcome.mp3",
+              isCheckpoint: true,
+            },
+            {
+              speaker: "adriana",
+              speech:
+                "It’s been bothering me ever since I last saw you in Colorado.",
+              audio: "/audio/welcome.mp3",
+            },
+            {
+              speaker: "carlos",
+              speech: "Me,too.",
+              audio: "/audio/welcome.mp3",
+            },
+            {
+              speaker: "adriana",
+              speech: "What’s that?",
+              audio: "/audio/welcome.mp3",
+            },
+            {
+              speaker: "carlos",
+              speech: "Ask your question.",
+              audio: "/audio/welcome.mp3",
+            },
+            {
+              speaker: "adriana",
+              speech: "Why’d you never say goodbye?",
+              audio: "/audio/welcome.mp3",
+            },
+
+            {
+              speaker: "carlos",
+              speech: "I don’t like goodbyes.",
+              audio: "/audio/welcome.mp3",
+            },
+            {
+              speaker: "adriana",
+              speech:
+                "But why didn’t you... forget it, forget it... maybe it’s best I don’t say it.",
+              audio: "/audio/welcome.mp3",
+            },
+            {
+              speaker: "carlos",
+              speech: "I never wanted to leave you, Adriana.",
+              audio: "/audio/welcome.mp3",
+            },
+            {
+              speaker: "adriana",
+              speech: "But you did.",
+              audio: "/audio/welcome.mp3",
+            },
+            {
+              speaker: "carlos",
+              speech: "I know I did.",
+              audio: "/audio/welcome.mp3",
+            },
+            {
+              speaker: "adriana",
+              speech: "Why?",
+              audio: "/audio/welcome.mp3",
+            },
+          ]}
+        />
+        {/* <div className="absolute left-0 transform -translate-x-1/2">
           <Character
             name="random"
             audio="/audio/welcome.mp3"
@@ -53,7 +149,7 @@ export default function Introduction() {
             speed={200}
             scale={{ x: 0.8, y: 0.55 }}
           />
-        </div>
+        </div> */}
       </GameBar>
     </Page>
   );
