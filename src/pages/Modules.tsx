19bb25fb564f7bf413c4fbc18993448ec9feafe8
sandesh <chapagainsandesh88@gmail.com>
2021-07-06 13:21:07 -0400

    move to different page after mystery selection

--- src/pages/Modules.tsx
+++ src/pages/Modules.tsx
@@ -14,54 +14,41 @@ import { CharacterType, useGameContext } from "../contexts/GameContext";
 import { useTransitionContext } from "../contexts/TransitionContext";
 import IslandBackgound from "../test/IslandBackgound";
 import Islands from "../test/Islands";
-import { Island, ModuleType } from "../test/islandsInfo";
+import islands, { Island, ModuleType } from "../test/islandsInfo";
+import { ValidIslandNames } from "../types";
+import { makeIntroScript } from "../utils/script";
 
-export default function Mysteries() {
+export default function Modules() {
   const [info, setInfo] = useState<string>();
   const [selectedIsland, setSelectedIsland] = useState<Island>();
-
+  const [inScriptMode, setScriptMode] = useState(false);
   const { characters } = useGameContext();
   const { startTransition } = useTransitionContext();
   const [script, setScript] = useState<ScriptType>();
   const history = useHistory();
+  const params = useParams<{ name?: ValidIslandNames }>();
 
   useEffect(() => {
-    if (selectedIsland) {
-      setScript({
-        lines: [
-          {
-            speaker: currentCharacter?.name,
-            speech: `That's ${
-              selectedIsland.displayName || selectedIsland.name
-            }! Feel free to take a look around the modules, or click on the button to explore the mystery futher.`,
-          },
-        ],
-      });
-    } else {
-      setScript({
-        id: "first",
-        lines: [
-          {
-            speaker: currentCharacter?.name,
-            speech: "So many islands! Which one do you wanna go to?",
-            audio: "/audio/welcome.mp3",
-          },
-        ],
-      });
+    console.log("changed:::", params);
+    if (params.name) {
+      const island = islands.find((island) => island.name === params.name);
+      if (island) {
+        setScript(makeIntroScript(params.name, currentCharacter, "demoUser"));
+        setScriptMode(true);
+        setSelectedIsland(island);
+      } else history.replace("/mysteries");
     }
-  }, [selectedIsland]);
+  }, [params.name]);
 
   const currentCharacter = characters[characters.length - 1];
   return !currentCharacter ? (
     <Redirect to="/character_select" />
   ) : (
     <Page className="bg-gradient-to-t from-primary to-primary-dark">
-      {selectedIsland && (
+      {selectedIsland && !params.name && (
         <div className="fixed top-5 left-1/2 z-40 transform -translate-x-1/2">
           <Button
-            onClick={() =>
-              startTransition(`/mysteries/${selectedIsland.name}`, "galaxy")
-            }
+            onClick={() => startTransition(`/mysteries/${selectedIsland.name}`)}
             size="lg"
             variant="wooden"
           >
@@ -77,16 +64,25 @@ export default function Mysteries() {
       <IslandBackgound className="h-9/12 w-full fixed top-0 left-1/2 transform -translate-x-1/2 p-10" />
 
       <Islands
-        className="h-9/12 w-full fixed top-0 left-1/2 transform -translate-x-1/2 p-10"
+        className={
+          (inScriptMode ? "pointer-events-none" : "") +
+          " h-9/12 w-full fixed top-0 left-1/2 transform -translate-x-1/2 p-10"
+        }
         onMouseEnter={(island: Island | ModuleType) => setInfo(island.info)}
         onMouseLeave={() => setInfo(undefined)}
-        onSelect={(island: Island) => setSelectedIsland(island)}
+        onSelect={
+          params.name
+            ? undefined
+            : (island: Island) => setSelectedIsland(island)
+        }
         selectedIsland={selectedIsland?.name}
       />
       <GameBar className="h-3/12 ">
-        {script && <Conversation script={script} />}
+        {script && (
+          <Conversation script={script} onFinish={() => setScriptMode(false)} />
+        )}
       </GameBar>
-      {info && <InfoBox info={info} />}
+      {info && !inScriptMode && <InfoBox info={info} />}
     </Page>
   );
 }
