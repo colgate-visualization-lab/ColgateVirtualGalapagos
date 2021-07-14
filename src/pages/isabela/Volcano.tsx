c706bdf7d46319ae07a7e75256bb0c96222a94e8
sandesh <chapagainsandesh88@gmail.com>
2021-07-14 11:13:19 -0400

    make minor changes

---
+++ src/pages/isabela/Volcano.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Page from "../../atomic-design/templates/Page";
+
+export default function Volcano() {
+  return (
+    <Page>
+      <p>Volcano Module Starts Here</p>
+    </Page>
+  );
+}
