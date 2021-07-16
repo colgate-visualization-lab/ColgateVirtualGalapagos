24381f9b8a7c99a42217e653bb1661d2f7b88e21
sandesh <chapagainsandesh88@gmail.com>
2021-07-16 10:32:06 -0400

    add base modal components

--- src/atomic-design/atoms/index.ts
+++ src/atomic-design/atoms/index.ts
@@ -4,3 +4,4 @@ export { default as AnimationVideo } from "./AnimationVideo/AnimationVideo";
 export { default as Sprite } from "./Sprite/Sprite";
 export { default as StaticAnimal } from "./StaticAnimal/StaticAnimal";
 export { default as Checkbox } from "./Checkbox/Checkbox";
+export { default as Modal } from "./Modal/Modal";
