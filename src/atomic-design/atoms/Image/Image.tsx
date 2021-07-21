0c7782679984424cd4af74cfd9e257795129803a
Sandesh Chapagain <chapagainsandesh88@gmail.com>
2021-07-20 21:23:12 -0400

    allow props to switch bettween cover and contain styles

--- src/atomic-design/atoms/Image/Image.tsx
+++ src/atomic-design/atoms/Image/Image.tsx
@@ -1,16 +1,22 @@
+import classNames from "classnames";
 import React from "react";
 
 export interface ImageProps {
   className?: string;
   src: string;
   alt?: string;
+  coverType?: "contain" | "cover";
 }
 
 Image.defaultProps = {
   alt: "",
+  coverType: "cover",
 };
 
-export default function Image({ className, ...rest }: ImageProps) {
-  const classes = "object-cover w-full h-auto " + className;
+export default function Image({ className, coverType, ...rest }: ImageProps) {
+  const classes = classNames(className, "block flex h-full w-full max-w-full", {
+    "object-cover": coverType === "cover",
+    "object-contain": coverType === "contain",
+  });
   return <img className={classes} {...rest} />;
 }
