4acb62aa81a48c7f258eecb1939a38557d85f70b
Sandesh Chapagain <chapagainsandesh88@gmail.com>
2021-07-28 15:56:45 -0400

    accept bg color

--- src/atomic-design/atoms/Modal/Modal.tsx
+++ src/atomic-design/atoms/Modal/Modal.tsx
@@ -1,20 +1,29 @@
 import classNames from "classnames";
 import React from "react";
 import ReactDOM from "react-dom";
+import { ValidBgColors } from "../../../types";
 
-interface ModalProps {
+export interface ModalProps {
   onDiscard?: React.MouseEventHandler<HTMLDivElement>;
   children?: React.ReactNode;
+  size?: "md" | "lg";
+  color?: ValidBgColors;
 }
 
-const Overlay = ({ children, onDiscard }: ModalProps) => {
+const Overlay = ({ children, onDiscard, size = "md", color }: ModalProps) => {
   const classes = classNames(
-    "h-1/2 w-3/4 max-w-lg max-h-96",
-    "filter shadow-2xl bg-primary-light"
+    "flex items-center justify-center",
+    "h-1/2 w-3/4",
+    color,
+    {
+      "filter shadow-2xl": color != null,
+      "max-w-lg max-h-96": size === "md",
+      "max-w-2xl max-h-104": size === "lg",
+    }
   );
 
   const wrapperClasses = classNames(
-    "fixed h-full w-full top-0 left-0",
+    "fixed h-full w-full z-50 top-0 left-0",
     "flex items-center text-center justify-center",
     "bg-dark bg-opacity-80",
     "animate-fade-in cursor-pointer"
